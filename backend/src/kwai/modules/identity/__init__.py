435b3409f2498511398fcc6178074a7ad5bad4c4
fbraem <franky.braem@gmail.com>
2022-12-30 13:22:13 +0100

    Rearrange order of import

--- backend/src/kwai/modules/identity/__init__.py
+++ backend/src/kwai/modules/identity/__init__.py
@@ -1,7 +1,8 @@
+from .exceptions import AuthenticationException
+
 from .authenticate_user import (
     AuthenticateUser,
     AuthenticateUserCommand,
 )
-from .exceptions import AuthenticationException
 
 __all__ = ["AuthenticationException", "AuthenticateUser", "AuthenticateUserCommand"]
