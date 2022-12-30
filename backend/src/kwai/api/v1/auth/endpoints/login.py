abebf9049fd3ac28359815dacf6c3a06696acdbb
fbraem <franky.braem@gmail.com>
2022-12-30 15:10:59 +0100

    Implement refresh access_token API

--- backend/src/kwai/api/v1/auth/endpoints/login.py
+++ backend/src/kwai/api/v1/auth/endpoints/login.py
@@ -1,38 +1,27 @@
-from datetime import datetime
-
-from fastapi import APIRouter, Depends, status, Response, HTTPException
+"""Module that implements all APIs for login."""
+from fastapi import APIRouter, Depends, status, HTTPException, Form
 from fastapi.security import OAuth2PasswordRequestForm
 from jose import jwt
 from pydantic import BaseModel
 
 from kwai.core.db.database import get_database
 from kwai.core.domain.value_objects import InvalidEmailException
-from kwai.core.settings import get_settings
+from kwai.core.settings import get_settings, Settings
 from kwai.modules.identity import (
     AuthenticateUser,
     AuthenticateUserCommand,
     AuthenticationException,
 )
+from kwai.modules.identity.refresh_access_token import (
+    RefreshAccessTokenCommand,
+    RefreshAccessToken,
+)
 from kwai.modules.identity.tokens import (
     AccessTokenDbRepository,
     RefreshTokenDbRepository,
+    RefreshTokenEntity,
 )
 from kwai.modules.identity.users import UserAccountDbRepository
-
-
-class JsonUserAttributes(BaseModel):
-    name: str
-    email: str
-
-
-class JsonUserData(BaseModel):
-    id: str
-    type: str = "users"
-    attributes: JsonUserAttributes
-
-
-class JsonUserDocument(BaseModel):
-    data: JsonUserData
 
 
 class TokenSchema(BaseModel):
@@ -56,8 +45,12 @@ async def login(
     db=Depends(get_database),
     form_data: OAuth2PasswordRequestForm = Depends(),
 ):
+    """Implements the login API."""
     command = AuthenticateUserCommand(
-        username=form_data.username, password=form_data.password
+        username=form_data.username,
+        password=form_data.password,
+        access_token_expiry_minutes=settings.access_token_expires_in,
+        refresh_token_expiry_minutes=settings.refresh_token_expires_in,
     )
 
     try:
@@ -66,20 +59,62 @@ async def login(
             AccessTokenDbRepository(db),
             RefreshTokenDbRepository(db),
         ).execute(command)
-    except InvalidEmailException as e:
+    except InvalidEmailException as exc:
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid email address"
-        )
-    except AuthenticationException as e:
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail=str(e))
+        ) from exc
+    except AuthenticationException as exc:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED, detail=str(exc)
+        ) from exc
 
+    return encode_token(refresh_token, settings)
+
+
+@router.post(
+    "/access_token",
+    response_model=TokenSchema,
+    summary="Renew an access token using a refresh token.",
+)
+async def renew_access_token(
+    settings=Depends(get_settings),
+    db=Depends(get_database),
+    refresh_token: str = Form(),
+):
+    """Implements the refresh access token API."""
+    decoded_refresh_token = jwt.decode(
+        refresh_token,
+        key=settings.jwt_refresh_secret,
+        algorithms=[settings.jwt_algorithm],
+    )
+
+    command = RefreshAccessTokenCommand(
+        identifier=decoded_refresh_token["jti"],
+        access_token_expiry_minutes=settings.access_token_expires_in,
+        refresh_token_expiry_minutes=settings.refresh_token_expires_in,
+    )
+
+    try:
+        new_refresh_token = RefreshAccessToken(
+            RefreshTokenDbRepository(db), AccessTokenDbRepository(db)
+        ).execute(command)
+    except AuthenticationException as exc:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED, detail=str(exc)
+        ) from exc
+
+    return encode_token(new_refresh_token, settings)
+
+
+def encode_token(refresh_token: RefreshTokenEntity, settings: Settings):
+    """Encode the access and refresh token with JWT."""
     return {
         "access_token": jwt.encode(
             {
-                "iat": refresh_token().access_token().traceable_time.created_at,
-                "exp": refresh_token().access_token().expiration,
-                "jti": str(refresh_token().access_token().identifier),
-                "sub": str(refresh_token().access_token().user_account().user.uuid),
+                "iat": refresh_token.access_token.traceable_time.created_at,
+                "exp": refresh_token.access_token.expiration,
+                "jti": str(refresh_token.access_token.identifier),
+                "sub": str(refresh_token.access_token.user_account.user.uuid),
                 "scope": [],
             },
             settings.jwt_secret,
@@ -87,14 +122,12 @@ async def login(
         ),
         "refresh_token": jwt.encode(
             {
-                "iat": refresh_token().traceable_time.created_at,
-                "exp": refresh_token().expiration,
-                "jti": str(refresh_token().identifier),
+                "iat": refresh_token.traceable_time.created_at,
+                "exp": refresh_token.expiration,
+                "jti": str(refresh_token.identifier),
             },
             settings.jwt_refresh_secret,
             settings.jwt_algorithm,
         ),
-        "expiration": refresh_token()
-        .access_token()
-        .expiration.isoformat(" ", "seconds"),
+        "expiration": refresh_token.access_token.expiration.isoformat(" ", "seconds"),
     }
---
+++ backend/src/kwai/modules/identity/refresh_access_token.py
@@ -0,0 +1,81 @@
+"""Module that defines the use case for refreshing an access token."""
+from dataclasses import dataclass
+from datetime import datetime, timedelta
+
+from kwai.modules.identity import AuthenticationException
+from kwai.modules.identity.tokens import (
+    RefreshTokenRepository,
+    AccessTokenRepository,
+    RefreshTokenEntity,
+    TokenIdentifier,
+    AccessToken,
+    RefreshToken,
+)
+
+
+@dataclass(kw_only=True, frozen=True, slots=True)
+class RefreshAccessTokenCommand:
+    """Input for the refresh access token use case."""
+
+    identifier: str  # The identifier of the refresh token.
+    access_token_expiry_minutes: int = 60 * 2  # 2 hours
+    refresh_token_expiry_minutes: int = 60 * 24 * 60  # 2 months
+
+
+class RefreshAccessToken:
+    """Use case for refreshing an access token. A new access token will also result in a new refresh token."""
+
+    def __init__(
+        self,
+        refresh_token_repo: RefreshTokenRepository,
+        access_token_repo: AccessTokenRepository,
+    ):
+        self._refresh_token_repo = refresh_token_repo
+        self._access_token_repo = access_token_repo
+
+    def execute(self, command: RefreshAccessTokenCommand) -> RefreshTokenEntity:
+        """Executes the use case.
+
+        :raises:
+            RefreshTokenNotFoundException: Raised when the refresh token does not exist.
+        :raises:
+            AuthenticationException: Raised when the refresh token is expired, the refresh token is revoked or the user
+                is revoked.
+        """
+        refresh_token = self._refresh_token_repo.get_by_token_identifier(
+            TokenIdentifier(command.identifier)
+        )
+
+        if refresh_token.is_expired:
+            raise AuthenticationException("Refresh token is expired")
+
+        if refresh_token.revoked:
+            raise AuthenticationException("Refresh token is revoked")
+
+        # Revoke the old refresh token and access token
+        refresh_token.access_token.revoke()
+        refresh_token.revoke()
+        self._refresh_token_repo.update(refresh_token)
+        self._access_token_repo.update(refresh_token.access_token)
+
+        if refresh_token.access_token.user_account.revoked:
+            raise AuthenticationException("User is revoked")
+
+        # Create a new access and refresh token
+        access_token = self._access_token_repo.create(
+            AccessToken(
+                identifier=TokenIdentifier.generate(),
+                expiration=datetime.utcnow()
+                + timedelta(minutes=command.access_token_expiry_minutes),
+                user_account=refresh_token.access_token.user_account,
+            )
+        )
+
+        return self._refresh_token_repo.create(
+            RefreshToken(
+                identifier=TokenIdentifier.generate(),
+                expiration=datetime.utcnow()
+                + timedelta(minutes=command.refresh_token_expiry_minutes),
+                access_token=access_token,
+            )
+        )
