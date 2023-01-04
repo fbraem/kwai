750f849c277f628818a34975897d44a156cfc0e1
fbraem <franky.braem@gmail.com>
2023-01-04 19:24:23 +0100

    Add test_smtp_mailer

---
+++ backend/src/tests/core/mail/test_smtp_mailer.py
@@ -0,0 +1,31 @@
+import pytest
+
+from kwai.core.domain.value_objects import EmailAddress
+from kwai.core.mail import Message, Mail, Recipients, Recipient
+from kwai.core.mail.smtp_mailer import SmtpMailer
+from kwai.core.settings import get_settings
+
+
+@pytest.fixture
+def mailer():
+    settings = get_settings()
+    m = SmtpMailer(settings.email.host, settings.email.port)
+    m.connect(settings.email.user, settings.email.password)
+    return m
+
+
+def test_text_message(mailer):
+    mail = Mail(
+        subject="test_text_message",
+        recipients=Recipients(
+            from_=Recipient(email=EmailAddress("webmaster@kwai.com"), name="Webmaster"),
+            to=[
+                Recipient(
+                    email=EmailAddress("jigoro.kano@kwai.com"), name="Jigoro Kano"
+                )
+            ],
+        ),
+        text="Hell world",
+        html="<b>Hello World</b>",
+    )
+    mailer.send(mail)
