6ebc2c0163ec016307a6aff03b0c5df1806fddac
fbraem <franky.braem@gmail.com>
2023-01-12 19:43:46 +0100

    Move celery setup to get_celery_app

--- backend/src/tasks.py
+++ backend/src/tasks.py
@@ -2,7 +2,6 @@ import os
 import sys
 
 from loguru import logger
-from kombu import Queue, Exchange
 
 from kwai.core.events import CeleryBus
 from kwai.core.events.celery import get_celery_app
@@ -12,24 +11,14 @@ from kwai.modules.identity.user_recoveries.user_recovery_events import (
 )
 from kwai.tasks.identity.user_recovery_tasks import EmailUserRecoveryTask
 
-_QUEUES = ("kwai", "identity")
-
-app = get_celery_app()
-app.conf.task_create_missing_queues = False
-app.conf.task_queues = (
-    Queue(name=q, exchange=Exchange("q"), routing_key=q) for q in _QUEUES
-)
-app.conf.task_default_queue = "kwai"
-app.conf.task_routes = {
-    "identity.*": {"queue": "identity"},
-}
-
 settings = None
 try:
     settings = get_settings()
 except SettingsException as se:
     logger.error(f"Could not load the settings: {se}!")
     exit(1)
+
+app = get_celery_app(settings.celery)
 
 if settings.celery.logger:
     logger.remove()  # Remove the default logger
