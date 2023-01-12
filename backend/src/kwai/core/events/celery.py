7413bed2be60098c5226b64492c6313ba63dc4d7
fbraem <franky.braem@gmail.com>
2023-01-12 19:43:12 +0100

    Move some setup code to get_celery_app and return result from __call__ in EventTask

--- backend/src/kwai/core/events/celery.py
+++ backend/src/kwai/core/events/celery.py
@@ -2,9 +2,10 @@
 from abc import ABC
 
 from celery import Celery, Task
+from kombu import Queue, Exchange
 from loguru import logger
 
-from kwai.core.settings import get_settings
+from kwai.core.settings import CelerySettings
 
 
 class EventTask(Task, ABC):
@@ -14,16 +15,27 @@ class EventTask(Task, ABC):
         with logger.contextualize(task_id=self.request.id, task_name=self.request.task):
             logger.info("start")
             try:
-                self.run(*args, **kwargs)
+                result = self.run(*args, **kwargs)
                 logger.info("ended")
             except Exception as exc:
                 logger.error(f"failed: {exc}")
                 raise
+        return result
 
 
-settings = get_settings()
+_QUEUES = ("kwai", "identity")
 
 
-def get_celery_app():
+def get_celery_app(settings: CelerySettings):
     """Returns a Celery instance."""
-    return Celery("kwai", broker=settings.celery.broker, task_cls=EventTask)
+    celery = Celery("kwai", broker=settings.broker, task_cls=EventTask)
+    celery.conf.task_create_missing_queues = False
+    celery.conf.task_queues = (
+        Queue(name=q, exchange=Exchange("q"), routing_key=q) for q in _QUEUES
+    )
+    celery.conf.task_default_queue = "kwai"
+    celery.conf.task_routes = {
+        "identity.*": {"queue": "identity"},
+    }
+
+    return celery
