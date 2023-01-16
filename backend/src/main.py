9c2cfad9d709502bb24629daa65f2fabd1bc56f8
fbraem <franky.braem@gmail.com>
2023-01-16 21:14:50 +0100

    Use sys.exit

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,15 +1,16 @@
+import sys
+
 from loguru import logger
 import uvicorn
 
 from kwai.app import create_app
 from kwai.core.settings import get_settings, SettingsException
 
-settings = None
 try:
     settings = get_settings()
 except SettingsException as se:
     logger.error(f"Could not load the settings: {se}!")
-    exit(1)
+    sys.exit(1)
 
 
 if __name__ == "__main__":
