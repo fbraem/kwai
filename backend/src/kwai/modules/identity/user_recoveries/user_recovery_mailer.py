f7878d1669cca26408576143a5cd64ad13920aab
fbraem <franky.braem@gmail.com>
2023-01-17 22:10:56 +0100

    Add UserRecoveryMailer

---
+++ backend/src/kwai/modules/identity/user_recoveries/user_recovery_mailer.py
@@ -0,0 +1,43 @@
+"""Module that defines a mailer for a user recovery email."""
+from kwai.core.domain.mailer_service import MailerService
+from kwai.core.mail import Message, Recipients, Recipient, Mailer
+from kwai.core.template.mail_template import MailTemplate
+from kwai.modules.identity.user_recoveries import UserRecoveryEntity
+
+
+class UserRecoveryMailer(MailerService):
+    """Sends a user recovery mail."""
+
+    def __init__(
+        self,
+        mailer: Mailer,
+        recipients: Recipients,
+        mail_template: MailTemplate,
+        user_recovery: UserRecoveryEntity,
+    ):
+        self._mailer = mailer
+        self._recipients = recipients
+        self._mail_template = mail_template
+        self._user_recovery = user_recovery
+
+    def send(self) -> Message:
+        template_vars = {
+            "uuid": str(self._user_recovery.uuid),
+            "name": str(self._user_recovery.user.name),
+            "expires": 2,
+        }
+
+        mail = self._mail_template.create_mail(
+            self._recipients.with_to(
+                Recipient(
+                    email=self._user_recovery.user.email,
+                    name=str(self._user_recovery.user.name),
+                )
+            ),
+            "User recovery",
+            **template_vars,
+        )
+
+        self._mailer.send(mail)
+
+        return mail
---
+++ backend/src/tests/modules/identity/user_recoveries/test_user_recovery_mailer.py
@@ -0,0 +1,53 @@
+from datetime import datetime
+
+import pytest
+
+from kwai.core.domain.value_objects import UniqueId, LocalTimestamp, EmailAddress, Name
+from kwai.core.mail import Recipients, Mailer
+from kwai.core.template import TemplateEngine
+from kwai.core.template.mail_template import MailTemplate
+from kwai.modules.identity.user_recoveries import UserRecovery, UserRecoveryEntity
+from kwai.modules.identity.user_recoveries.user_recovery_mailer import (
+    UserRecoveryMailer,
+)
+from kwai.modules.identity.users import UserEntity, User
+
+
+@pytest.fixture(scope="module")
+def user_recovery() -> UserRecoveryEntity:
+    """Fixture creating a user recovery entity."""
+    return UserRecoveryEntity(
+        id=1,
+        domain=UserRecovery(
+            uuid=UniqueId.generate(),
+            expiration=LocalTimestamp(timestamp=datetime.utcnow(), timezone="UTC"),
+            user=UserEntity(
+                id=1,
+                domain=User(
+                    uuid=UniqueId.generate(),
+                    email=EmailAddress("jigoro.kano@kwai.com"),
+                    name=Name(first_name="Jigoro", last_name="Kano"),
+                ),
+            ),
+        ),
+    )
+
+
+def test_user_recovery_mailer_send(
+    mailer: Mailer,
+    recipients: Recipients,
+    template_engine: TemplateEngine,
+    user_recovery: UserRecoveryEntity,
+):
+    """Test user recovery mailer send"""
+    mail = UserRecoveryMailer(
+        mailer,
+        recipients,
+        MailTemplate(
+            template_engine.create("identity/recover_html"),
+            template_engine.create("identity/recover_txt"),
+        ),
+        user_recovery,
+    ).send()
+
+    assert mail, "There should be an email."
