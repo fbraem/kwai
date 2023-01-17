abc60b8d5ecdcc17a849261437ff1dee3ddf6660
fbraem <franky.braem@gmail.com>
2023-01-17 22:11:18 +0100

    Add MailTemplate

---
+++ backend/src/kwai/core/template/mail_template.py
@@ -0,0 +1,25 @@
+"""Module that defines a template for creating HTML/Text mails."""
+from kwai.core.mail import Recipients, Mail
+from kwai.core.template import Template
+
+
+class MailTemplate:
+    """Defines a wrapper around template to generate a HTML and text mail."""
+
+    def __init__(self, html_template: Template, text_template: Template):
+        self._html_template = html_template
+        self._text_template = text_template
+
+    def render_html(self, **kwargs):
+        return self._html_template.render(**kwargs)
+
+    def render_text(self, **kwargs):
+        return self._text_template.render(**kwargs)
+
+    def create_mail(self, recipients: Recipients, subject: str, **kwargs):
+        return Mail(
+            recipients,
+            subject,
+            self.render_text(**kwargs),
+            self.render_html(**kwargs),
+        )
