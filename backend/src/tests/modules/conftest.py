e94674e7e26181a902f9bb0b561c26ba43d9cc50
fbraem <franky.braem@gmail.com>
2023-01-17 22:02:30 +0100

    Add fixture for mailer

--- backend/src/tests/modules/conftest.py
+++ backend/src/tests/modules/conftest.py
@@ -2,6 +2,7 @@
 import pytest
 
 from kwai.core.db import Database
+from kwai.core.mail import Mailer
 from kwai.core.settings import get_settings
 
 
@@ -11,3 +12,14 @@ def database():
     database = Database(get_settings().db)  # pylint: disable=redefined-outer-name
     database.connect()
     return database
+
+
+@pytest.fixture
+def mailer() -> Mailer:
+    """Fixture for getting a mailer."""
+    from kwai.core.mail.smtp_mailer import SmtpMailer
+
+    settings = get_settings()
+    m = SmtpMailer(settings.email.host, settings.email.port)
+    m.connect(settings.email.user, settings.email.password)
+    return m
