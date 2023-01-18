e4d2cfbdbb6e75da96a478a27d8ba02c1befc2c3
fbraem <franky.braem@gmail.com>
2023-01-18 20:34:55 +0100

    Add mail_user_recovery

---
+++ backend/src/kwai/modules/identity/mail_user_recovery.py
@@ -0,0 +1,69 @@
+"""Module that defines the use case for sending a recovery email."""
+from dataclasses import dataclass
+
+from kwai.core.domain.exceptions import UnprocessableException
+from kwai.core.domain.value_objects import UniqueId
+from kwai.core.mail import Mailer, Recipients
+from kwai.core.template.mail_template import MailTemplate
+from kwai.modules.identity.user_recoveries import (
+    UserRecoveryRepository,
+    UserRecoveryEntity,
+)
+from kwai.modules.identity.user_recoveries.user_recovery_mailer import (
+    UserRecoveryMailer,
+)
+
+
+@dataclass(frozen=True, kw_only=True)
+class MailUserRecoveryCommand:
+    uuid: str
+
+
+class MailUserRecovery:
+    """Use case for sending a recovery email."""
+
+    def __init__(
+        self,
+        user_recovery_repo: UserRecoveryRepository,
+        mailer: Mailer,
+        recipients: Recipients,
+        mail_template: MailTemplate,
+    ):
+        self._user_recovery_repo = user_recovery_repo
+        self._mailer = mailer
+        self._recipients = recipients
+        self._mail_template = mail_template
+
+    def execute(self, command: MailUserRecoveryCommand) -> UserRecoveryEntity:
+        """Executes the use case.
+
+        :raises:
+            kwai.modules.identity.user_recoveries.UserRecoveryNotFoundException: Raised when the user
+                recovery cannot be found.
+            kwai.core.domain.exceptions.UnprocessableException:
+                Raised when the mail was already send.
+                Raised when the user recovery was already confirmed.
+        """
+        user_recovery = self._user_recovery_repo.get_by_uuid(
+            UniqueId.create_from_string(command.uuid)
+        )
+        if user_recovery.mailed:
+            raise UnprocessableException(
+                f"Mail already send for user recovery {command.uuid}"
+            )
+
+        if user_recovery.confirmed:
+            raise UnprocessableException(
+                f"User recovery {command.uuid} already confirmed"
+            )
+
+        UserRecoveryMailer(
+            self._mailer, self._recipients, self._mail_template, user_recovery
+        ).send()
+
+        user_recovery.mail_send()
+        user_recovery.traceable_time.mark_for_update()
+
+        self._user_recovery_repo.update(user_recovery)
+
+        return user_recovery
---
+++ backend/src/tests/modules/identity/test_mail_user_recovery.py
@@ -0,0 +1,80 @@
+"""Module for testing mail_user_recovery."""
+from datetime import datetime
+
+import pytest
+
+from kwai.core.db import Database
+from kwai.core.domain.exceptions import UnprocessableException
+from kwai.core.domain.value_objects import UniqueId, LocalTimestamp, EmailAddress, Name
+from kwai.core.mail import Mailer, Recipients
+from kwai.core.template.mail_template import MailTemplate
+from kwai.modules.identity.mail_user_recovery import (
+    MailUserRecovery,
+    MailUserRecoveryCommand,
+)
+from kwai.modules.identity.user_recoveries import (
+    UserRecoveryRepository,
+    UserRecoveryDbRepository,
+    UserRecoveryEntity,
+    UserRecovery,
+)
+from kwai.modules.identity.users import UserEntity, User
+
+
+@pytest.fixture(scope="module")
+def repo(database: Database) -> UserRecoveryRepository:
+    return UserRecoveryDbRepository(database)
+
+
+@pytest.fixture(scope="module")
+def user_recovery(repo: UserRecoveryRepository) -> UserRecoveryEntity:
+    user_recovery = UserRecovery(
+        uuid=UniqueId.generate(),
+        expiration=LocalTimestamp(timestamp=datetime.utcnow(), timezone="UTC"),
+        user=UserEntity(
+            id=1,
+            domain=User(
+                uuid=UniqueId.generate(),
+                email=EmailAddress("jigoro.kano@kwai.com"),
+                name=Name(first_name="Jigoro", last_name="Kano"),
+            ),
+        ),
+    )
+    entity = repo.create(user_recovery)
+    yield entity
+    repo.delete(entity)
+
+
+def test_mail_user_recovery(
+    repo: UserRecoveryRepository,
+    user_recovery: UserRecoveryEntity,
+    mailer: Mailer,
+    recipients: Recipients,
+    recovery_mail_template: MailTemplate,
+):
+    command = MailUserRecoveryCommand(uuid=str(user_recovery.uuid))
+    updated_user_recovery = MailUserRecovery(
+        repo,
+        mailer,
+        recipients,
+        recovery_mail_template,
+    ).execute(command)
+
+    assert updated_user_recovery.mailed is not None, "mailed should be set."
+
+
+def test_mail_user_recovery_already_mailed(
+    repo: UserRecoveryRepository,
+    user_recovery: UserRecoveryEntity,
+    mailer: Mailer,
+    recipients: Recipients,
+    recovery_mail_template: MailTemplate,
+):
+    command = MailUserRecoveryCommand(uuid=str(user_recovery.uuid))
+    with pytest.raises(UnprocessableException):
+        MailUserRecovery(
+            repo,
+            mailer,
+            recipients,
+            recovery_mail_template,
+        ).execute(command)
