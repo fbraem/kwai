41c16b4a72822a76cbc192a7ecfe3afd8b7b91c4
fbraem <franky.braem@gmail.com>
2023-01-23 20:17:13 +0100

    Add create_event_bus for creating a Bus dependency

--- backend/src/kwai/core/dependencies.py
+++ backend/src/kwai/core/dependencies.py
@@ -3,9 +3,12 @@ Module that defines a dependency injector container.
 
 Auto-wiring is avoided. It should be clear why and when a class is loaded.
 """
+import dramatiq.brokers.rabbitmq
+
 from lagom import Container, dependency_definition, ExplicitContainer, Singleton
 
 from kwai.core.db import Database
+from kwai.core.events import Bus, DramatiqBus
 from kwai.core.mail import Mailer
 from kwai.core.mail.smtp_mailer import SmtpMailer
 from kwai.core.settings import Settings, get_settings
@@ -23,6 +26,17 @@ container[TemplateEngine] = lambda c: Jinja2Engine(
 
 @dependency_definition(container)
 def create_mailer(c: Container) -> Mailer:
+    """Creates the mailer."""
     mailer = SmtpMailer(c[Settings].email.host, c[Settings].email.port)
     mailer.connect(c[Settings].email.user, c[Settings].email.password)
     return mailer
+
+
+@dependency_definition(container)
+def create_event_bus(c: Container) -> Bus:
+    """Creates the event bus."""
+    broker = dramatiq.brokers.rabbitmq.RabbitmqBroker(
+        url=c[Settings].broker.url + "/" + c[Settings].broker.name
+    )
+    dramatiq.set_broker(broker)
+    return DramatiqBus(broker)
