a4f3b84ca1d41f60f163834ab8dae214bca8fcb8
fbraem <franky.braem@gmail.com>
2023-01-28 20:59:52 +0100

    Fix mailed property

--- backend/src/kwai/modules/identity/user_recoveries/user_recovery.py
+++ backend/src/kwai/modules/identity/user_recoveries/user_recovery.py
@@ -1,6 +1,5 @@
 """Module that implements a user recovery entity."""
 from dataclasses import dataclass
-from datetime import datetime
 
 from kwai.core.domain.value_objects.identifier import IntIdentifier
 from kwai.core.domain.value_objects.local_timestamp import LocalTimestamp
@@ -15,26 +14,31 @@ UserRecoveryIdentifier = IntIdentifier
 class UserRecoveryEntity:
     """A user recovery domain."""
 
-    expiration: LocalTimestamp
     user: UserEntity
+    expiration: LocalTimestamp = LocalTimestamp()
     id: UserRecoveryIdentifier = UserRecoveryIdentifier(0)
     uuid: UniqueId = UniqueId.generate()
     remark: str | None = None
-    confirmation: datetime | None = None
-    mailed: datetime | None = None
+    confirmation: LocalTimestamp() = LocalTimestamp()
+    mailed_at: LocalTimestamp() = LocalTimestamp()
     traceable_time: TraceableTime = TraceableTime()
 
     def confirm(self):
         """Confirms the user recovery."""
-        self.confirmation = datetime.utcnow()
+        self.confirmation = LocalTimestamp.create_now()
         self.traceable_time = self.traceable_time.mark_for_update()
 
     @property
     def confirmed(self) -> bool:
         """Returns True when this user recovery was confirmed."""
-        return self.confirmation is not None
+        return not self.confirmation.empty
+
+    @property
+    def mailed(self) -> bool:
+        """Returns True when the mail was already send."""
+        return not self.mailed_at.empty
 
     def mail_send(self):
         """Sets the timestamp when mail has been sent."""
-        self.mailed = datetime.utcnow()
+        self.mailed_at = LocalTimestamp.create_now()
         self.traceable_time = self.traceable_time.mark_for_update()
--- backend/src/tests/modules/identity/test_mail_user_recovery.py
+++ backend/src/tests/modules/identity/test_mail_user_recovery.py
@@ -1,5 +1,4 @@
 """Module for testing mail_user_recovery."""
-from datetime import datetime
 
 import pytest
 
@@ -31,7 +30,7 @@ def repo(database: Database) -> UserRecoveryRepository:
 @pytest.fixture(scope="module")
 def user_recovery(repo: UserRecoveryRepository, user: UserEntity) -> UserRecoveryEntity:
     user_recovery = UserRecoveryEntity(
-        expiration=LocalTimestamp(timestamp=datetime.utcnow(), timezone="UTC"),
+        expiration=LocalTimestamp.create_future(hours=2),
         user=user,
     )
     entity = repo.create(user_recovery)
