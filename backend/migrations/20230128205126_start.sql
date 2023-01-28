46dc178c0edf886ae027632c2a43a61234e61972
fbraem <franky.braem@gmail.com>
2023-01-28 21:59:27 +0100

    Add the first migration file

---
+++ backend/migrations/20230128205126_start.sql
@@ -0,0 +1,57 @@
+-- migrate:up
+create table if not exists users (
+    id                      int unsigned auto_increment          primary key,
+    email                   varchar(255)                         not null,
+    password                varchar(255)                         not null,
+    last_login              datetime                             null,
+    first_name              varchar(255)                         null,
+    last_name               varchar(255)                         null,
+    remark                  text                                 null,
+    uuid                    varchar(255)                         not null,
+    created_at              timestamp  default CURRENT_TIMESTAMP not null,
+    updated_at              timestamp                            null,
+    member_id               int                                  null,
+    revoked                 tinyint(1) default 0                 not null,
+    last_unsuccessful_login datetime                             null,
+    admin                   tinyint(1) default 0                 not null
+) charset = utf8mb3;
+
+create table if not exists user_recoveries
+(
+    id                  int unsigned auto_increment         primary key,
+    user_id             int                                 not null,
+    uuid                varchar(255)                        not null,
+    expired_at          datetime                            not null,
+    expired_at_timezone varchar(255)                        not null,
+    confirmed_at        datetime                            null,
+    remark              varchar(255)                        null,
+    created_at          timestamp default CURRENT_TIMESTAMP not null,
+    updated_at          timestamp                           null,
+    mailed_at           datetime                            null
+) charset = utf8mb3;
+
+create table oauth_access_tokens
+(
+    id         int unsigned auto_increment          primary key,
+    identifier varchar(128)                         not null,
+    user_id    int                                  not null,
+    expiration timestamp                            null,
+    revoked    tinyint(1) default 0                 not null,
+    created_at timestamp  default CURRENT_TIMESTAMP not null,
+    updated_at timestamp                            null
+) charset = utf8mb3;
+
+create table oauth_refresh_tokens
+(
+    id              int unsigned auto_increment          primary key,
+    identifier      varchar(128)                         not null,
+    access_token_id int                                  not null,
+    expiration      timestamp                            null,
+    revoked         tinyint(1) default 0                 not null,
+    created_at      timestamp  default CURRENT_TIMESTAMP not null,
+    updated_at      timestamp                            null
+) charset = utf8mb3;
+
+
+-- migrate:down
+
