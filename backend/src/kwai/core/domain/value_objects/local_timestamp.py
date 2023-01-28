1717d33b544ed48281d368db45d608fb2f0eb940
fbraem <franky.braem@gmail.com>
2023-01-28 17:57:32 +0100

    freeze

--- backend/src/kwai/core/domain/value_objects/local_timestamp.py
+++ backend/src/kwai/core/domain/value_objects/local_timestamp.py
@@ -3,7 +3,7 @@ from dataclasses import dataclass
 from datetime import datetime, timedelta
 
 
-@dataclass
+@dataclass(frozen=True)
 class LocalTimestamp:
     """A value object for a timestamp with a timezone.
 
