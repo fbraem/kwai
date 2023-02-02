56bab944a85452caa1957be11a0ddc8c25de77ad
fbraem <franky.braem@gmail.com>
2023-02-02 19:24:50 +0100

    test: add test for access_token api

--- backend/src/tests/api/v1/auth/endpoints/test_login.py
+++ backend/src/tests/api/v1/auth/endpoints/test_login.py
@@ -8,7 +8,7 @@ from kwai.modules.identity.users.user_account import UserAccountEntity
 pytestmark = pytest.mark.integration
 
 
-def test_login(user_account: UserAccountEntity, client: TestClient):
+def test_login(client: TestClient, user_account: UserAccountEntity):
     """Test the login api."""
     response = client.post(
         "/api/v1/auth/login",
@@ -30,10 +30,31 @@ def test_login_with_unknown_user(client: TestClient):
     assert response.status_code == status.HTTP_401_UNAUTHORIZED
 
 
-def test_login_with_wrong_password(user_account: UserAccountEntity, client: TestClient):
+def test_login_with_wrong_password(client: TestClient, user_account: UserAccountEntity):
     """Test the login api with a wrong password."""
     response = client.post(
         "/api/v1/auth/login",
         data={"username": str(user_account.user.email), "password": "Test/1234"},
     )
     assert response.status_code == status.HTTP_401_UNAUTHORIZED
+
+
+def test_renew_access_token(client: TestClient, user_account: UserAccountEntity):
+    """Test the renewal of an access token."""
+
+    # First login to get an access and refresh token.
+    response = client.post(
+        "/api/v1/auth/login",
+        data={"username": str(user_account.user.email), "password": "Nage-waza/1882"},
+    )
+
+    refresh_token = response.json()["refresh_token"]
+    response = client.post(
+        "/api/v1/auth/access_token", data={"refresh_token": refresh_token}
+    )
+
+    assert response.status_code == status.HTTP_200_OK
+    json = response.json()
+    assert "access_token" in json
+    assert "refresh_token" in json
+    assert "expiration" in json
