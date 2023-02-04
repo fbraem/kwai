a9659d8f5c8aa861f2d1bce07e0f38a4677296b0
fbraem <franky.braem@gmail.com>
2023-02-04 21:09:25 +0100

    ci: add -e

--- backend/docs/build.sh
+++ backend/docs/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 rm -rf ./kwai
 rm -rf ./tests
-poetry -C ../src run sphinx-apidoc -o ./kwai ../src/kwai
-poetry -C ../src run sphinx-apidoc -o ./tests ../src/tests
+poetry -C ../src run sphinx-apidoc -e -o ./kwai ../src/kwai
+poetry -C ../src run sphinx-apidoc -e -o ./tests ../src/tests
 make html
