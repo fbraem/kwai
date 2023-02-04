3bf1a564b081f001bf405062878ba079375c1ca3
fbraem <franky.braem@gmail.com>
2023-02-04 20:51:02 +0100

    fix: move code to create_bus and protect with __name__ check

--- backend/src/tasks.py
+++ backend/src/tasks.py
@@ -10,45 +10,6 @@ from kwai.core.dependencies import container
 from kwai.core.events.dramatiq_bus import DramatiqBus
 from kwai.core.settings import Settings, SettingsException
 
-try:
-    settings = container[Settings]
-except SettingsException as se:
-    logger.error(f"Could not load settings: {se}")
-    sys.exit(0)
-
-if settings.broker.logger:
-    logger.remove(0)  # Remove the default logger
-
-    def log_format(record):
-        """Creates a logging format for a message."""
-        if (
-            "message_id" in record["extra"]
-            and "actor_name" in record["extra"]
-            and "queue_name" in record["extra"]
-        ):
-            return (
-                "{time} - {level} - "
-                "({extra[message_id]} - {extra[actor_name]} - {extra[queue_name]}) - "
-                "{message}" + os.linesep
-            )
-        return "{time} - {level} - {message}" + os.linesep
-
-    logger.add(
-        settings.broker.logger.file or sys.stderr,
-        format=log_format,
-        level=settings.broker.logger.level,
-        colorize=True,
-        retention=settings.broker.logger.retention,
-        rotation=settings.broker.logger.rotation,
-    )
-
-
-broker = dramatiq.brokers.rabbitmq.RabbitmqBroker(
-    url=settings.broker.url + "/" + settings.broker.name
-)
-broker.add_middleware(CurrentMessage())
-dramatiq.set_broker(broker)
-
 
 def create_bus():
     """Wrap the creation of the bus in this method.
@@ -56,13 +17,57 @@ def create_bus():
     :remark: We must be sure that the @actor decorators are run after setting the
     broker. That's why all actors are imported here.
     """
+    try:
+        settings = container[Settings]
+    except SettingsException as ex:
+        logger.error(f"Could not load settings: {ex}")
+        sys.exit(0)
+
+    if settings.broker.logger:
+        logger.remove(0)  # Remove the default logger
+
+        def log_format(record):
+            """Create a logging format for a message."""
+            if (
+                "message_id" in record["extra"]
+                and "actor_name" in record["extra"]
+                and "queue_name" in record["extra"]
+            ):
+                return (
+                    "{time} - {level} - "
+                    "({extra[message_id]} - "
+                    "{extra[actor_name]} - "
+                    "{extra[queue_name]}) - "
+                    "{message}" + os.linesep
+                )
+            return "{time} - {level} - {message}" + os.linesep
+
+        logger.add(
+            settings.broker.logger.file or sys.stderr,
+            format=log_format,
+            level=settings.broker.logger.level,
+            colorize=True,
+            retention=settings.broker.logger.retention,
+            rotation=settings.broker.logger.rotation,
+        )
+
+    broker = dramatiq.brokers.rabbitmq.RabbitmqBroker(
+        url=settings.broker.url + "/" + settings.broker.name
+    )
+    broker.add_middleware(CurrentMessage())
+    dramatiq.set_broker(broker)
+
     bus = DramatiqBus()
 
+    # pylint: disable=(import-outside-toplevel)
     from kwai.modules.identity.tasks import tasks as identity_tasks
 
     # Subscribe all identity tasks to their event
-    [bus.subscribe(event, task_fn) for event, task_fn in identity_tasks.items()]
+    for event, task_fn in identity_tasks.items():
+        bus.subscribe(event, task_fn)
+
     return bus
 
 
-create_bus()
+if __name__ == "__main__":
+    create_bus()
