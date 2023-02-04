0ce50e62a87a43237bd6f6e81ed59ac01fa890ee
fbraem <franky.braem@gmail.com>
2023-02-04 21:58:18 +0100

    Add sphinx.ext.napoleon extension

--- backend/docs/conf.py
+++ backend/docs/conf.py
@@ -16,7 +16,7 @@ author = "Franky Braem"
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ["sphinx.ext.autodoc", "sphinx.ext.viewcode"]
+extensions = ["sphinx.ext.autodoc", "sphinx.ext.viewcode", "sphinx.ext.napoleon"]
 
 templates_path = ["_templates"]
 exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
