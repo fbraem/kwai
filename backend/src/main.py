79a1443fd33e9715eaed983b9360d37a9d955abb
fbraem <franky.braem@gmail.com>
2023-02-04 15:43:35 +0100

    fix: protect execution of code with check __main__

--- backend/src/main.py
+++ backend/src/main.py
@@ -8,13 +8,11 @@ from kwai.app import create_app
 from kwai.core.dependencies import container
 from kwai.core.settings import Settings, SettingsException
 
-try:
-    settings = container[Settings]
-except SettingsException as se:
-    logger.error(f"Could not load settings: {se}")
-    sys.exit(0)
-
 if __name__ == "__main__":
+    try:
+        settings = container[Settings]
+    except SettingsException as se:
+        logger.error(f"Could not load settings: {se}")
+        sys.exit(0)
+
     uvicorn.run(create_app(settings), host="0.0.0.0", port=8000)
-else:
-    app = create_app(settings)
