a499452d24e49ca0ad74a4079dc50757cbe4d22e
fbraem <franky.braem@gmail.com>
2023-02-08 19:38:01 +0100

    feat: use class for decorator instead of function

--- backend/src/kwai/core/db/table.py
+++ backend/src/kwai/core/db/table.py
@@ -3,48 +3,66 @@
 from dataclasses import fields, is_dataclass
 from typing import Any
 
-from sql_smith.functions import alias, field as sql_field
+from sql_smith.functions import alias
+from sql_smith.functions import field as sql_field
 
 
-def table(name: str):
+# noinspection PyPep8Naming
+class table:  # pylint: disable=invalid-name, too-few-public-methods
     """A decorator that will add methods to a dataclass for handling database tables.
 
     The table name will be stored in the __table_name__ class variable.
     """
 
-    def decorator(cls):
-        if not is_dataclass(cls):
-            raise RuntimeError("The table decorator expects a dataclass")
+    def __init__(self, name: str):
+        self._table_name = name
 
-        def aliases(table_name=name):
-            """Returns aliases for all fields of the dataclass."""
-            return [
-                alias(table_name + "." + prop.name, table_name + "_" + prop.name)
-                for prop in fields(cls)
-            ]
+    def __call__(self, data_class: Any):
+        """Wrapping the dataclass with Table class."""
+        assert is_dataclass(data_class)
 
-        def column(column_name: str) -> str:
-            return name + "." + column_name
+        class Table(data_class):
+            """A class that represents a table record."""
 
-        def field(column_name: str):
-            return sql_field(column(column_name))
+            __table_name__: str = self._table_name
 
-        def map_row(row: dict[str, Any], table_name=name) -> cls:
-            """Maps the data of a row to the dataclass."""
-            table_alias = table_name + "_"
-            # First, only select the values that belong to this table.
-            filtered = {
-                k.removeprefix(table_alias): v
-                for (k, v) in row.items()
-                if k.startswith(table_alias)
-            }
-            return cls(**filtered)
+            @classmethod
+            def aliases(
+                cls, table_name=self._table_name
+            ):  # pylint: disable=protected-access
+                """Return aliases for all fields of the dataclass."""
+                return [
+                    alias(table_name + "." + prop.name, table_name + "_" + prop.name)
+                    for prop in fields(cls)
+                ]
 
-        cls.aliases = aliases
-        cls.column = column
-        cls.field = field
-        cls.map_row = map_row
-        cls.__table_name__ = name
-        return cls
+            @classmethod
+            def column(cls, column_name: str) -> str:
+                """Return column as <table>.<column>."""
+                return cls.__table_name__ + "." + column_name
 
-    return decorator
+            @classmethod
+            def field(cls, column_name: str):
+                """Call sql-smith field with the given column.
+
+                short-cut for: field(table.__table__name__ + '.' + column_name)
+                """
+                return sql_field(cls.column(column_name))
+
+            @classmethod
+            def map_row(
+                cls,
+                row: dict[str, Any],
+                table_name=self._table_name,  # pylint: disable=protected-access
+            ) -> data_class:
+                """Map the data of a row to the dataclass."""
+                table_alias = table_name + "_"
+                # First, only select the values that belong to this table.
+                filtered = {
+                    k.removeprefix(table_alias): v
+                    for (k, v) in row.items()
+                    if k.startswith(table_alias)
+                }
+                return data_class(**filtered)
+
+        return Table
