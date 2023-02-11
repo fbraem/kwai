44933679bca1cdcb5da06448c69866a395b9662c
fbraem <franky.braem@gmail.com>
2023-02-11 16:45:48 +0100

    docs: add DatabaseQuery

--- backend/src/kwai/core/db/__init__.py
+++ backend/src/kwai/core/db/__init__.py
@@ -4,5 +4,8 @@ ORM (Object-relational mapping) software (like SQLAlchemy) is not used. Instead,
 repository pattern is used. A repository is responsible for saving, creating, querying
 and deleting entities. [sql-smith][sql-smith] is used to build queries.
 
+The [DatabaseQuery][kwai.core.db.database_query.DatabaseQuery] class can be used to
+implement the Query interface.
+
 [sql-smith]: https://github.com/fbraem/sql-smith
 """
