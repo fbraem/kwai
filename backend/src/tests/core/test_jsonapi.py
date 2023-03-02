e9d2d4ebc93409bbc5b6af95c5b359012de1c313
fbraem <franky.braem@gmail.com>
2023-03-02 19:28:32 +0100

    refactor: rename jsonapi into json_api

--- backend/src/tests/core/test_jsonapi.py
+++ backend/src/tests/core/test_jsonapi.py
@@ -4,10 +4,10 @@ from dataclasses import dataclass
 
 from pydantic import BaseModel, Field
 
-from kwai.core import jsonapi
+from kwai.core import json_api
 
 
-@jsonapi.resource(type_="members")
+@json_api.resource(type_="members")
 @dataclass
 class Member:
     """A member."""
@@ -15,13 +15,13 @@ class Member:
     id: int
     name: str
 
-    @jsonapi.attribute(name="name")
+    @json_api.attribute(name="name")
     def get_name(self):
         """Return the attribute 'name'."""
         return self.name
 
 
-@jsonapi.resource(type_="teams")
+@json_api.resource(type_="teams")
 class Team(BaseModel):
     """A team."""
 
@@ -29,18 +29,18 @@ class Team(BaseModel):
     name: str
     members: list[Member] = Field(default_factory=list)
 
-    @jsonapi.attribute(name="name")
+    @json_api.attribute(name="name")
     def get_name(self) -> str:
         """Return the attribute 'name'."""
         return self.name
 
-    @jsonapi.relationship(name="members")
+    @json_api.relationship(name="members")
     def get_members(self) -> list[Member]:
         """Return the relationship 'members'."""
         return self.members
 
 
-@jsonapi.resource(type_="coaches")
+@json_api.resource(type_="coaches")
 class Coach:
     """A coach."""
 
@@ -56,17 +56,17 @@ class Coach:
         """Return the id."""
         return self._id
 
-    @jsonapi.attribute()
+    @json_api.attribute()
     def name(self) -> str:
         """Return the attribute 'name'."""
         return self._name
 
-    @jsonapi.attribute(name="year_of_birth")
+    @json_api.attribute(name="year_of_birth")
     def year_of_birth(self) -> int:
         """Return the attribute 'year_of_birth'."""
         return self._year_of_birth
 
-    @jsonapi.relationship()
+    @json_api.relationship()
     def team(self) -> Team | None:
         """Return the relationship 'team'."""
         return self._team
@@ -76,32 +76,32 @@ def test_jsonapi_common_class():
     """Test with a Python class."""
     coach = Coach(id_=1, name="Jigoro Kano", year_of_birth=1882)
 
-    doc = jsonapi.Document(coach)
-    json_api = json.loads(doc.serialize())
-    assert json_api["data"]["type"] == "coaches"
-    assert json_api["data"]["id"] == "1"
-    assert json_api["data"]["attributes"]["name"] == "Jigoro Kano"
-    assert json_api["data"]["attributes"]["year_of_birth"] == 1882
+    doc = json_api.Document(coach)
+    json_api_response = json.loads(doc.serialize())
+    assert json_api_response["data"]["type"] == "coaches"
+    assert json_api_response["data"]["id"] == "1"
+    assert json_api_response["data"]["attributes"]["name"] == "Jigoro Kano"
+    assert json_api_response["data"]["attributes"]["year_of_birth"] == 1882
 
 
 def test_jsonapi_pydantic():
     """Test with a Pydantic class."""
     team = Team(id=1, name="U15")
-    doc = jsonapi.Document(team)
-    json_api = json.loads(doc.serialize())
-    assert json_api["data"]["type"] == "teams"
-    assert json_api["data"]["id"] == "1"
-    assert json_api["data"]["attributes"]["name"] == "U15"
+    doc = json_api.Document(team)
+    json_api_response = json.loads(doc.serialize())
+    assert json_api_response["data"]["type"] == "teams"
+    assert json_api_response["data"]["id"] == "1"
+    assert json_api_response["data"]["attributes"]["name"] == "U15"
 
 
 def test_jsonapi_dataclass():
     """Test with a dataclass class."""
     member = Member(id=1, name="Kyuzo Mifune")
-    doc = jsonapi.Document(member)
-    json_api = json.loads(doc.serialize())
-    assert json_api["data"]["type"] == "members"
-    assert json_api["data"]["id"] == "1"
-    assert json_api["data"]["attributes"]["name"] == "Kyuzo Mifune"
+    doc = json_api.Document(member)
+    json_api_response = json.loads(doc.serialize())
+    assert json_api_response["data"]["type"] == "members"
+    assert json_api_response["data"]["id"] == "1"
+    assert json_api_response["data"]["attributes"]["name"] == "Kyuzo Mifune"
 
 
 def test_jsonapi_relationship():
@@ -110,12 +110,12 @@ def test_jsonapi_relationship():
         id_=1, name="Jigoro Kano", year_of_birth=1882, team=Team(id=1, name="U15")
     )
 
-    doc = jsonapi.Document(coach)
-    json_api = json.loads(doc.serialize())
-    assert json_api["data"]["type"] == "coaches"
-    assert json_api["data"]["id"] == "1"
-    assert json_api["data"]["attributes"]["name"] == "Jigoro Kano"
-    assert json_api["data"]["relationships"]["team"] == {
+    doc = json_api.Document(coach)
+    json_api_response = json.loads(doc.serialize())
+    assert json_api_response["data"]["type"] == "coaches"
+    assert json_api_response["data"]["id"] == "1"
+    assert json_api_response["data"]["attributes"]["name"] == "Jigoro Kano"
+    assert json_api_response["data"]["relationships"]["team"] == {
         "data": {"type": "teams", "id": "1"}
     }
-    assert "included" in json_api
+    assert "included" in json_api_response
