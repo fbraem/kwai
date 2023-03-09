0a10cee6e5e6e0f728f5fe8e90e76bb32c662b32
fbraem <franky.braem@gmail.com>
2023-03-09 19:28:34 +0100

    feat: add secure_client

--- backend/src/tests/api/conftest.py
+++ backend/src/tests/api/conftest.py
@@ -4,10 +4,27 @@ from fastapi.testclient import TestClient
 
 from kwai.app import create_app
 from kwai.core.settings import get_settings
+from kwai.modules.identity.users.user_account import UserAccountEntity
 
 
-@pytest.fixture(scope="session")
+# pylint: disable=redefined-outer-name
+
+
+@pytest.fixture(scope="module")
 def client() -> TestClient:
     """Get an HTTP client."""
     app = create_app(get_settings())
     return TestClient(app)
+
+
+@pytest.fixture(scope="module")
+def secure_client(client: TestClient, user_account: UserAccountEntity) -> TestClient:
+    """Return a test client that is logged-in."""
+    response = client.post(
+        "/api/v1/auth/login",
+        data={"username": str(user_account.user.email), "password": "Nage-waza/1882"},
+    )
+    access_token = response.json()["access_token"]
+    client.headers["Authorization"] = f"Bearer {access_token}"
+
+    return client
