b2a19917a980d035520ca3c8de3655cea6b7ee64
fbraem <franky.braem@gmail.com>
2023-04-08 21:47:48 +0200

    test: add test for UserInvitationMailer

--- backend/src/tests/modules/identity/conftest.py
+++ backend/src/tests/modules/identity/conftest.py
@@ -1,8 +1,8 @@
 """Module that defines fixtures for user recovery testing."""
 import pytest
 
-from kwai.core.template.template_engine import TemplateEngine
 from kwai.core.template.mail_template import MailTemplate
+from kwai.core.template.template_engine import TemplateEngine
 
 
 @pytest.fixture(scope="module")
@@ -11,4 +11,13 @@ def recovery_mail_template(template_engine: TemplateEngine) -> MailTemplate:
     return MailTemplate(
         template_engine.create("identity/recover_html"),
         template_engine.create("identity/recover_txt"),
+    )
+
+
+@pytest.fixture(scope="module")
+def user_invitation_mail_template(template_engine: TemplateEngine) -> MailTemplate:
+    """Returns a template for the user invitation mail."""
+    return MailTemplate(
+        template_engine.create("identity/invitation_html"),
+        template_engine.create("identity/invitation_txt"),
     )
---
+++ backend/src/tests/modules/identity/user_invitations/test_user_invitation_mailer.py
@@ -0,0 +1,37 @@
+import pytest
+
+from kwai.core.domain.value_objects.email_address import EmailAddress
+from kwai.core.domain.value_objects.name import Name
+from kwai.core.mail.mailer import Mailer
+from kwai.core.mail.recipient import Recipients
+from kwai.core.template.mail_template import MailTemplate
+from kwai.modules.identity.user_invitations.user_invitation import UserInvitationEntity
+from kwai.modules.identity.user_invitations.user_invitation_mailer import (
+    UserInvitationMailer,
+)
+from kwai.modules.identity.users.user import UserEntity
+
+pytestmark = pytest.mark.integration
+
+
+@pytest.fixture(scope="module")
+def user_invitation(user: UserEntity) -> UserInvitationEntity:
+    return UserInvitationEntity(
+        email=EmailAddress("ichiro.abe@kwai.com"),
+        name=Name(first_name="Ichiro", last_name="Abe"),
+        user=user,
+    )
+
+
+def test_user_recovery_mailer_sent(
+    mailer: Mailer,
+    recipients: Recipients,
+    user_invitation_mail_template: MailTemplate,
+    user_invitation: UserInvitationEntity,
+):
+    """Test user invitation mailer."""
+    mail = UserInvitationMailer(
+        mailer, recipients, user_invitation_mail_template, user_invitation
+    ).send()
+
+    assert mail, "There should be an email."
