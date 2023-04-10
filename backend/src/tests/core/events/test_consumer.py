6ef9cb89f791603961760cec5e752fa018862822
fbraem <franky.braem@gmail.com>
2023-04-10 14:58:48 +0200

    fix: use different group-name to avoid collision with stream test

--- backend/src/tests/core/events/test_consumer.py
+++ backend/src/tests/core/events/test_consumer.py
@@ -10,18 +10,18 @@ from kwai.core.events.stream import RedisMessage, RedisStream
 @pytest.mark.asyncio
 async def test_consumer(stream: RedisStream):
     """Test the consumer."""
-    await stream.create_group("kwai_test_group")
+    await stream.create_group("kwai_test_consumer_group")
     await stream.add(RedisMessage(data={"text": "Hello consumer!"}))
 
     def out(message: RedisMessage) -> bool:  # pylint: disable=unused-argument
         out.counter = getattr(out, "counter", 0) + 1
         return True
 
-    consumer = RedisConsumer(stream, "kwai_test_group", out)
+    consumer = RedisConsumer(stream, "kwai_test_consumer_group", out)
     task = asyncio.create_task(consumer.consume("kwai_test_consumer"))
     await stream.add(RedisMessage(data={"text": "Hello runnable consumer!"}))
     try:
-        await asyncio.wait_for(task, 2)
+        await asyncio.wait_for(task, 4)
     except asyncio.TimeoutError:
         pass
 
