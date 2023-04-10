3bbd4ec64bc3205ef02358ecd2d074da98055458
fbraem <franky.braem@gmail.com>
2023-04-10 17:12:56 +0200

    fix: set default for remark

--- backend/src/kwai/api/schemas/user_invitation.py
+++ backend/src/kwai/api/schemas/user_invitation.py
@@ -19,7 +19,7 @@ class UserInvitationAttributes(BaseModel):
     email: str
     first_name: str
     last_name: str
-    remark: str
+    remark: str = ""
     expired_at: str | None = None
     confirmed_at: str | None = None
     created_at: str | None = None
