ee4291d7a67be0f85c2a19e5c37a43f4fe0b3776
fbraem <franky.braem@gmail.com>
2023-04-10 21:06:15 +0200

    feat: add get user invitation use case

---
+++ backend/src/kwai/modules/identity/get_user_invitation.py
@@ -0,0 +1,53 @@
+"""Implement the use case: get a user invitation."""
+from dataclasses import dataclass
+
+from kwai.core.domain.value_objects.unique_id import UniqueId
+from kwai.modules.identity.user_invitations.user_invitation import UserInvitationEntity
+from kwai.modules.identity.user_invitations.user_invitation_repository import (
+    UserInvitationRepository,
+)
+
+
+# pylint: disable=too-few-public-methods
+
+
+@dataclass(kw_only=True, frozen=True, slots=True)
+class GetUserInvitationCommand:
+    """Input for the use case
+    [GetUserInvitation][kwai.modules.identity.get_user_invitation.GetUserInvitation]
+
+    Attributes:
+        uuid: The unique id of the user invitation.
+    """
+
+    uuid: str
+
+
+class GetUserInvitation:
+    """Implements the use case 'get a user invitation'."""
+
+    def __init__(self, user_invitation_repo: UserInvitationRepository):
+        """Initialize the use case.
+
+        Args:
+            user_invitation_repo: A repository for getting the user invitation.
+        """
+        self._user_invitation_repo = user_invitation_repo
+
+    def execute(self, command: GetUserInvitationCommand) -> UserInvitationEntity:
+        """Execute the use case.
+
+        Args:
+            command: The input for this use case.
+
+        Returns:
+            A user invitation entity
+
+        Raises:
+            UserInvitationNotFoundException: Raised when then invitation is not found.
+        """
+
+        entity = self._user_invitation_repo.get_invitation_by_uuid(
+            UniqueId.create_from_string(command.uuid)
+        )
+        return entity
---
+++ backend/src/tests/modules/identity/test_get_user_invitation.py
@@ -0,0 +1,52 @@
+"""Module for testing the use case GetUserInvitation."""
+
+import pytest
+
+from kwai.core.db.database import Database
+from kwai.core.domain.value_objects.email_address import EmailAddress
+from kwai.core.domain.value_objects.name import Name
+from kwai.modules.identity.get_user_invitation import (
+    GetUserInvitationCommand,
+    GetUserInvitation,
+)
+from kwai.modules.identity.user_invitations.user_invitation import UserInvitationEntity
+from kwai.modules.identity.user_invitations.user_invitation_db_repository import (
+    InvitationDbRepository,
+)
+from kwai.modules.identity.user_invitations.user_invitation_repository import (
+    UserInvitationRepository,
+)
+from kwai.modules.identity.users.user import UserEntity
+
+
+@pytest.fixture(scope="module")
+def user_invitation(
+    repo: UserInvitationRepository, user: UserEntity
+) -> UserInvitationEntity:
+    """Fixture for a user invitation."""
+    invitation = UserInvitationEntity(
+        email=EmailAddress("ichiro.abe@kwai.com"),
+        name=Name(first_name="Ichiro", last_name="Abe"),
+        remark="Created with pytest",
+        user=user,
+    )
+    yield repo.create(invitation)
+    repo.delete(invitation)
+
+
+@pytest.fixture(scope="module")
+def repo(database: Database) -> UserInvitationRepository:
+    """Create a user invitation repository."""
+    return InvitationDbRepository(database)
+
+
+def test_get_invitations(
+    repo: UserInvitationRepository, user_invitation: UserInvitationEntity
+):
+    """Test the use case: get user invitation."""
+    command = GetUserInvitationCommand(uuid=str(user_invitation.uuid))
+    result = GetUserInvitation(repo).execute(command)
+
+    assert (
+        result.uuid == user_invitation.uuid
+    ), "The invitation must have the same uuid."
