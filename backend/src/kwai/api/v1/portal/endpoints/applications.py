aa0746a576002420c675beef8ab3eb07d2ae5b04
fbraem <franky.braem@gmail.com>
2023-04-28 16:53:08 +0200

    feat(api): implement get portal/applications

--- backend/src/kwai/api/v1/portal/endpoints/applications.py
+++ backend/src/kwai/api/v1/portal/endpoints/applications.py
@@ -2,16 +2,63 @@
 from fastapi import APIRouter
 
 from kwai.api.dependencies import deps
-from kwai.api.schemas.application import ApplicationsDocument, ApplicationData
+from kwai.api.schemas.application import (
+    ApplicationsDocument,
+    ApplicationData,
+    ApplicationAttributes,
+)
 from kwai.api.schemas.jsonapi import Meta
 from kwai.core.db.database import Database
+from kwai.modules.portal.applications.application import ApplicationEntity
+from kwai.modules.portal.applications.application_db_repository import (
+    ApplicationDbRepository,
+)
+from kwai.modules.portal.get_applications import GetApplicationsCommand, GetApplications
 
 router = APIRouter()
 
 
-@router.get("/applications")
-def get_applications(db=deps.depends(Database)) -> ApplicationsDocument:
-    """Get all applications of kwai."""
-    result: list[ApplicationData] = []
+def _create_application_data(application: ApplicationEntity) -> ApplicationData:
+    """Transform an application entity into a JSONAPI resource.
 
-    return ApplicationsDocument(meta=Meta(count=0), data=result)
+    Args:
+        application: An application entity.
+
+    Returns:
+        A JSONAPI structure for an application resource.
+    """
+    return ApplicationData(
+        id=str(application.id),
+        attributes=ApplicationAttributes(
+            name=application.name,
+            title=application.title,
+            description=application.description,
+            short_description=application.short_description,
+            remark=application.remark,
+            news=application.can_contain_news,
+            pages=application.can_contain_pages,
+            events=application.can_contain_events,
+            weight=application.weight,
+            created_at=str(application.traceable_time.created_at),
+            updated_at=(
+                None
+                if application.traceable_time.updated_at.empty
+                else str(application.traceable_time.updated_at)
+            ),
+        ),
+    )
+
+
+@router.get("/applications")
+async def get_applications(db=deps.depends(Database)) -> ApplicationsDocument:
+    """Get all applications of kwai."""
+    command = GetApplicationsCommand()
+    count, applications = await GetApplications(ApplicationDbRepository(db)).execute(
+        command
+    )
+
+    result: list[ApplicationData] = []
+    async for application in applications:
+        result.append(_create_application_data(application))
+
+    return ApplicationsDocument(meta=Meta(count=count), data=result)
