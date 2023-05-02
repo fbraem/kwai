6f5c19d02d3c35310c557aa68153cd370ca7b00d
fbraem <franky.braem@gmail.com>
2023-05-02 20:54:41 +0200

    feat: add news tables

--- backend/migrations/20230128205126_start.sql
+++ backend/migrations/20230128205126_start.sql
@@ -91,6 +91,38 @@ create table if not exists applications
   pages                 tinyint(1) default 1                 not null,
   events                tinyint(1) default 1                 not null,
   weight                int default 0                        not null
-)
+);
 
+create table if not exists news_stories (
+  id                    int unsigned auto_increment          primary key,
+  enabled               tinyint(1) default 0                 not null,
+  promotion             int default 0                        not null,
+  promotion_end_date    datetime                             null,
+  publish_date          datetime                             not null,
+  timezone              varchar(255)                         not null,
+  end_date              datetime                             null,
+  remark                text                                 null,
+  application_id        int unsigned                         not null,
+  created_at            datetime default current_timestamp   not null,
+  updated_at            datetime                             null
+);
+
+-- In kwai_api the table for news_contents was called news_contents_2,
+create table if not exists news_contents_2 (
+  news_id               int unsigned                         not null,
+  locale                varchar(255)                         not null,
+  format                varchar(255)                         not null,
+  title                 varchar(255)                         not null,
+  content               text,
+  summary               text                                 not null,
+  user_id               int                                  not null,
+  created_at            datetime default CURRENT_TIMESTAMP   not null,
+  updated_at            datetime                             null,
+  primary key (news_id, locale)
+);
+-- Move news_contents_2 back to news_contents
+drop table if exists news_contents;
+CREATE TABLE news_contents LIKE news_contents_2;
+INSERT INTO news_contents SELECT * FROM news_contents_2;
+drop table if exists news_contents_2;
 -- migrate:down
