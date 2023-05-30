04baa184c808e0115f127373b6f4ce916ad96351
fbraem <franky.braem@gmail.com>
2023-05-30 20:28:17 +0200

    feat: include news router

--- backend/src/kwai/api/v1/portal/api.py
+++ backend/src/kwai/api/v1/portal/api.py
@@ -1,7 +1,8 @@
 """Module that defines the portal api."""
 from fastapi import APIRouter
 
-from kwai.api.v1.portal.endpoints import applications
+from kwai.api.v1.portal.endpoints import applications, news
 
 api_router = APIRouter(prefix="/portal")
 api_router.include_router(applications.router, tags=["portal/applications"])
+api_router.include_router(news.router, tags=["portal/news"])
