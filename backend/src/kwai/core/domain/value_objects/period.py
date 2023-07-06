1d7eccc730eb179d95b1e08e12638c23ec8cd893
fbraem <franky.braem@gmail.com>
2023-07-06 21:00:55 +0200

    feat: add get_delta and check for a valid period on initialization

--- backend/src/kwai/core/domain/value_objects/period.py
+++ backend/src/kwai/core/domain/value_objects/period.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass, field
+from datetime import timedelta
 
 from kwai.core.domain.value_objects.local_timestamp import LocalTimestamp
 
@@ -9,3 +10,16 @@ class Period:
 
     start_date: LocalTimestamp = field(default_factory=LocalTimestamp.create_now)
     end_date: LocalTimestamp = field(default_factory=LocalTimestamp)
+
+    def __post_init__(self):
+        """Perform post initialization.
+
+        Raises:
+            ValueError: when the start date is before the end date.
+        """
+        if self.start_date.timestamp > self.end_date.timestamp:
+            raise ValueError("start_date should be before end_date")
+
+    def get_delta(self) -> timedelta:
+        """Return the delta between end and start time."""
+        return self.end_date.timestamp - self.start_date.timestamp
