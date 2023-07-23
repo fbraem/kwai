db1c7ff4e7c9caff158bc525a617f34ef6a1e7e0
fbraem <franky.braem@gmail.com>
2023-07-23 10:55:02 +0200

    feat: add endless

--- backend/src/kwai/core/domain/value_objects/time_period.py
+++ backend/src/kwai/core/domain/value_objects/time_period.py
@@ -9,7 +9,7 @@ class TimePeriod:
     """A period between two times."""
 
     start: time
-    end: time
+    end: time | None
 
     def __post_init__(self):
         """Perform post initialization.
@@ -17,12 +17,26 @@ class TimePeriod:
         Raises:
             ValueError: when the start time is before the end time.
         """
-        if self.start > self.end:
-            raise ValueError("start should be before end")
+        if self.end is not None:
+            if self.start > self.end:
+                raise ValueError("start should be before end")
 
     @property
-    def delta(self) -> timedelta:
-        """Returns the delta between end and start time."""
+    def endless(self) -> bool:
+        """Return True when the period does not have an end time."""
+        return self.end is None
+
+    @property
+    def delta(self) -> timedelta | None:
+        """Return the delta between end and start time.
+
+        Returns: The delta between the start and end time.
+
+        When there is no end time, the result will be None.
+        """
+        if self.end is None:
+            return None
+
         return datetime.combine(date(1, 1, 1), self.end) - datetime.combine(
             date(1, 1, 1), self.start
         )
