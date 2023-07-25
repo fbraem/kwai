41b1ddf7a964f8b143391f39171b2c2dc94c2fb6
fbraem <franky.braem@gmail.com>
2023-07-25 22:03:20 +0200

    feat: add Text value object

--- backend/src/kwai/core/domain/value_objects/content.py
+++ backend/src/kwai/core/domain/value_objects/content.py
@@ -26,3 +26,97 @@ class Content:
     summary: str
     author: Owner
     traceable_time: TraceableTime = field(default_factory=TraceableTime)
+
+
+class Text:
+    """A value object containing content in several locales.
+
+    This class is immutable. A new Text instance will be created and returned when
+    content is added, changed or removed.
+    """
+
+    def __init__(self, content: dict[str, Content] = None):
+        """Initialize the text value object with content.
+
+        Args:
+            content: A dictionary with content and locale as key.
+        """
+        self._content = {} if content is None else content.copy()
+
+    def contains_translation(self, locale: str) -> bool:
+        """Check if the given locale is available as translation.
+
+        Returns:
+            True when the translation is available.
+        """
+        return locale in self._content
+
+    def get_translation(self, locale: str) -> Content:
+        """Get a translation.
+
+        Args:
+            locale: The locale of the content
+
+        Returns:
+            The content, when available.
+
+        Raises:
+            KeyError when the locale is not available.
+        """
+        if locale in self._content:
+            return self._content[locale]
+        raise KeyError(f"{locale} is not found.")
+
+    def add_translation(self, content: Content) -> "Text":
+        """Add a new translation.
+
+        Args:
+            content: The translated content.
+
+        Returns:
+            A new Text value object that also contains the translated content.
+
+        Raises:
+            KeyError: when a translation already existed.
+        """
+        if content.locale not in self._content:
+            return Text(self._content | {content.locale: content})
+
+        raise KeyError(f"{content.locale} was already used.")
+
+    def remove_translation(self, content: Content) -> "Text":
+        """Remove a translation.
+
+        Args:
+            content: The content to remove.
+
+        Returns:
+            A new Text value object that also contains the rest of the translated
+            content.
+
+        Raises:
+            KeyError: when the translation does not exist.
+        """
+        if content.locale not in self._content:
+            raise KeyError(f"{content.locale} is not found.")
+        new_dict = self._content.copy()
+        new_dict.pop(content.locale)
+        return Text(new_dict)
+
+    def replace_translation(self, content: Content):
+        """Replace a translation.
+
+        Args:
+            content: The content to remove.
+
+        Returns:
+            A new Text value object that also contains the rest of the translated
+            content.
+
+        Raises:
+            KeyError: when the translation does not exist.
+
+        """
+        if content.locale not in self._content:
+            raise KeyError(f"{content.locale} is not found.")
+        return Text(self._content | {content.locale: content})
---
+++ backend/src/tests/core/domain/value_objects/test_text.py
@@ -0,0 +1,61 @@
+"""Module that tests the Text value object."""
+import dataclasses
+
+import pytest
+
+from kwai.core.domain.value_objects.content import Content, Text
+from kwai.core.domain.value_objects.identifier import IntIdentifier
+from kwai.core.domain.value_objects.name import Name
+from kwai.core.domain.value_objects.owner import Owner
+from kwai.core.domain.value_objects.unique_id import UniqueId
+
+
+@pytest.fixture()
+def content(locale: str):
+    """Create a fixture for Content."""
+    return Content(
+        locale=locale,
+        format="MD",
+        title="Test",
+        content="Test",
+        summary="Test",
+        author=Owner(
+            id=IntIdentifier(1),
+            uuid=UniqueId.generate(),
+            name=Name(first_name="Jigoro", last_name="Kano"),
+        ),
+    )
+
+
+@pytest.mark.parametrize("locale", ["nl"])
+def test_add(locale, request):
+    """Test the add_translation method."""
+    content = request.getfixturevalue("content")
+    text = Text().add_translation(content)
+    assert text.contains_translation(
+        locale
+    ), f"There should be a translation available for {locale}"
+
+
+@pytest.mark.parametrize("locale", ["nl"])
+def test_remove(locale, request):
+    """Test the remove_translation method."""
+    content = request.getfixturevalue("content")
+    text = Text().add_translation(content).remove_translation(content)
+    assert not text.contains_translation(
+        locale
+    ), f"There should be no translation available for {locale}"
+
+
+@pytest.mark.parametrize("locale", ["nl"])
+def test_replace(locale, request):
+    """Test the replace_translation method."""
+    content = request.getfixturevalue("content")
+    new_content = dataclasses.replace(content, content="Updated")
+    text = Text().add_translation(content).replace_translation(new_content)
+    assert text.contains_translation(
+        locale
+    ), f"There should be a translation available for {locale}"
+    assert (
+        text.get_translation(locale).content == "Updated"
+    ), "The text should be updated"
