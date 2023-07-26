c0d373586bf93489b86cc840dc4bb0ccc263981e
fbraem <franky.braem@gmail.com>
2023-07-26 19:42:59 +0200

    feat: add Coach, Team

--- backend/src/kwai/modules/training/trainings/value_objects.py
+++ backend/src/kwai/modules/training/trainings/value_objects.py
@@ -3,6 +3,7 @@
 from dataclasses import dataclass
 
 from kwai.core.domain.value_objects.identifier import IntIdentifier
+from kwai.core.domain.value_objects.name import Name
 
 
 @dataclass(kw_only=True, frozen=True, slots=True)
@@ -12,6 +13,32 @@ class Season:
     Attributes:
         id: The id of the season.
         name: The name of the season.
+    """
+
+    id: IntIdentifier
+    name: str
+
+
+@dataclass(kw_only=True, frozen=True, slots=True)
+class Coach:
+    """A coach of a training.
+
+    Attributes:
+        id: The id of the coach.
+        name: The name of the coach.
+    """
+
+    id: IntIdentifier
+    name: Name
+
+
+@dataclass(kw_only=True, frozen=True, slots=True)
+class Team:
+    """A team.
+
+    Attributes:
+        id: The id of the team.
+        name: The name of the team.
     """
 
     id: IntIdentifier
