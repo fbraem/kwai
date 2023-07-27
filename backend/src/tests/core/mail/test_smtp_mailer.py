cd1416528c7dfde2740d16cad78a18c67e37fbb3
fbraem <franky.braem@gmail.com>
2023-07-27 20:41:41 +0200

    fix: ruff recommendations

--- backend/src/tests/core/mail/test_smtp_mailer.py
+++ backend/src/tests/core/mail/test_smtp_mailer.py
@@ -6,7 +6,7 @@ import pytest
 from kwai.core.domain.value_objects.email_address import EmailAddress
 from kwai.core.mail.mail import Mail
 from kwai.core.mail.mailer import Mailer
-from kwai.core.mail.recipient import Recipients, Recipient
+from kwai.core.mail.recipient import Recipient, Recipients
 from kwai.core.settings import get_settings
 
 pytestmark = pytest.mark.integration
