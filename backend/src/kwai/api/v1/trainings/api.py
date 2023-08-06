41f183d88e76e652fb3619111359f01d5b82c33f
fbraem <franky.braem@gmail.com>
2023-08-06 22:23:53 +0200

    fix: remove prefix to avoid / at the end of the url

--- backend/src/kwai/api/v1/trainings/api.py
+++ backend/src/kwai/api/v1/trainings/api.py
@@ -4,5 +4,5 @@ from fastapi import APIRouter
 
 from kwai.api.v1.trainings.endpoints import trainings
 
-api_router = APIRouter(prefix="/trainings")
+api_router = APIRouter()
 api_router.include_router(trainings.router, tags=["trainings"])
