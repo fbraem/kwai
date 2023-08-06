a1e9782f3e797f19d8449d72b96122614df59a91
fbraem <franky.braem@gmail.com>
2023-08-06 20:20:15 +0200

    feat: add trainings endpoints

--- backend/src/kwai/api/v1/trainings/endpoints/trainings.py
+++ backend/src/kwai/api/v1/trainings/endpoints/trainings.py
@@ -1,14 +1,32 @@
 """Module that defines the trainings API."""
 
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
 
 from kwai.api.dependencies import deps
+from kwai.api.v1.trainings.schemas.training import TrainingResource
 from kwai.core.db.database import Database
+from kwai.core.json_api import Meta, PaginationModel
+from kwai.modules.training.get_trainings import GetTrainings, GetTrainingsCommand
+from kwai.modules.training.trainings.training_db_repository import TrainingDbRepository
 
-router = APIRouter(prefix="/trainings", tags=["trainings"])
+router = APIRouter(tags=["trainings"])
 
 
 @router.get("/")
-async def get_trainings(db=deps.depends(Database)):
+async def get_trainings(
+    pagination: PaginationModel = Depends(PaginationModel),
+    db=deps.depends(Database),
+) -> TrainingResource.get_document_model():
     """Get all trainings."""
-    pass
+    command = GetTrainingsCommand(offset=pagination.offset or 0, limit=pagination.limit)
+
+    count, training_iterator = await GetTrainings(TrainingDbRepository(db)).execute(
+        command
+    )
+
+    document = TrainingResource.serialize_list(
+        [TrainingResource(training) async for training in training_iterator]
+    )
+    document.meta = Meta(count=count, offset=command.offset, limit=command.limit)
+
+    return document
---
+++ backend/src/tests/api/v1/trainings/endpoints/__init__.py
@@ -0,0 +1,1 @@
+"""Package for testing the trainings endpoints."""
