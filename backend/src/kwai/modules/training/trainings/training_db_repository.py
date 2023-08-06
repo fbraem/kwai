7974d3a79505be7dbc7cb58341563d9653563eca
fbraem <franky.braem@gmail.com>
2023-08-06 20:21:09 +0200

    feat: add training repository

---
+++ backend/src/kwai/modules/training/trainings/training_db_repository.py
@@ -0,0 +1,110 @@
+"""Module for implementing a training repository for a database."""
+from typing import AsyncIterator
+
+from kwai.core.db.database import Database, Record
+from kwai.core.db.rows import OwnersTable
+from kwai.core.domain.entity import Entity
+from kwai.modules.training.trainings.training import TrainingEntity, TrainingIdentifier
+from kwai.modules.training.trainings.training_coach_db_query import TrainingCoachDbQuery
+from kwai.modules.training.trainings.training_db_query import TrainingDbQuery
+from kwai.modules.training.trainings.training_query import TrainingQuery
+from kwai.modules.training.trainings.training_repository import TrainingRepository
+from kwai.modules.training.trainings.training_tables import (
+    TrainingContentsTable,
+    TrainingDefinitionsTable,
+    TrainingsTable,
+)
+from kwai.modules.training.trainings.training_team_db_query import TrainingTeamDbQuery
+from kwai.modules.training.trainings.value_objects import Team, TrainingCoach
+
+
+def _create_entity(rows: list[Record]) -> TrainingEntity:
+    """Create a training entity from a group of rows."""
+    if rows[0][TrainingDefinitionsTable.alias_name("id")] is None:
+        definition = None
+    else:
+        definition = TrainingDefinitionsTable(rows[0]).create_entity(
+            OwnersTable(rows[0], "definition_owners").create_owner()
+        )
+    return TrainingsTable(rows[0]).create_entity(
+        [
+            TrainingContentsTable(row).create_content(OwnersTable(row).create_owner())
+            for row in rows
+        ],
+        definition=definition,
+    )
+
+
+class TrainingDbRepository(TrainingRepository):
+    """A training repository for a database."""
+
+    def __init__(self, database: Database):
+        """Initialize the repository.
+
+        Args:
+            database: The database for this repository.
+        """
+        self._database = database
+
+    def create_query(self) -> TrainingQuery:
+        return TrainingDbQuery(self._database)
+
+    async def get_all(
+        self,
+        coach_query: TrainingQuery | None = None,
+        limit: int | None = None,
+        offset: int | None = None,
+    ) -> AsyncIterator[TrainingEntity]:
+        if coach_query is None:
+            coach_query = self.create_query()
+
+        trainings: dict[TrainingIdentifier, TrainingEntity] = {}
+        group_by_column = TrainingsTable.alias_name("id")
+
+        row_it = coach_query.fetch(limit, offset)
+        # Handle the first row
+        try:
+            record = await anext(row_it)
+        except StopAsyncIteration:
+            return
+
+        group = [record]
+        current_key = record[group_by_column]
+
+        # Process all other rows
+        async for record in row_it:
+            new_key = record[group_by_column]
+            if new_key != current_key:
+                training = _create_entity(group)
+                trainings[training.id] = training
+                group = [record]
+                current_key = new_key
+            else:
+                group.append(record)
+
+        training = _create_entity(group)
+        trainings[training.id] = training
+
+        # Get the coaches of all the trainings.
+        coach_query = TrainingCoachDbQuery(self._database).filter_by_trainings(
+            *trainings.keys()
+        )
+        coaches: dict[
+            TrainingIdentifier, list[TrainingCoach]
+        ] = await coach_query.fetch_coaches()
+
+        # Get the teams of all trainings
+        team_query = TrainingTeamDbQuery(self._database).filter_by_trainings(
+            *trainings.keys()
+        )
+        teams: dict[TrainingIdentifier, list[Team]] = await team_query.fetch_teams()
+
+        for training in trainings.values():
+            training_coaches = coaches.get(training.id, [])
+            training_teams = teams.get(training.id, [])
+            if len(training_coaches) > 0 or len(training_teams) > 0:
+                yield Entity.replace(
+                    training, coaches=training_coaches, teams=training_teams
+                )
+            else:
+                yield training
---
+++ backend/src/kwai/modules/training/trainings/training_repository.py
@@ -0,0 +1,38 @@
+"""Module that defines an interface for a repository of trainings."""
+from abc import ABC, abstractmethod
+from typing import AsyncIterator
+
+from kwai.modules.training.trainings.training import TrainingEntity
+from kwai.modules.training.trainings.training_query import TrainingQuery
+
+
+class TrainingNotFoundException(Exception):
+    """Raised when a training can not be found."""
+
+
+class TrainingRepository(ABC):
+    """An interface for a repository of trainings."""
+
+    @abstractmethod
+    def create_query(self) -> TrainingQuery:
+        """Create a query for querying trainings."""
+        raise NotImplementedError
+
+    @abstractmethod
+    async def get_all(
+        self,
+        query: TrainingQuery | None = None,
+        limit: int | None = None,
+        offset: int | None = None,
+    ) -> AsyncIterator[TrainingEntity]:
+        """Return all trainings of a given query.
+
+        Args:
+            query: The query to use for selecting the rows.
+            limit: The maximum number of entities to return.
+            offset: Skip the offset rows before beginning to return entities.
+
+        Returns:
+            A list of trainings.
+        """
+        raise NotImplementedError
