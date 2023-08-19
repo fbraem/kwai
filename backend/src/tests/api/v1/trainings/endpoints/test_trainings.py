527604e846b11d4fb09c0157ac420b8cd9db210f
fbraem <franky.braem@gmail.com>
2023-08-19 20:29:50 +0200

    test: add more filter tests

--- backend/src/tests/api/v1/trainings/endpoints/test_trainings.py
+++ backend/src/tests/api/v1/trainings/endpoints/test_trainings.py
@@ -42,3 +42,48 @@ def test_get_trainings_filter_start_end(client: TestClient):
     json = response.json()
     assert "meta" in json, "There should be a meta object in the response"
     assert "data" in json, "There should be a data list in the response"
+
+
+def test_get_trainings_filter_coach(client: TestClient):
+    """Test get trainings api with a filter for a coach."""
+    response = client.get(
+        "/api/v1/trainings",
+        params={"filter[coach]": "1"},
+    )
+    assert response.status_code in [status.HTTP_200_OK, status.HTTP_404_NOT_FOUND]
+
+    json = response.json()
+    if response.status_code == status.HTTP_200_OK:
+        assert "meta" in json, "There should be a meta object in the response"
+        assert "data" in json, "There should be a data list in the response"
+    else:
+        assert "detail" in json
+
+
+def test_get_trainings_filter_active(client: TestClient):
+    """Test get trainings api with a filter for active trainings."""
+    response = client.get(
+        "/api/v1/trainings",
+        params={"filter[active]": "false"},
+    )
+    assert response.status_code in [status.HTTP_200_OK]
+
+    json = response.json()
+    assert "meta" in json, "There should be a meta object in the response"
+    assert "data" in json, "There should be a data list in the response"
+
+
+def test_get_trainings_filter_definition(client: TestClient):
+    """Test get trainings api with a filter for a definition."""
+    response = client.get(
+        "/api/v1/trainings",
+        params={"filter[definition]": "1"},
+    )
+    assert response.status_code in [status.HTTP_200_OK, status.HTTP_404_NOT_FOUND]
+
+    json = response.json()
+    if response.status_code == status.HTTP_200_OK:
+        assert "meta" in json, "There should be a meta object in the response"
+        assert "data" in json, "There should be a data list in the response"
+    else:
+        assert "detail" in json
