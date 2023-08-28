1f9233cfe1b91fe6752738994c908bdd4eb4cdf6
fbraem <franky.braem@gmail.com>
2023-08-28 20:36:05 +0200

    test: add test_create

--- backend/src/tests/modules/training/trainings/test_training_db_repository.py
+++ backend/src/tests/modules/training/trainings/test_training_db_repository.py
@@ -1,13 +1,20 @@
 """Module for testing the training database repository."""
+
 import pytest
 
 from kwai.core.db.database import Database
-from kwai.modules.training.trainings.training import TrainingIdentifier
+from kwai.core.domain.value_objects.name import Name
+from kwai.core.domain.value_objects.owner import Owner
+from kwai.core.domain.value_objects.period import Period
+from kwai.core.domain.value_objects.text import LocaleText
+from kwai.modules.training.coaches.coach import CoachEntity, CoachIdentifier
+from kwai.modules.training.trainings.training import TrainingEntity, TrainingIdentifier
 from kwai.modules.training.trainings.training_db_repository import TrainingDbRepository
 from kwai.modules.training.trainings.training_repository import (
     TrainingNotFoundException,
     TrainingRepository,
 )
+from kwai.modules.training.trainings.value_objects import TrainingCoach
 
 pytestmark = pytest.mark.db
 
@@ -16,6 +23,46 @@ pytestmark = pytest.mark.db
 def repo(database: Database) -> TrainingRepository:
     """Fixture for a training repository."""
     return TrainingDbRepository(database)
+
+
+@pytest.fixture(scope="module")
+def training(user, context) -> TrainingEntity:
+    """A fixture for a training."""
+    coach = context["coaches"][0]
+    person = context["persons"][coach.person_id]
+
+    text = [
+        LocaleText(
+            locale="en",
+            format="md",
+            title="Training U13",
+            summary="Training for U13",
+            content="",
+            author=Owner(id=user.id, uuid=user.uuid, name=user.name),
+        )
+    ]
+    return TrainingEntity(
+        content=text,
+        period=Period.create_from_delta(hours=1),
+        coaches=[
+            TrainingCoach(
+                coach=CoachEntity(
+                    id_=CoachIdentifier(coach.id),
+                    name=Name(first_name=person.firstname, last_name=person.lastname),
+                    active=True,
+                ),
+                owner=Owner(id=user.id, uuid=user.uuid, name=user.name),
+                remark="Test Training Coach",
+            )
+        ],
+    )
+
+
+@pytest.mark.asyncio
+async def test_create(repo: TrainingRepository, training: TrainingEntity):
+    """Test create a training."""
+    new_training = await repo.create(training)
+    assert new_training is not None, "There should be a training."
 
 
 @pytest.mark.asyncio
