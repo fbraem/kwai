80b646f278c142edc2a8c48cd06d286cc78477a2
fbraem <franky.braem@gmail.com>
2023-09-02 19:25:54 +0200

    feat: implement update training

---
+++ backend/src/kwai/modules/training/update_training.py
@@ -0,0 +1,123 @@
+"""Module for defining the use case "Update Training"."""
+from dataclasses import dataclass
+
+from kwai.core.domain.entity import Entity
+from kwai.core.domain.value_objects.local_timestamp import LocalTimestamp
+from kwai.core.domain.value_objects.owner import Owner
+from kwai.core.domain.value_objects.period import Period
+from kwai.core.domain.value_objects.text import LocaleText
+from kwai.modules.training.coaches.coach import CoachIdentifier
+from kwai.modules.training.coaches.coach_repository import CoachRepository
+from kwai.modules.training.teams.team import TeamIdentifier
+from kwai.modules.training.teams.team_repository import TeamRepository
+from kwai.modules.training.training_command import TrainingCommand
+from kwai.modules.training.trainings.training import TrainingEntity, TrainingIdentifier
+from kwai.modules.training.trainings.training_definition import (
+    TrainingDefinitionIdentifier,
+)
+from kwai.modules.training.trainings.training_definition_repository import (
+    TrainingDefinitionRepository,
+)
+from kwai.modules.training.trainings.training_repository import TrainingRepository
+from kwai.modules.training.trainings.value_objects import TrainingCoach
+
+
+@dataclass(kw_only=True, frozen=True, slots=True)
+class UpdateTrainingCommand(TrainingCommand):
+    """Input for the "Update Training" use case."""
+
+    id: int
+
+
+class UpdateTraining:
+    """Use case for updating a training."""
+
+    def __init__(
+        self,
+        repo: TrainingRepository,
+        definition_repo: TrainingDefinitionRepository,
+        coach_repo: CoachRepository,
+        team_repo: TeamRepository,
+        owner: Owner,
+    ):
+        """Initialize the use case.
+
+        Args:
+            repo: The repository used to create the training.
+            definition_repo: The repository for getting the training definition.
+            coach_repo: The repository for getting the coaches.
+            team_repo: The repository for getting the teams.
+            owner: The user that executes this use case.
+        """
+        self._repo = repo
+        self._definition_repo = definition_repo
+        self._coach_repo = coach_repo
+        self._team_repo = team_repo
+        self._owner = owner
+
+    async def execute(self, command: UpdateTrainingCommand) -> TrainingEntity:
+        """Executes the use case.
+
+        Args:
+            command: The input for this use case.
+
+        Raises:
+            TrainingDefinitionNotFoundException: Raised when a training definition
+                cannot be found.
+        """
+        training = await self._repo.get_by_id(TrainingIdentifier(command.id))
+
+        if command.definition:
+            definition = self._definition_repo.get_by_id(
+                TrainingDefinitionIdentifier(command.definition)
+            )
+        else:
+            definition = None
+
+        if command.teams:
+            teams = [
+                entity
+                async for entity in self._team_repo.get_by_ids(
+                    *[TeamIdentifier(team_id) for team_id in command.teams]
+                )
+            ]
+        else:
+            teams = []
+
+        if command.coaches:
+            coaches = [
+                TrainingCoach(coach=coach, owner=self._owner)
+                async for coach in self._coach_repo.get_by_ids(
+                    *[CoachIdentifier(coach.id) for coach in command.coaches]
+                )
+            ]
+        else:
+            coaches = []
+
+        training = Entity.replace(training,
+            content=[
+                LocaleText(
+                    locale=text["locale"],
+                    format=text["format"],
+                    title=text["title"],
+                    content=text["content"],
+                    summary=text["summary"],
+                    author=self._owner,
+                )
+                for text in command.text
+            ],
+            definition=definition,
+            coaches=coaches,
+            teams=teams,
+            period=Period(
+                start_date=LocalTimestamp.create_from_string(command.start_date),
+                end_date=LocalTimestamp.create_from_string(command.end_date),
+            ),
+            active=command.active,
+            cancelled=command.cancelled,
+            location=command.location,
+            remark=command.remark,
+        )
+
+        await self._repo.update(training)
+        return training
---
+++ backend/src/tests/modules/training/test_update_training.py
@@ -0,0 +1,146 @@
+"""Module for testing the use case "Update Training"."""
+import pytest
+
+from kwai.core.db.database import Database
+from kwai.core.domain.value_objects.local_timestamp import LocalTimestamp
+from kwai.core.domain.value_objects.owner import Owner
+from kwai.core.domain.value_objects.period import Period
+from kwai.core.domain.value_objects.text import LocaleText
+from kwai.modules.training.coaches.coach import CoachEntity, CoachIdentifier
+from kwai.modules.training.coaches.coach_db_repository import CoachDbRepository
+from kwai.modules.training.coaches.coach_repository import CoachRepository
+from kwai.modules.training.teams.team import TeamEntity, TeamIdentifier
+from kwai.modules.training.teams.team_db_repository import TeamDbRepository
+from kwai.modules.training.teams.team_repository import TeamRepository
+from kwai.modules.training.training_command import Coach
+from kwai.modules.training.trainings.training import TrainingEntity
+from kwai.modules.training.trainings.training_db_repository import TrainingDbRepository
+from kwai.modules.training.trainings.training_definition_db_repository import (
+    TrainingDefinitionDbRepository,
+)
+from kwai.modules.training.trainings.training_definition_repository import (
+    TrainingDefinitionRepository,
+)
+from kwai.modules.training.trainings.training_repository import TrainingRepository
+from kwai.modules.training.trainings.value_objects import TrainingCoach
+from kwai.modules.training.update_training import UpdateTraining, UpdateTrainingCommand
+from tests.modules.training.conftest import Context
+
+
+@pytest.fixture
+def training_repo(database: Database) -> TrainingRepository:
+    """A fixture for a training repository."""
+    return TrainingDbRepository(database)
+
+
+@pytest.fixture
+def coach_repo(database: Database) -> CoachRepository:
+    """A fixture for a coach repository."""
+    return CoachDbRepository(database)
+
+
+@pytest.fixture
+def definition_repo(database: Database) -> TrainingDefinitionRepository:
+    """A fixture for a training definition repository."""
+    return TrainingDefinitionDbRepository(database)
+
+
+@pytest.fixture
+def team_repo(database: Database) -> TeamRepository:
+    """A fixture for a team repository."""
+    return TeamDbRepository(database)
+
+
+@pytest.fixture
+async def coach(
+    database: Database, context: Context, coach_repo: CoachRepository
+) -> CoachEntity:
+    """A fixture for a coach."""
+    return await coach_repo.get_by_id(CoachIdentifier(context.get("coaches")[0].id))
+
+
+@pytest.fixture
+async def team(
+    database: Database, context: Context, team_repo: TeamRepository
+) -> TeamEntity:
+    """A fixture for a team repository."""
+    teams_iterator = team_repo.get_by_ids(TeamIdentifier(context.get("teams")[0].id))
+    return await anext(teams_iterator)
+
+
+@pytest.fixture
+async def training_entity(
+    training_repo: TrainingRepository,
+    coach: CoachEntity,
+    team: TeamEntity,
+    owner: Owner,
+) -> TrainingEntity:
+    """A fixture for a training entity."""
+    start_date = LocalTimestamp.create_now()
+    return await training_repo.create(
+        TrainingEntity(
+            content=[
+                LocaleText(
+                    locale="en",
+                    format="md",
+                    title="Test training",
+                    content="This is a test",
+                    summary="This is a test",
+                    author=owner,
+                )
+            ],
+            coaches=[TrainingCoach(coach=coach, owner=owner)],
+            teams=[team],
+            period=Period(
+                start_date=start_date, end_date=start_date.add_delta(hours=1)
+            ),
+        )
+    )
+
+
+async def test_update_training(
+    training_entity: TrainingEntity,
+    training_repo: TrainingRepository,
+    definition_repo: TrainingDefinitionRepository,
+    coach_repo: CoachRepository,
+    team_repo: TeamRepository,
+    owner: Owner,
+):
+    """Test the use case "Update Training"."""
+    start_date = LocalTimestamp.create_now().add_delta(hours=1)
+    end_date = start_date.add_delta(hours=1)
+    command = UpdateTrainingCommand(
+        id=training_entity.id.value,
+        start_date=str(start_date),
+        end_date=str(end_date),
+        active=training_entity.active,
+        cancelled=training_entity.cancelled,
+        location=training_entity.location,
+        text=[
+            {
+                "locale": "en",
+                "format": "md",
+                "title": "Updated Test Training",
+                "content": "This is a test (updated)",
+                "summary": "This is a test (updated)",
+            }
+        ],
+        remark="This is a test",
+        coaches=[
+            Coach(
+                id=training_coach.coach.id.value,
+                head=training_coach.type == 1,
+                present=training_coach.present,
+                payed=training_coach.payed,
+            )
+            for training_coach in training_entity.coaches
+        ],
+        teams=[team.id.value for team in training_entity.teams],
+        definition=None,
+    )
+
+    training = await UpdateTraining(
+        training_repo, definition_repo, coach_repo, team_repo, owner
+    ).execute(command)
+
+    assert training is not None, "There should be a training"
