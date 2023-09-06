3006c815fb05e5f66b77a89fe687d68c75e7da79
fbraem <franky.braem@gmail.com>
2023-09-06 20:31:05 +0200

    refactor: move some fixtures to conftest.py

--- backend/src/tests/modules/training/trainings/test_training_definition_db_repository.py
+++ backend/src/tests/modules/training/trainings/test_training_definition_db_repository.py
@@ -1,16 +1,9 @@
 """Module for testing the training definition repository."""
 
-import datetime
-
 import pytest
 
 from kwai.core.db.database import Database
 from kwai.core.domain.entity import Entity
-from kwai.core.domain.value_objects.identifier import IntIdentifier
-from kwai.core.domain.value_objects.owner import Owner
-from kwai.core.domain.value_objects.time_period import TimePeriod
-from kwai.core.domain.value_objects.weekday import Weekday
-from kwai.modules.identity.users.user import UserEntity
 from kwai.modules.training.trainings.training_definition import (
     TrainingDefinitionEntity,
 )
@@ -25,53 +18,37 @@ from kwai.modules.training.trainings.training_definition_repository import (
 pytestmark = pytest.mark.db
 
 
-def _find(entity_list: list, id_: IntIdentifier):
-    """Search for an entity with the given id."""
-    for entity in entity_list:
-        if entity.id == id_:
-            return entity
-    return None
-
-
 @pytest.fixture(scope="module")
 def repo(database: Database) -> TrainingDefinitionRepository:
     """Fixture for a training definition repository."""
     return TrainingDefinitionDbRepository(database)
 
 
-@pytest.fixture(scope="module")
-async def training_definition(
-    repo: TrainingDefinitionRepository, user: UserEntity
+@pytest.fixture
+async def saved_training_definition(
+    repo: TrainingDefinitionRepository,
+    training_definition: TrainingDefinitionEntity,
 ) -> TrainingDefinitionEntity:
-    """Fixture for a training definition."""
-    training_definition = TrainingDefinitionEntity(
-        name="Wednesday Training",
-        description="Test training",
-        weekday=Weekday.WEDNESDAY,
-        period=TimePeriod(
-            start=datetime.time(hour=21, minute=0),
-            end=datetime.time(hour=22, minute=0),
-        ),
-        owner=Owner(id=user.id, uuid=user.uuid, name=user.name),
-    )
+    """A fixture for a training definition in the database."""
     return await repo.create(training_definition)
 
 
-def test_create(training_definition: TrainingDefinitionEntity):
+def test_create(saved_training_definition: TrainingDefinitionEntity):
     """Test if the training definition was created."""
     assert (
-        not training_definition.id.is_empty()
+        not saved_training_definition.id.is_empty()
     ), "There should be a training definition created"
 
 
 async def test_get_by_id(
-    repo: TrainingDefinitionRepository, training_definition: TrainingDefinitionEntity
+    repo: TrainingDefinitionRepository,
+    saved_training_definition: TrainingDefinitionEntity,
 ):
     """Test if the training definition can be found with the id."""
-    entity = await repo.get_by_id(training_definition.id)
+    entity = await repo.get_by_id(saved_training_definition.id)
 
     assert (
-        entity.id == training_definition.id
+        entity.id == saved_training_definition.id
     ), "The training definition should be found"
 
 
@@ -82,20 +59,22 @@ async def test_get_all(repo: TrainingDefinitionRepository):
 
 
 async def test_update(
-    repo: TrainingDefinitionRepository, training_definition: TrainingDefinitionEntity
+    repo: TrainingDefinitionRepository,
+    saved_training_definition: TrainingDefinitionEntity,
 ):
     """Test update of training definition."""
     training_definition = Entity.replace(
-        training_definition, remark="Training definition updated"
+        saved_training_definition, remark="Training definition updated"
     )
     await repo.update(training_definition)
 
 
 async def test_delete(
-    repo: TrainingDefinitionRepository, training_definition: TrainingDefinitionEntity
+    repo: TrainingDefinitionRepository,
+    saved_training_definition: TrainingDefinitionEntity,
 ):
     """Test if the training definition can be deleted."""
-    await repo.delete(training_definition)
+    await repo.delete(saved_training_definition)
 
     with pytest.raises(TrainingDefinitionNotFoundException):
-        await repo.get_by_id(training_definition.id)
+        await repo.get_by_id(saved_training_definition.id)
