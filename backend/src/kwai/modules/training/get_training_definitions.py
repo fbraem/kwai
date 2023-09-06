c58e887db6fc7bae3e5eabd3417dd9df2347f656
fbraem <franky.braem@gmail.com>
2023-09-06 21:25:01 +0200

    feat: add use case "Get Training Definitions"

---
+++ backend/src/kwai/modules/training/get_training_definitions.py
@@ -0,0 +1,38 @@
+"""Module that defines the use case "Get Training Definitions"."""
+from dataclasses import dataclass
+
+from kwai.core.domain.use_case import UseCaseBrowseResult
+from kwai.modules.training.trainings.training_definition_repository import (
+    TrainingDefinitionRepository,
+)
+
+
+@dataclass(kw_only=True, frozen=True, slots=True)
+class GetTrainingDefinitionsCommand:
+    """Input for the use case "Get Training Definitions"."""
+
+    limit: int | None
+    offset: int | None
+
+
+class GetTrainingDefinitions:
+    """Use case "Get Training Definitions"."""
+
+    def __init__(self, repo: TrainingDefinitionRepository):
+        """Initialize the use case.
+
+        Args:
+            repo: A repository for retrieving training definitions.
+        """
+        self._repo = repo
+
+    async def execute(
+        self, command: GetTrainingDefinitionsCommand
+    ) -> UseCaseBrowseResult:
+        """Execute the use case."""
+        query = self._repo.create_query()
+
+        return UseCaseBrowseResult(
+            count=await query.count(),
+            iterator=self._repo.get_all(query, command.limit, command.offset),
+        )
