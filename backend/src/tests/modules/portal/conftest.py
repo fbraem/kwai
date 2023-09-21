fdb6c7474bec079fd00df4a14f859cfa9effe31e
fbraem <franky.braem@gmail.com>
2023-09-21 16:31:34 +0200

    refactor: add application_repo fixture

--- backend/src/tests/modules/portal/conftest.py
+++ backend/src/tests/modules/portal/conftest.py
@@ -8,23 +8,31 @@ from kwai.modules.portal.applications.application import ApplicationEntity
 from kwai.modules.portal.applications.application_db_repository import (
     ApplicationDbRepository,
 )
+from kwai.modules.portal.applications.application_repository import (
+    ApplicationRepository,
+)
 from kwai.modules.portal.pages.page import PageEntity
 from kwai.modules.portal.pages.page_db_repository import PageDbRepository
 from kwai.modules.portal.pages.page_repository import PageRepository
 
 
 @pytest.fixture(scope="module")
-async def application(database: Database) -> ApplicationEntity:
+def application_repo(database: Database) -> ApplicationRepository:
+    """Fixture for an application repository."""
+    return ApplicationDbRepository(database)
+
+
+@pytest.fixture(scope="module")
+async def application(application_repo: ApplicationRepository) -> ApplicationEntity:
     """Fixture for an application."""
-    repo = ApplicationDbRepository(database)
     application = ApplicationEntity(
         title="Test", name="test", short_description="An application used for testing"
     )
-    application = await repo.create(application)
+    application = await application_repo.create(application)
 
     yield application
 
-    await repo.delete(application)
+    await application_repo.delete(application)
 
 
 @pytest.fixture(scope="module")
