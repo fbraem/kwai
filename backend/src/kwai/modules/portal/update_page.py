fc7d8d4419bb2cfa8e5136e63aee368cd07156bb
fbraem <franky.braem@gmail.com>
2023-09-21 20:00:52 +0200

    feat: add update page

---
+++ backend/src/kwai/modules/portal/update_page.py
@@ -0,0 +1,79 @@
+"""Module for the use case "Update Page"."""
+from dataclasses import dataclass
+
+from kwai.core.domain.entity import Entity
+from kwai.core.domain.value_objects.owner import Owner
+from kwai.core.domain.value_objects.text import DocumentFormat, Locale, LocaleText
+from kwai.modules.portal.applications.application import ApplicationIdentifier
+from kwai.modules.portal.applications.application_repository import (
+    ApplicationRepository,
+)
+from kwai.modules.portal.page_command import PageCommand
+from kwai.modules.portal.pages.page import PageEntity, PageIdentifier
+from kwai.modules.portal.pages.page_repository import PageRepository
+
+
+@dataclass(kw_only=True, frozen=True, slots=True)
+class UpdatePageCommand(PageCommand):
+    """Input for the "Update Page" use case."""
+
+    id: int
+
+
+class UpdatePage:
+    """Use case for updating a page."""
+
+    def __init__(
+        self,
+        repo: PageRepository,
+        application_repo: ApplicationRepository,
+        owner: Owner,
+    ):
+        """Initialize the use case.
+
+        Args:
+            repo: A repository for updating pages.
+            application_repo: A repository for getting the application.
+            owner: The owner of the page.
+        """
+        self._repo = repo
+        self._application_repo = application_repo
+        self._owner = owner
+
+    async def execute(self, command: UpdatePageCommand) -> PageEntity:
+        """Execute the use case.
+
+        Args:
+            command: The input for this use case.
+
+        Raises:
+            PageNotFoundException: When the page does not exist.
+            ApplicationNotFoundException: When the application does not exist.
+        """
+        page = await self._repo.get_by_id(PageIdentifier(command.id))
+        application = await self._application_repo.get_by_id(
+            ApplicationIdentifier(command.application)
+        )
+
+        page = Entity.replace(
+            page,
+            enabled=command.enabled,
+            application=application,
+            content=[
+                LocaleText(
+                    locale=Locale(text.locale),
+                    format=DocumentFormat(text.format),
+                    title=text.locale,
+                    content=text.content,
+                    summary=text.summary,
+                    author=self._owner,
+                )
+                for text in command.text
+            ],
+            priority=command.priority,
+            remark=command.remark,
+        )
+
+        await self._repo.update(page)
+
+        return page
---
+++ backend/src/tests/modules/portal/test_update_page.py
@@ -0,0 +1,40 @@
+"""Module for testing the "Update Page" use case."""
+from kwai.core.domain.value_objects.owner import Owner
+from kwai.modules.portal.applications.application_repository import (
+    ApplicationRepository,
+)
+from kwai.modules.portal.page_command import TextCommand
+from kwai.modules.portal.pages.page import PageEntity
+from kwai.modules.portal.pages.page_repository import PageRepository
+from kwai.modules.portal.update_page import UpdatePage, UpdatePageCommand
+
+
+async def test_update_page(
+    page_repo: PageRepository,
+    application_repo: ApplicationRepository,
+    owner: Owner,
+    saved_page: PageEntity,
+):
+    """Test the "Update Page" use case."""
+    command = UpdatePageCommand(
+        id=saved_page.id.value,
+        enabled=saved_page.enabled,
+        text=[
+            TextCommand(
+                locale=text.locale.value,
+                format=text.format.value,
+                title=text.title,
+                summary=text.summary,
+                content=text.content,
+            )
+            for text in saved_page.content
+        ],
+        application=saved_page.application.id.value,
+        priority=saved_page.priority,
+        remark="Updated with test_update_page",
+    )
+    updated_page = await UpdatePage(page_repo, application_repo, owner).execute(command)
+    assert updated_page is not None, "There should be an updated page."
+    assert (
+        updated_page.remark == "Updated with test_update_page"
+    ), "The page should be updated."
