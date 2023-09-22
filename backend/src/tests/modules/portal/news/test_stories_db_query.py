0cbfbcaa1f0918ce6d25b98a2fca4e866e4f632b
fbraem <franky.braem@gmail.com>
2023-09-22 17:12:04 +0200

    refactor: rename story into news item

--- backend/src/tests/modules/portal/news/test_stories_db_query.py
+++ backend/src/tests/modules/portal/news/test_stories_db_query.py
@@ -1,4 +1,4 @@
-"""Module for testing the story database query."""
+"""Module for testing the news item database query."""
 import pytest
 
 from kwai.core.db.database import Database
@@ -7,37 +7,37 @@ from kwai.modules.portal.news.news_item_db_query import NewsItemDbQuery
 pytestmark = pytest.mark.db
 
 
-async def test_story_db_query(database: Database):
+async def test_news_item_db_query(database: Database):
     """Test the query."""
     query = NewsItemDbQuery(database)
     count = await query.count()
-    assert count >= 0, "There should be 0 or more stories."
+    assert count >= 0, "There should be 0 or more news items."
     rows = query.order_by_publication_date().fetch(limit=10)
     assert rows is not None, "There should be a result"
 
 
 async def test_filter_by_publication_date(database: Database):
-    """Test the query with filtering on the story publication date."""
+    """Test the query with filtering on the news item publication date."""
     query = NewsItemDbQuery(database)
     query.filter_by_publication_date(2023)
     count = await query.count()
-    assert count >= 0, "There should be 0 or more stories."
+    assert count >= 0, "There should be 0 or more news items."
 
 
 async def test_filter_by_promoted(database: Database):
-    """Test the query with filtering on promoted stories."""
+    """Test the query with filtering on promoted news items."""
     query = NewsItemDbQuery(database)
     query.filter_by_promoted()
     count = await query.count()
-    assert count >= 0, "There should be 0 or more stories."
+    assert count >= 0, "There should be 0 or more news items."
 
 
 async def test_filter_by_active(database: Database):
-    """Test the query with filtering on active stories."""
+    """Test the query with filtering on active news items."""
     query = NewsItemDbQuery(database)
     query.filter_by_active()
     count = await query.count()
-    assert count >= 0, "There should be 0 or more stories."
+    assert count >= 0, "There should be 0 or more news items."
 
 
 async def test_filter_by_user(database: Database):
@@ -45,4 +45,4 @@ async def test_filter_by_user(database: Database):
     query = NewsItemDbQuery(database)
     query.filter_by_user(1)
     count = await query.count()
-    assert count >= 0, "There should be 0 or more stories."
+    assert count >= 0, "There should be 0 or more news items."
