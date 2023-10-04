72c70e5d364c6f0e59194ad3e19d5f08143fe66b
fbraem <franky.braem@gmail.com>
2023-10-04 21:40:06 +0200

    test: add test for invalid application

--- backend/src/tests/modules/portal/test_create_news_item.py
+++ backend/src/tests/modules/portal/test_create_news_item.py
@@ -1,8 +1,11 @@
 """Module for testing the use case "Create News Item"."""
+import pytest
+
 from kwai.core.domain.use_case import TextCommand
 from kwai.core.domain.value_objects.owner import Owner
 from kwai.modules.portal.applications.application import ApplicationEntity
 from kwai.modules.portal.applications.application_repository import (
+    ApplicationNotFoundException,
     ApplicationRepository,
 )
 from kwai.modules.portal.create_news_item import CreateNewsItem, CreateNewsItemCommand
@@ -38,3 +41,32 @@ async def test_create_news_item(
         command
     )
     assert news_item is not None, "There should be a news item."
+
+
+async def test_create_news_item_with_wrong_application(
+    news_item_repo: NewsItemRepository,
+    application: ApplicationEntity,
+    application_repo: ApplicationRepository,
+    owner: Owner,
+):
+    """Test "Create News Item" raising ApplicationNotFoundException."""
+    command = CreateNewsItemCommand(
+        enabled=True,
+        texts=[
+            TextCommand(
+                locale="en",
+                format="md",
+                title="Test",
+                summary="This is a test",
+                content="This is a test",
+            )
+        ],
+        application=0,
+        publish_datetime="2023-01-01 00:00:00",
+        end_datetime=None,
+        promotion=0,
+        promotion_end_datetime=None,
+        remark="Created with test_create_news_item",
+    )
+    with pytest.raises(ApplicationNotFoundException):
+        await CreateNewsItem(news_item_repo, application_repo, owner).execute(command)
