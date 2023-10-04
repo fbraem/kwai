5c4c328214e775815c83257f1be762d7d314cc97
fbraem <franky.braem@gmail.com>
2023-10-04 21:55:45 +0200

    feat: add more attributes

--- backend/src/kwai/api/schemas/news_item.py
+++ backend/src/kwai/api/schemas/news_item.py
@@ -38,10 +38,24 @@ class NewsItemResource:
         """Get the priority of the promotion."""
         return self._news_item.promotion.priority
 
+    @json_api.attribute(name="promotion_end_date")
+    def get_promotion_end_date(self) -> str | None:
+        """Get the publication end date."""
+        if self._news_item.promotion.end_date.empty:
+            return None
+        return str(self._news_item.period.end_date)
+
     @json_api.attribute(name="publish_date")
     def get_publish_date(self) -> str:
         """Get the publication date."""
         return str(self._news_item.period.start_date)
+
+    @json_api.attribute(name="end_date")
+    def get_end_date(self) -> str | None:
+        """Get the publication end date."""
+        if self._news_item.period.endless:
+            return None
+        return str(self._news_item.period.end_date)
 
     @json_api.attribute(name="texts")
     def get_texts(self) -> list[NewsItemText]:
@@ -62,3 +76,13 @@ class NewsItemResource:
     def get_application(self) -> ApplicationResource:
         """Get the application of the news item."""
         return ApplicationResource(self._news_item.application)
+
+    @json_api.attribute(name="enabled")
+    def get_enabled(self) -> bool:
+        """Is the news item enabled?"""
+        return self._news_item.is_enabled
+
+    @json_api.attribute(name="remark")
+    def get_remark(self) -> str:
+        """Get the remark of the news item."""
+        return self._news_item.remark
