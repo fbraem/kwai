b82cb6610d672cef22ccdbe309268c80ab8ad2e9
zumuta <franky.braem@zumuta.be>
2023-10-17 21:20:41 +0200

    fix: use rollupTypes and change fileName

--- frontend/packages/kwai-ui/vite.config.ts
+++ frontend/packages/kwai-ui/vite.config.ts
@@ -1,31 +1,31 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import dts from 'vite-plugin-dts'
-import { resolve } from 'path'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import dts from 'vite-plugin-dts';
+import { resolve } from 'path';
 
 export default defineConfig({
-    plugins: [
-        vue(),
-        dts()
-    ],
-    build: {
-        lib: {
-            entry: resolve(__dirname, "src/index.ts"),
-            name: '@kwai/ui',
-            fileName: (format) => { console.log(format); return `index.${format}.js` }
-        },
-        rollupOptions: {
-            // make sure to externalize deps that shouldn't be bundled into the library
-            external: ['vue', 'vue-router', 'vee-validate'],
-            output: {
-                // Provide global variables to use in the UMD build
-                // for externalized deps
-                globals: {
-                    vue: 'Vue',
-                    'vue-router': 'VueRouter',
-                    'vee-validate': 'VeeValidate'
-                }
-            }
-        }
+  plugins: [
+    vue(),
+    dts({ rollupTypes: true }),
+  ],
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/index.ts'),
+      name: '@kwai/ui',
+      fileName: 'kwai-ui',
     },
+    rollupOptions: {
+      // make sure to externalize deps that shouldn't be bundled into the library
+      external: ['vue', 'vue-router', 'vee-validate'],
+      output: {
+        // Provide global variables to use in the UMD build
+        // for externalized deps
+        globals: {
+          vue: 'Vue',
+          'vue-router': 'VueRouter',
+          'vee-validate': 'VeeValidate',
+        },
+      },
+    },
+  },
 });
