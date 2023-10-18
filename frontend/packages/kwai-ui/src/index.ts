be526ece04dce1ccc8b595c4364626565eca4d23
zumuta <franky.braem@zumuta.be>
2023-10-18 20:43:41 +0200

    refactor: eslint

--- frontend/packages/kwai-ui/src/index.ts
+++ frontend/packages/kwai-ui/src/index.ts
@@ -3,34 +3,34 @@ import './index.css';
 
 import Alert from './alerts/Alert.vue';
 import ErrorAlert from './alerts/ErrorAlert.vue';
-export { Alert, ErrorAlert };
-
 import Card from './card/Card.vue';
 import CardTitle from './card/CardTitle.vue';
 import CardLinkedTitle from './card/CardLinkedTitle.vue';
 import CardRouterLinkedTitle from './card/CardRouterLinkedTitle.vue';
-
-export { Card, CardTitle, CardLinkedTitle, CardRouterLinkedTitle };
-
 import InformationDialog from './dialogs/InformationDialog.vue';
-export { InformationDialog };
-
 import Toolbar from './nav/Toolbar.vue';
 import Sidebar from './nav/Sidebar.vue';
 import { useSidebar } from './nav/useSidebar';
-export { Toolbar, Sidebar, useSidebar };
-
 import PortalLayout from './layout/PortalLayout.vue';
 import DialogLayout from './layout/DialogLayout.vue';
-export { PortalLayout, DialogLayout };
-
 import InputField from './form/InputField.vue';
 import Button from './form/Button.vue';
-export { InputField, Button };
-
 import BarsIcon from './icons/BarsIcon.vue';
 import CheckIcon from './icons/CheckIcon.vue';
 import CloseIcon from './icons/CloseIcon.vue';
 import ErrorIcon from './icons/ErrorIcon.vue';
 import RequiredIcon from './icons/RequiredIcon.vue';
+
+export { Alert, ErrorAlert };
+
+export { Card, CardTitle, CardLinkedTitle, CardRouterLinkedTitle };
+
+export { InformationDialog };
+
+export { Toolbar, Sidebar, useSidebar };
+
+export { PortalLayout, DialogLayout };
+
+export { InputField, Button };
+
 export { BarsIcon, CheckIcon, CloseIcon, ErrorIcon, RequiredIcon };
