b5683ffab794ca2dd09b9cb6bc523cb72128e6cf
zumuta <franky.braem@zumuta.be>
2023-10-20 20:44:06 +0200

    fix: check for redis logger

--- backend/src/kwai/kwai_bus.py
+++ backend/src/kwai/kwai_bus.py
@@ -32,8 +32,13 @@ def create_bus(settings: Settings = Depends(get_settings)):
 
         return new_format
 
+    if settings.redis.logger:
+        log_file = settings.redis.logger.file
+    else:
+        log_file = None
+
     logger.add(
-        settings.redis.logger.file or sys.stderr,
+        log_file or sys.stderr,
         format=log_format,
         level=settings.redis.logger.level,
         colorize=True,
