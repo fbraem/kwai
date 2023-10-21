b36a23ccb2a18cd76983b17a8f6f806efabb25b4
zumuta <franky.braem@zumuta.be>
2023-10-21 21:23:58 +0200

    feat: IntroductionText

---
+++ frontend/apps/portal/src/components/IntroductionText.vue
@@ -0,0 +1,13 @@
+<template>
+  <blockquote class="p-4 my-4 border-l-4 border-gray-300 bg-gray-50">
+    <slot />
+  </blockquote>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>
