b8a0c9b4b515c1941213a054e117c760cd88e224
zumuta <franky.braem@zumuta.be>
2023-10-21 22:10:38 +0200

    refactor: start using vue-query

---
+++ frontend/apps/portal/src/composables/useNewsItem.ts
@@ -0,0 +1,100 @@
+import { useQuery } from '@tanstack/vue-query';
+import type { JsonApiDataType } from '@kwai/api';
+import { JsonApiDocument, JsonResourceIdentifier, useHttpApi } from '@kwai/api';
+import { z } from 'zod';
+import { createDateTimeFromUTC } from '@kwai/date';
+import type { DateType } from '@kwai/date';
+
+const JsonApiText = z.object({
+  locale: z.string(),
+  format: z.string(),
+  title: z.string(),
+  summary: z.string(),
+  content: z.nullable(z.string()),
+});
+
+const JsonApiNewsItem = z.object({
+  id: z.string(),
+  type: z.literal('news_items'),
+  attributes: z.object({
+    priority: z.number(),
+    publish_date: z.string(),
+    texts: z.array(JsonApiText),
+  }),
+  relationships: z.object({
+    application: z.object({
+      data: JsonResourceIdentifier,
+    }),
+  }),
+});
+type JsonApiNewsItemType = z.infer<typeof JsonApiNewsItem>;
+
+const JsonApiApplication = z.object({
+  id: z.string(),
+  type: z.literal('applications'),
+  attributes: z.object({
+    name: z.string(),
+    title: z.string(),
+  }),
+});
+
+const JsonApiNewsItemData = z.object({
+  data: z.union([JsonApiNewsItem, z.array(JsonApiNewsItem).default([])]),
+  included: z.array(JsonApiApplication).default([]),
+});
+const JsonApiNewsItemDocument = JsonApiDocument.extend(JsonApiNewsItemData.shape);
+type JSONApiNewsItemDocumentType = z.infer<typeof JsonApiNewsItemData>;
+
+interface NewsItemText {
+  locale: string,
+  title: string,
+  summary: string,
+  content?: string | null
+}
+
+export interface NewsItem {
+  id: string,
+  priority: number,
+  publishDate: DateType,
+  texts: NewsItemText[]
+}
+
+const toModel = (json: JSONApiNewsItemDocumentType): NewsItem | NewsItem[] => {
+  const mapModel = (d: JsonApiDataType): NewsItem => {
+    const newsItem = <JsonApiNewsItemType> d;
+    return {
+      id: newsItem.id,
+      priority: newsItem.attributes.priority,
+      publishDate: createDateTimeFromUTC(newsItem.attributes.publish_date),
+      texts: newsItem.attributes.texts.map(text => ({
+        locale: text.locale,
+        title: text.title,
+        summary: text.summary,
+        content: text.content,
+      })),
+    };
+  };
+  if (Array.isArray(json.data)) {
+    return json.data.map(mapModel);
+  }
+  return mapModel(json.data);
+};
+
+const getNewsItem = (id: string) => {
+  const url = `/v1/news_items/${id}`;
+  const api = useHttpApi().url(url);
+  return api.get().json(json => {
+    const result = JsonApiNewsItemDocument.safeParse(json);
+    if (result.success) {
+      return toModel(result.data);
+    }
+    throw result.error;
+  });
+};
+
+export const useNewsItem = (id: string) => {
+  return useQuery({
+    queryKey: ['portal/news_items', id],
+    queryFn: () => getNewsItem(id),
+  });
+};
--- frontend/apps/portal/src/index.ts
+++ frontend/apps/portal/src/index.ts
@@ -6,10 +6,13 @@ import './index.css';
 import { createPinia } from 'pinia';
 import { createRouter, createWebHistory } from 'vue-router';
 import routes from './routes';
+import { VueQueryPlugin } from '@tanstack/vue-query';
 
 const pinia = createPinia();
 const app = createApp(App);
 app.use(pinia);
+
+app.use(VueQueryPlugin);
 
 const router = createRouter({
   history: createWebHistory(),
