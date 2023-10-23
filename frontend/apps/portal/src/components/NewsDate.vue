1ae80d0e058ceda961d15bf4025c0f89d90da4f9
zumuta <franky.braem@zumuta.be>
2023-10-23 21:56:00 +0200

    feat: add NewsDate component

---
+++ frontend/apps/portal/src/components/NewsDate.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="text-center p-2 bg-red-600 text-white min-w-fit">
+    <span class="block font-bold text-xl">
+      {{ date.format('D') }}
+    </span>
+    <span class="block text-sm">
+      {{ date.format('MMM') }}
+    </span>
+    <span class="block text-sm">
+      {{ date.format('YYYY') }}
+    </span>
+  </div>
+</template>
+
+<script setup lang="ts">
+import type { DateType } from '@kwai/date';
+
+interface Props {
+  date: DateType
+}
+
+defineProps<Props>();
+</script>
