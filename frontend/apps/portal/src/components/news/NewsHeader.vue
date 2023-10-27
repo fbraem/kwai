f8735dd274ac94829ac1a4eeab77dc01e9e883cc
zumuta <franky.braem@zumuta.be>
2023-10-27 21:04:39 +0200

    feat: add application as badge

--- frontend/apps/portal/src/components/news/NewsHeader.vue
+++ frontend/apps/portal/src/components/news/NewsHeader.vue
@@ -4,10 +4,19 @@
     :to="`/news/${newsItem.id}`"
   >
     <NewsDate :date="newsItem.publishDate " />
-    <div class="flex-grow p-2 border-b-2 border-red-600 items-center">
-      <h3 class="text-2xl text-gray-500 font-bold">
+    <div class="flex-grow flex py-2 pl-2 border-b-2 border-red-600 items-center">
+      <h3 class="flex-grow text-2xl text-gray-500 font-bold">
         {{ newsItem.texts[0].title }}
       </h3>
+      <router-link
+        :to="`/news/application/${newsItem.application.name}`"
+      >
+        <div>
+          <span class="inline-block font-bold text-xs bg-gray-200 px-[0.65em] pb-[0.25em] pt-[0.35em]">
+            {{ newsItem.application.title }}
+          </span>
+        </div>
+      </router-link>
     </div>
   </router-link>
 </template>
