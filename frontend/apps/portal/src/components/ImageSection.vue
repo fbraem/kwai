dfd1b2a9fbb2a236b60207c80d3d67086749e745
zumuta <franky.braem@zumuta.be>
2023-11-02 19:47:24 +0100

    feat: Add ImageSection

---
+++ frontend/apps/portal/src/components/ImageSection.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+interface Props {
+  backgroundImage: string
+}
+defineProps<Props>();
+
+</script>
+
+<template>
+  <section>
+    <div
+      class="relative w-full bg-center bg-cover"
+      :style="{ 'background-image' : `url(${backgroundImage})` }"
+    >
+      <span class="absolute w-full h-full opacity-50 bg-gradient-to-br from-black to-red-600" />
+      <slot />
+    </div>
+  </section>
+</template>
+
+<style scoped>
+
+</style>
