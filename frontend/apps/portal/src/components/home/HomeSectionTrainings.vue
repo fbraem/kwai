dae9fa3c2dd34bb6f92dc3f64433be8f571f6976
zumuta <franky.braem@zumuta.be>
2023-11-03 22:14:27 +0100

    refactor: use composable useTrainings and show trainings of the current day + 7 days

--- frontend/apps/portal/src/components/home/HomeSectionTrainings.vue
+++ frontend/apps/portal/src/components/home/HomeSectionTrainings.vue
@@ -5,89 +5,117 @@ import trainingImage from '/training.jpg';
 import sporthalImage from '/sporthal.jpg';
 
 import ApplicationList from '@root/components/ApplicationList.vue';
-import LoadingIcon from '@root/components/icons/LoadingIcon.vue';
 import { useTrainings } from '@root/composables/useTraining';
-import type { TrainingPeriod } from '@root/composables/useTraining';
+import type { Training, TrainingPeriod } from '@root/composables/useTraining';
 import type { Ref } from 'vue';
-import { ref } from 'vue';
+import { computed, ref } from 'vue';
 import { now } from '@kwai/date';
+import ImageSection from '@root/components/ImageSection.vue';
+import BetweenIcon from '@root/components/icons/BetweenIcon.vue';
 
 const period : Ref<TrainingPeriod> = ref({
   start: now(),
   end: now().add(1, 'week'),
 });
 const { isLoading, data: trainings } = useTrainings(period);
+
+type TrainingDays = {[key: string] : Training[]};
+const trainingDays = computed(() => {
+  const result : TrainingDays = {};
+  if (!trainings.value) {
+    return {};
+  }
+  trainings.value.items.forEach(training => {
+    const date = training.start_date.format('YYYY-MM-DD');
+    if (!result[date]) {
+      result[date] = [];
+    }
+    result[date].push(training);
+  });
+  return result;
+});
 </script>
 
 <template>
-  <section>
-    <div
-      class="relative w-full bg-center bg-cover"
-      :style="{ 'background-image' : `url(${trainingImage})` }"
-    >
-      <span class="absolute w-full h-full opacity-50 bg-gradient-to-br from-black to-red-600" />
-      <div class="container relative mx-auto h-full p-4">
-        <div class="grid lg:grid-cols-2 gap-10 justify-items-center lg:my-10">
-          <div class="mx-auto">
-            <img
-              :src="sporthalImage"
-              alt="sporthal"
-              class="border-2 border-black max-h-80"
-            >
+  <ImageSection :background-image="trainingImage">
+    <div class="flex flex-col items-center text-white pt-12 px-12">
+      <ApplicationList :filter="['trainings']">
+        <template
+          #default="{ application }"
+        >
+          <h2 class="text-4xl font-bold mb-2">
+            {{ application.title }}
+          </h2>
+        </template>
+      </ApplicationList>
+    </div>
+    <div class="flex flex-wrap p-12">
+      <div class="mb-12 w-full shrink-0 grow-0 basis-auto lg:mb-0 lg:w-5/12">
+        <div class="flex lg:py-12">
+          <img
+            :src="sporthalImage"
+            class="z-[10] w-full rounded-lg shadow-lg"
+            alt="Sporthal"
+          >
+        </div>
+      </div>
+      <div class="w-full shrink-0 grow-0 basis-auto rounded-lg bg-zinc-50 opacity-75 lg:w-7/12 lg:ml-[-50px]">
+        <div class="flex flex-col h-full p-6 lg:pl-12 lg:text-left">
+          <h4 class="lg:ml-12 pb-2 text-2xl font-bold">
+            Trainingsrooster
+          </h4>
+          <div
+            v-if="!isLoading && Object.keys(trainingDays).length === 0"
+            class="lg:ml-12 flex flex-row items-center flex-grow"
+          >
+            <div>
+              <p>De komende dagen zijn er geen trainingen gepland.</p>
+              <p>Wil je weten wanneer de volgende training is? Kijk dan op onze Training pagina.</p>
+            </div>
           </div>
-          <div class="flex flex-col gap-4">
-            <ApplicationList :filter="['trainings']">
-              <template
-                #default="{ application }"
-              >
-                <h2 class="text-4xl font-semibold text-white mb-2">
-                  {{ application.title }}
-                </h2>
-                <p
-                  class="text-white leading-8"
-                >
-                  {{ application.short_description }}
+          <ol
+            v-if="!isLoading && Object.keys(trainingDays).length > 0"
+            class="lg:ml-12 border-l border-red-600"
+          >
+            <li
+              v-for="(trainingDay, day) in trainingDays"
+              :key="day"
+            >
+              <div class="flex-start flex items-center pt-3">
+                <div class="-ml-[5px] mr-3 h-[9px] w-[9px] rounded-full bg-red-600" />
+                <p class="font-bold">
+                  {{ trainingDay[0].start_date.format("dddd") }} {{ trainingDay[0].start_date.format("DD-MM-YYYY") }}
                 </p>
-              </template>
-            </ApplicationList>
-            <div v-if="trainings">
-              <div class="flex flex-row gap-4 items-center">
-                <h3 class="text-white text-2xl font-semibold">
-                  Volgende trainingen
-                </h3>
-                <div>
-                  <LoadingIcon
-                    v-show="isLoading"
-                    class="w-8 h-8 fill-red-600 text-gray-600"
-                  />
-                </div>
               </div>
-              <div class="bg-gray-100 px-3 py-2 text-gray-800 rounded divide-y divide-gray-300">
+              <div class="mb-6 ml-4 mt-2">
                 <div
-                  v-for="training in trainings.items.slice(0, 4)"
+                  v-for="training in trainingDay"
                   :key="training.id"
-                  class="flex gap-4 py-1"
+                  class="flex items-center gap-4"
                 >
-                  <div class="text-sm text-center font-medium">
-                    {{ training.start_date.format("DD-MM-YYYY HH:mm") }}&nbsp;-&nbsp;{{ training.end_date.format("HH:mm") }}
-                  </div>
-                  <div class="text-sm">
+                  <div>{{ training.start_date.format("HH:mm") }}u</div>
+                  <BetweenIcon class="w-4 h-4" />
+                  <div>{{ training.end_date.format("HH:mm") }}u</div>
+                  <div class="font-medium">
                     {{ training.title }}
                   </div>
                 </div>
               </div>
-            </div>
-            <div class="my-6">
-              <router-link
-                class="border border-red-600 bg-red-600 hover:bg-white hover:text-red-600 rounded text-sm text-white py-1 px-3"
-                :to="{ 'name': 'portal.trainings' }"
-              >
-                Alle trainingen
-              </router-link>
-            </div>
+            </li>
+          </ol>
+          <div class="my-6 self-end">
+            <router-link
+              class="border border-red-600 bg-red-600 hover:bg-white hover:text-red-600 rounded text-sm text-white py-1 px-3"
+              :to="{ 'name': 'portal.trainings' }"
+            >
+              Alle trainingen
+            </router-link>
+          </div>
+          <div class="text-xs">
+            Al onze trainingen gaan door in de gevechtssportzaal van de sporthal te Stekene.
           </div>
         </div>
       </div>
     </div>
-  </section>
+  </ImageSection>
 </template>
