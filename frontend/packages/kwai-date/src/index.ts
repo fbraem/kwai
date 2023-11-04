1c3b3eaed270302ee045f1d538f4c8dc2554b3c5
zumuta <franky.braem@zumuta.be>
2023-11-04 15:26:07 +0100

    feat: allow null for year, month, day

--- frontend/packages/kwai-date/src/index.ts
+++ frontend/packages/kwai-date/src/index.ts
@@ -74,7 +74,7 @@ function wrapDayjs(d: dayjs.Dayjs): Readonly<DateType> {
   });
 }
 
-export function createDate(year: number = null, month: number = null, day: number = null): Readonly<DateType> {
+export function createDate(year?: number | null, month?: number | null, day?: number | null): Readonly<DateType> {
   year = year || dayjs().year();
   month = month || dayjs().month();
   day = day || dayjs().date();
