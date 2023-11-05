ef2688342a57db88b688c9187bbb8e6a4edc067e
zumuta <franky.braem@zumuta.be>
2023-11-05 18:12:07 +0100

    fix: check for /# to make it possible to convert old urls to web history.

--- frontend/apps/portal/src/index.ts
+++ frontend/apps/portal/src/index.ts
@@ -18,6 +18,15 @@ const router = createRouter({
   history: createWebHistory(),
   routes: [],
 });
+router.beforeEach((to, from, next) => {
+  // Redirect if fullPath begins with a hash (ignore hashes later in path)
+  if (to.fullPath.substring(0, 2) === '/#') {
+    const path = to.fullPath.substring(2);
+    next(path);
+    return;
+  }
+  next();
+});
 routes.forEach(route => router.addRoute(route));
 app.use(router);
 
