2b4368c456c139e119108e28e508e14a6f646557
zumuta <franky.braem@zumuta.be>
2023-11-11 15:20:39 +0100

    fix: pinia is not used anymore

--- frontend/apps/portal/src/index.ts
+++ frontend/apps/portal/src/index.ts
@@ -3,14 +3,11 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import './index.css';
 
-import { createPinia } from 'pinia';
 import { createRouter, createWebHistory } from 'vue-router';
 import routes from './routes';
 import { VueQueryPlugin } from '@tanstack/vue-query';
 
-const pinia = createPinia();
 const app = createApp(App);
-app.use(pinia);
 
 app.use(VueQueryPlugin);
 
