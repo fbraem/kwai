9908d2e33d2da2b6b908633ea8e06b756eb70b19
zumuta <franky.braem@zumuta.be>
2023-11-11 15:37:22 +0100

    ci: fix some vite config

--- frontend/apps/author/vite.config.ts
+++ frontend/apps/author/vite.config.ts
@@ -1,12 +1,22 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
+import { resolve } from 'path';
 
 export default defineConfig({
-  base: '/author/',
+  base: '/',
   server: {
+    host: '0.0.0.0',
     port: 3001,
   },
   plugins: [
     vue(),
   ],
+  resolve: {
+    alias: [
+      {
+        find: /^@root\/(.*)/,
+        replacement: `${resolve(__dirname)}/src/$1`,
+      },
+    ],
+  },
 });
