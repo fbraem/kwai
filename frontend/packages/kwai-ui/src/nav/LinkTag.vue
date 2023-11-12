4d6b1520b5be0c230d5b001eae9015c0192d57f7
zumuta <franky.braem@zumuta.be>
2023-11-12 17:57:00 +0100

    feat: add LinkTag

---
+++ frontend/packages/kwai-ui/src/nav/LinkTag.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import { computed } from 'vue';
+import type { RouteRecord } from 'vue-router';
+
+interface Props {
+  route?: RouteRecord,
+  url?: string,
+  method?: () => void
+}
+const props = defineProps<Props>();
+
+const tag = computed(() => props.route ? 'router-link' : props.url ? 'a' : 'button');
+const to = computed(() => props.route ? 'to' : null);
+const href = computed(() => props.route ? 'url' : null);
+const click = computed(() => props.method ? 'click' : null);
+</script>
+
+<template>
+  <component
+    :is="tag"
+    :[to]="route"
+    :[href]="href"
+    @[click]="method"
+  >
+    <slot />
+  </component>
+</template>
+
+<style scoped>
+
+</style>
