0a752d7d847b615c23138e65b36d1194da1cfefd
zumuta <franky.braem@zumuta.be>
2023-11-16 19:48:40 +0100

    feat: add use case update application

---
+++ backend/src/kwai/modules/portal/update_application.py
@@ -0,0 +1,61 @@
+"""Module that defines the use case: update an application."""
+from dataclasses import dataclass
+
+from kwai.core.domain.entity import Entity
+from kwai.core.domain.value_objects.identifier import IntIdentifier
+from kwai.modules.portal.applications.application import ApplicationEntity
+from kwai.modules.portal.applications.application_repository import (
+    ApplicationRepository,
+)
+
+
+@dataclass(kw_only=True, frozen=True, slots=True)
+class UpdateApplicationCommand:
+    """Input for the use case [UpdateApplication][kwai.modules.portal.update_application.UpdateApplication].
+
+    Attributes:
+        id: The id of the application
+    """
+
+    id: int
+    title: str
+    short_description: str
+    description: str
+    remark: str
+    weight: int
+    events: bool
+    pages: bool
+    news: bool
+
+
+class UpdateApplication:
+    """Implements the use case 'update an application'."""
+
+    def __init__(self, application_repo: ApplicationRepository):
+        """Initialize the use case.
+
+        Args:
+            application_repo: A repository for updating an application.
+        """
+        self._application_repo = application_repo
+
+    async def execute(self, command: UpdateApplicationCommand) -> ApplicationEntity:
+        """Execute the use case.
+
+        Args:
+            command: The input for this use case.
+        """
+        application = await self._application_repo.get_by_id(IntIdentifier(command.id))
+        updated_application = Entity.replace(
+            application,
+            title=command.title,
+            short_description=command.short_description,
+            description=command.description,
+            remark=command.remark,
+            events=command.events,
+            pages=command.pages,
+            news=command.news,
+            weight=command.weight,
+        )
+        await self._application_repo.update(updated_application)
+        return updated_application
---
+++ backend/src/tests/modules/portal/test_update_application.py
@@ -0,0 +1,34 @@
+"""Module for testing the "Update Application" use case."""
+from kwai.core.domain.value_objects.owner import Owner
+from kwai.modules.portal.applications.application import ApplicationEntity
+from kwai.modules.portal.applications.application_repository import (
+    ApplicationRepository,
+)
+from kwai.modules.portal.update_application import (
+    UpdateApplication,
+    UpdateApplicationCommand,
+)
+
+
+async def test_update_application(
+    application_repo: ApplicationRepository,
+    owner: Owner,
+    application: ApplicationEntity,
+):
+    """Test the "Update Application" use case."""
+    command = UpdateApplicationCommand(
+        id=application.id.value,
+        title=application.title,
+        short_description=application.short_description,
+        description=application.description,
+        weight=application.weight,
+        events=application.can_contain_events,
+        pages=application.can_contain_pages,
+        news=application.can_contain_news,
+        remark="Updated with test_update_application",
+    )
+    updated_page = await UpdateApplication(application_repo).execute(command)
+    assert updated_page is not None, "There should be an updated application."
+    assert (
+        updated_page.remark == "Updated with test_update_application"
+    ), "The application should be updated."
