8249e931fc657713bd5d5ad443a63419aaf036df
zumuta <franky.braem@zumuta.be>
2023-11-17 20:32:33 +0100

    refactor: eslint

--- frontend/packages/kwai-ui/src/form/Button.vue
+++ frontend/packages/kwai-ui/src/form/Button.vue
@@ -1,13 +1,13 @@
+<script setup lang="ts">
+defineEmits(['click']);
+</script>
+
 <template>
   <a
-      class="hover:cursor-pointer text-sm font-bold px-12 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1"
-      tabindex="0"
-      @click="$emit('click')"
+    class="hover:cursor-pointer text-sm font-bold px-12 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1"
+    tabindex="0"
+    @click="$emit('click')"
   >
     <slot />
   </a>
 </template>
-
-<script setup lang="ts">
-const emits = defineEmits(['click'])
-</script>
