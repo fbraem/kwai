7f9d417bb899c271bb3b13f65bd47478ce4ed242
zumuta <franky.braem@zumuta.be>
2023-11-18 14:46:54 +0100

    fix: remove format

--- frontend/packages/kwai-types/src/index.ts
+++ frontend/packages/kwai-types/src/index.ts
@@ -2,7 +2,6 @@ import { z } from 'zod';
 
 export const JsonApiText = z.object({
   locale: z.string(),
-  format: z.string(),
   title: z.string(),
   summary: z.string(),
   content: z.nullable(z.string()),
