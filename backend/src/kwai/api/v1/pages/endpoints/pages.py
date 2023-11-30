4708dce2284af1b6488d3672c4ab51b7cb6cf1cb
zumuta <franky.braem@zumuta.be>
2023-11-30 14:22:42 +0100

    fix: use original summary/content for create/update

--- backend/src/kwai/api/v1/pages/endpoints/pages.py
+++ backend/src/kwai/api/v1/pages/endpoints/pages.py
@@ -82,8 +82,8 @@ async def create_page(
                 locale=text.locale,
                 format=text.format,
                 title=text.title,
-                summary=text.summary,
-                content=text.content,
+                summary=text.original_summary,
+                content=text.original_content,
             )
             for text in resource.data.attributes.texts
         ],
@@ -122,8 +122,8 @@ async def update_page(
                 locale=text.locale,
                 format=text.format,
                 title=text.title,
-                summary=text.summary,
-                content=text.content,
+                summary=text.original_summary,
+                content=text.original_content,
             )
             for text in resource.data.attributes.texts
         ],
