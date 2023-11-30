b67c68445a7d9686357b07f36e95ee229d26e93f
zumuta <franky.braem@zumuta.be>
2023-11-30 10:14:13 +0100

    feat: ContainerSectionBanner

--- frontend/packages/kwai-ui/src/section/index.ts
+++ frontend/packages/kwai-ui/src/section/index.ts
@@ -1,3 +1,4 @@
-export { default as ContainerSectionTitle } from './ContainerSectionTitle.vue';
-export { default as ContainerSectionContent } from './ContainerSectionContent.vue';
 export { default as ContainerSection } from './ContainerSection.vue';
+export { default as ContainerSectionBanner } from './ContainerSectionBanner.vue';
+export { default as ContainerSectionContent } from './ContainerSectionContent.vue';
+export { default as ContainerSectionTitle } from './ContainerSectionTitle.vue';
