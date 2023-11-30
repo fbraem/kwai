cef7494c97ce4d50454a28795f5d8f80e9f9a691
zumuta <franky.braem@zumuta.be>
2023-11-30 16:14:21 +0100

    docs: add comment about the composable

--- frontend/packages/kwai-ui/src/nav/useMenu.ts
+++ frontend/packages/kwai-ui/src/nav/useMenu.ts
@@ -2,6 +2,10 @@ import { useRouter } from 'vue-router';
 import { computed } from 'vue';
 import { MenuItem } from '../../dist';
 
+/**
+ * A composable that returns a MenuItem array based on the routes.
+ * When a route has a meta property 'title', it will be added to the list.
+ */
 export const useMenu = () => {
   const router = useRouter();
 
