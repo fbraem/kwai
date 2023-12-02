1555bb37bf29deb2efc7929b93e41ee5e55d2d85
zumuta <franky.braem@zumuta.be>
2023-12-02 13:10:31 +0100

    fix: add h-full and space between multiple fields

--- frontend/packages/kwai-ui/src/form/FormSectionFields.vue
+++ frontend/packages/kwai-ui/src/form/FormSectionFields.vue
@@ -3,8 +3,8 @@
 </script>
 
 <template>
-  <div class="shadow rounded-md">
-    <div class="px-2 py-3 sm:p-6">
+  <div class="shadow rounded-md h-full">
+    <div class="px-2 py-3 sm:p-6 space-y-4">
       <slot />
     </div>
   </div>
