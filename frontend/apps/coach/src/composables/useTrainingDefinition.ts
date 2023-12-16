cadab0bbda681b67e0cc5e8fdc73aa0f8bd7eabd
zumuta <franky.braem@zumuta.be>
2023-12-16 17:19:52 +0100

    feat: add mutation

--- frontend/apps/coach/src/composables/useTrainingDefinition.ts
+++ frontend/apps/coach/src/composables/useTrainingDefinition.ts
@@ -1,10 +1,11 @@
 import { JsonApiData, JsonApiDocument, JsonResourceIdentifier, useHttpApi } from '@kwai/api';
 import { z } from 'zod';
-import { useQuery } from '@tanstack/vue-query';
+import { useMutation, useQuery, useQueryClient } from '@tanstack/vue-query';
 import type { DateType } from '@kwai/date';
-import { createDateTimeFromUTC } from '@kwai/date';
+import { createDateTimeFromUTC, formatToUTC } from '@kwai/date';
 import type { Ref } from 'vue';
 import { toValue } from 'vue';
+import type { Team } from '@root/composables/useTeam';
 
 const TeamResourceSchema = JsonApiData.extend({
   type: z.literal('teams'),
@@ -29,18 +30,13 @@ const TrainingDefinitionResourceSchema = JsonApiData.extend({
   relationships: z.object({
     team: z.object({
       data: JsonResourceIdentifier.nullable(),
-    }),
-  }),
+    }).optional(),
+  }).optional(),
 });
 type TrainingDefinitionResource = z.infer<typeof TrainingDefinitionResourceSchema>;
 
-type Team = {
-  id: string,
-  name: string,
-}
-
 export type TrainingDefinition = {
-  id: string,
+  id?: string,
   active: boolean,
   description: string,
   end_time: DateType,
@@ -57,9 +53,9 @@ const TrainingDefinitionDocumentSchema = JsonApiDocument.extend({
 }).transform(doc => {
   const mapModel = (trainingDefinitionResource: TrainingDefinitionResource): TrainingDefinition => {
     let team: Team | null = null;
-    if (trainingDefinitionResource.relationships.team.data?.id) {
+    if (trainingDefinitionResource.relationships?.team?.data?.id) {
       const teamResource = doc.included?.find(
-        included => included.type === 'teams' && included.id === trainingDefinitionResource.relationships.team.data?.id
+        included => included.type === 'teams' && included.id === trainingDefinitionResource.relationships?.team?.data?.id
       ) as TeamResource;
       if (teamResource) {
         team = {
@@ -129,5 +125,86 @@ export const useTrainingDefinitions = () => {
   return useQuery({
     queryKey: ['coach/training_definitions'],
     queryFn: () => getTrainingDefinitions(),
+  });
+};
+
+const mutateTrainingDefinition = (trainingDefinition: TrainingDefinition) : Promise<TrainingDefinition> => {
+  const payload: TrainingDefinitionDocument = {
+    data: {
+      id: trainingDefinition.id,
+      type: 'training_definitions',
+      attributes: {
+        name: trainingDefinition.name,
+        active: trainingDefinition.active,
+        description: trainingDefinition.description,
+        start_time: formatToUTC(trainingDefinition.start_time, 'HH:mm')!,
+        end_time: formatToUTC(trainingDefinition.end_time, 'HH:mm')!,
+        location: trainingDefinition.location,
+        remark: trainingDefinition.remark,
+        weekday: trainingDefinition.weekday,
+      },
+      relationships: {
+        team: trainingDefinition.team
+          ? {
+              data: {
+                id: trainingDefinition.team.id,
+                type: 'teams',
+              },
+            }
+          : { data: null },
+      },
+    },
+  };
+  if (trainingDefinition.id) {
+    return useHttpApi()
+      .url(`/v1/training_definitions/${trainingDefinition.id}`)
+      .patch(payload)
+      .json()
+      .then(json => {
+        const result = TrainingDefinitionDocumentSchema.safeParse(json);
+        if (result.success) {
+          return result.data as TrainingDefinition;
+        }
+        throw result.error;
+      });
+  }
+  return useHttpApi()
+    .url('/v1/training_definitions')
+    .post()
+    .json()
+    .then(json => {
+      const result = TrainingDefinitionDocumentSchema.safeParse(json);
+      if (result.success) {
+        return result.data as TrainingDefinition;
+      }
+      throw result.error;
+    });
+};
+
+type OnSuccessCallback = (resource: TrainingDefinition) => void;
+type OnSuccessAsyncCallback = (resource: TrainingDefinition) => Promise<void>;
+interface MutationOptions {
+  onSuccess?: OnSuccessCallback | OnSuccessAsyncCallback
+}
+
+export const useTrainingDefinitionMutation = ({ onSuccess } : MutationOptions = {}) => {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: (data: TrainingDefinition) => mutateTrainingDefinition(data),
+    onSuccess: async(data: TrainingDefinition) => {
+      queryClient.setQueryData(['coach/training_definitions', data.id], data);
+      if (onSuccess) {
+        if (onSuccess.constructor.name === 'AsyncFunction') {
+          await onSuccess(data);
+        } else {
+          onSuccess(data);
+        }
+      }
+    },
+    onSettled: () => queryClient.invalidateQueries({
+      queryKey: ['coach/training_definitions'],
+      exact: true,
+    }),
   });
 };
