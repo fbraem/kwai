614b67ed9f736c1254a5dd79f47e890ce3f3df8d
zumuta <franky.braem@zumuta.be>
2023-12-25 20:10:54 +0100

    test: add test script

--- frontend/apps/portal/vite.config.ts
+++ frontend/apps/portal/vite.config.ts
@@ -19,26 +19,32 @@ const resolveTheme = (path: string) => {
   return original;
 };
 
-export default defineConfig({
-  base: '/',
-  server: {
-    host: '0.0.0.0',
-    port: 3000,
-  },
-  plugins: [
-    vue(), splitVendorChunkPlugin(), visualizer(),
-  ],
-  resolve: {
-    alias: [
-      {
-        find: '@theme',
-        replacement: '',
-        customResolver: resolveTheme,
-      },
-      {
-        find: /^@root\/(.*)/,
-        replacement: `${resolve(__dirname)}/src/$1`,
-      },
+export default defineConfig(() => {
+  return {
+    base: '/',
+    server: {
+      host: '0.0.0.0',
+      port: 3000,
+    },
+    plugins: [
+      vue(), splitVendorChunkPlugin(), visualizer(),
     ],
-  },
+    resolve: {
+      alias: [
+        {
+          find: '@theme',
+          replacement: '',
+          customResolver: resolveTheme,
+        },
+        {
+          find: /^@root\/(.*)/,
+          replacement: `${resolve(__dirname)}/src/$1`,
+        },
+      ],
+    },
+    test: {
+      global: true,
+      environment: 'jsdom',
+    },
+  };
 });
