464e1761622bcc7aede40e879fa9e819df1194b0
zumuta <franky.braem@zumuta.be>
2024-01-06 15:22:52 +0100

    feat: add more logging

--- backend/src/kwai/kwai_events.py
+++ backend/src/kwai/kwai_events.py
@@ -1,17 +1,72 @@
 """Module for defining the faststream application."""
-from faststream import FastStream
-from faststream.rabbit import RabbitBroker
+import os
+import sys
 
-from kwai.core.settings import get_settings
-from kwai.events.identity import router
+from faststream import BaseMiddleware, FastStream
+from faststream.rabbit import RabbitBroker
+from faststream.types import DecodedMessage
+from loguru import logger
+
+from kwai.core.settings import LoggerSettings, get_settings
+from kwai.events.v1 import router
 
 settings = get_settings()
+
+
+class LoggerMiddleware(BaseMiddleware):
+    """Middleware for setting up logger."""
+
+    async def on_consume(self, msg: DecodedMessage) -> DecodedMessage:
+        """Set up a context for the logger."""
+        with logger.contextualize():
+            return await super().on_consume(msg)
+
+
+def configure_logger(logger_settings: LoggerSettings):
+    """Configure the logger."""
+    try:
+        logger.remove(0)  # Remove the default logger
+    except ValueError:
+        pass
+
+    def log_format(record):
+        """Change the format when a request_id is set in extra."""
+        if "event_id" in record["extra"]:
+            new_format = (
+                "{time} - {level} - ({extra[event_id]}) - {message}" + os.linesep
+            )
+        else:
+            new_format = "{time} - {level} - {message}" + os.linesep
+        if record["exception"]:
+            new_format += "{exception}" + os.linesep
+        return new_format
+
+    logger.add(
+        logger_settings.file or sys.stderr,
+        format=log_format,
+        level=logger_settings.level,
+        colorize=True,
+        retention=logger_settings.retention,
+        rotation=logger_settings.rotation,
+        backtrace=False,
+        diagnose=False,
+    )
+
+
+if settings.rabbitmq.logger:
+    configure_logger(settings.rabbitmq.logger)
+    middlewares = [LoggerMiddleware]
+else:
+    middlewares = []
+
+
 broker = RabbitBroker(
     host=settings.rabbitmq.host,
     port=settings.rabbitmq.port,
     login=settings.rabbitmq.user,
     password=settings.rabbitmq.password,
     virtualhost=settings.rabbitmq.vhost,
+    middlewares=middlewares,
 )
 broker.include_router(router)
 app = FastStream(broker)
