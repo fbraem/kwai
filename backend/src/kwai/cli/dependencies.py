63e2e8e736f2fe5c36b4e77a2051e168278d7613
zumuta <franky.braem@zumuta.be>
2024-01-24 20:58:26 +0100

    chore: add redis dependency

--- backend/src/kwai/cli/dependencies.py
+++ backend/src/kwai/cli/dependencies.py
@@ -4,6 +4,7 @@ from typing import AsyncGenerator
 
 import inject
 from inject import Binder
+from redis.asyncio import Redis
 
 from kwai.core.db.database import Database
 from kwai.core.settings import Settings, get_settings
@@ -20,9 +21,25 @@ async def create_database(settings: Settings) -> AsyncGenerator[Database, None]:
         await database.close()
 
 
+@contextlib.asynccontextmanager
+@inject.autoparams()
+async def create_redis(settings: Settings) -> AsyncGenerator[Redis, None]:
+    """Create the Redis dependency."""
+    redis = Redis(
+        host=settings.redis.host,
+        port=settings.redis.port,
+        password=settings.redis.password,
+    )
+    try:
+        yield redis
+    finally:
+        await redis.aclose()
+
+
 def _configure_dependencies(binder: Binder):
     binder.bind_to_provider(Settings, get_settings)
     binder.bind_to_provider(Database, create_database)
+    binder.bind_to_provider(Redis, create_redis)
 
 
 def configure():
