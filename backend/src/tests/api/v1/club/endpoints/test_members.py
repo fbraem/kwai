517355260ba6056989403cc545f6c3ff50d34b0d
zumuta <franky.braem@zumuta.be>
2024-02-29 19:24:30 +0100

    fix: file upload needs a file handle

--- backend/src/tests/api/v1/club/endpoints/test_members.py
+++ backend/src/tests/api/v1/club/endpoints/test_members.py
@@ -13,7 +13,11 @@ def test_import_members(secure_client: TestClient):
     """Test /api/v1/club/members."""
     filename = Path(__file__).parent / "data" / "flemish_members_test.csv"
 
-    response = secure_client.post(
-        "/api/v1/club/members/upload", files={"member_file": str(filename)}
-    )
-    assert response.status_code == status.HTTP_200_OK
+    with open(filename, "rb") as file_handle:
+        response = secure_client.post(
+            "/api/v1/club/members/upload", files={"member_file": file_handle}
+        )
+        assert response.status_code == status.HTTP_200_OK
+        result = response.json()
+        assert "members" in result, "There should be a 'members' property."
+        assert len(result["members"]) == 2, "There should be 2 members uploaded."
