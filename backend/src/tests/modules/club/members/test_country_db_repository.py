2394161f9e101d5cd04b345efa51e21c43cea051
zumuta <franky.braem@zumuta.be>
2024-03-30 16:13:42 +0100

    test: use fixtures from club country

--- backend/src/tests/modules/club/members/test_country_db_repository.py
+++ backend/src/tests/modules/club/members/test_country_db_repository.py
@@ -1,7 +1,5 @@
 """Module for testing the country db repository."""
 
-from typing import AsyncGenerator
-
 import pytest
 
 from kwai.core.db.database import Database
@@ -19,33 +17,28 @@ async def country_repo(database: Database) -> CountryRepository:
     return CountryDbRepository(database)
 
 
-@pytest.fixture(scope="function")
-async def country(
-    country_repo: CountryRepository, make_country
-) -> AsyncGenerator[CountryEntity, None]:
-    """A fixture for a country in the database."""
-    country = await country_repo.create(
-        make_country(iso_2="XX", iso_3="XXX", name="Test")
-    )
-    yield country
-    await country_repo.delete(country)
-
-
-async def test_create_country(country):
+async def test_create_country(make_country_in_db):
     """Test creating a country."""
-    assert country, "Country should be created"
+    country = await make_country_in_db(
+        CountryEntity(iso_2="XX", iso_3="XXX", name="Test")
+    )
+    assert not country.id.is_empty(), "Country should be created"
 
 
 async def test_get_country_by_iso_2(
-    country_repo: CountryRepository, country: CountryEntity
+    country_repo: CountryRepository, make_country_in_db
 ):
     """Testing getting a country with an iso_2 code."""
+    await make_country_in_db(CountryEntity(iso_2="XX", iso_3="XXX", name="Test"))
     country = await country_repo.get_by_iso_2("XX")
     assert country.iso_2 == "XX", "The returned country should have iso_2 XX."
 
 
-async def test_delete_country(country_repo: CountryRepository, country: CountryEntity):
+async def test_delete_country(country_repo: CountryRepository, make_country_in_db):
     """Test deleting a country with an iso_2 code."""
+    country = await make_country_in_db(
+        CountryEntity(iso_2="XX", iso_3="XXX", name="Test")
+    )
     await country_repo.delete(country)
     with pytest.raises(CountryNotFoundException):
         await country_repo.get_by_iso_2("XX")
