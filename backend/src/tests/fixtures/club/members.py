42c70391fb516c82595dc965cf4c4e5783056fd1
zumuta <franky.braem@zumuta.be>
2024-04-04 15:31:35 +0200

    test: fixtures for member(s)

---
+++ backend/src/tests/fixtures/club/members.py
@@ -0,0 +1,61 @@
+"""Module for defining fixtures of members."""
+
+from typing import Callable, TypeAlias
+
+import pytest
+
+from kwai.core.db.database import Database
+from kwai.core.db.uow import UnitOfWork
+from kwai.core.domain.value_objects.date import Date
+from kwai.modules.club.members.member import MemberEntity
+from kwai.modules.club.members.member_db_repository import MemberDbRepository
+from kwai.modules.club.members.person import PersonEntity
+from kwai.modules.club.members.value_objects import License
+
+MemberFixtureFactory: TypeAlias = Callable[[License | None], MemberEntity]
+
+
+@pytest.fixture
+def make_member(make_person) -> MemberFixtureFactory:
+    """A factory fixture for a member."""
+
+    def _make_member_entity(
+        *, license: License | None = None, person: PersonEntity | None = None
+    ) -> MemberEntity:
+        license = license or License(
+            number="12345678", end_date=Date.today().add(years=1)
+        )
+        person = person or make_person()
+        return MemberEntity(license=license, person=person)
+
+    return _make_member_entity
+
+
+@pytest.fixture
+def make_member_in_db(
+    request,
+    event_loop,
+    database: Database,
+    make_member,
+    make_person_in_db,
+):
+    """A fixture for a member in the database."""
+
+    async def _make_member_in_db(member: MemberEntity | None = None) -> MemberEntity:
+        member = member or make_member(person=await make_person_in_db())
+        repo = MemberDbRepository(database)
+        async with UnitOfWork(database):
+            member = await repo.create(member)
+
+        def cleanup():
+            async def acleanup():
+                async with UnitOfWork(database):
+                    await repo.delete(member)
+
+            event_loop.run_until_complete(acleanup())
+
+        request.addfinalizer(cleanup)
+
+        return member
+
+    return _make_member_in_db
--- backend/src/tests/modules/club/conftest.py
+++ backend/src/tests/modules/club/conftest.py
@@ -1,6 +1,6 @@
 """Module for defining common fixtures for testing code related to members."""
 
 from tests.fixtures.club.countries import *  # noqa
-from tests.fixtures.club.members import *  # noqa
 from tests.fixtures.club.contacts import *  # noqa
 from tests.fixtures.club.persons import *  # noqa
+from tests.fixtures.club.members import *  # noqa
