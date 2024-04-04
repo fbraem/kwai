1dd3f30e6acb8c2f1c27112374eec1f57ffed5ee
zumuta <franky.braem@zumuta.be>
2024-04-04 13:55:54 +0200

    refactor: add person repository

---
+++ backend/src/tests/fixtures/club/persons.py
@@ -0,0 +1,70 @@
+"""Module for defining fixtures for persons."""
+
+import pytest
+
+from kwai.core.db.database import Database
+from kwai.core.db.uow import UnitOfWork
+from kwai.core.domain.value_objects.date import Date
+from kwai.core.domain.value_objects.name import Name
+from kwai.modules.club.members.contact import ContactEntity
+from kwai.modules.club.members.country import CountryEntity
+from kwai.modules.club.members.person import PersonEntity
+from kwai.modules.club.members.person_db_repository import PersonDbRepository
+from kwai.modules.club.members.value_objects import Birthdate, Gender
+
+
+@pytest.fixture
+def make_person(make_contact, country_japan):
+    """A factory fixture for a person."""
+
+    def _make_person(
+        name: Name | None = None,
+        gender: Gender | None = None,
+        birthdate: Birthdate | None = None,
+        nationality: CountryEntity | None = None,
+        contact: ContactEntity | None = None,
+    ) -> PersonEntity:
+        return PersonEntity(
+            name=name or Name(first_name="Jigoro", last_name="Kano"),
+            gender=gender or Gender.MALE,
+            birthdate=birthdate or Birthdate(Date.create(year=1860, month=10, day=28)),
+            nationality=nationality or country_japan,
+            contact=contact or make_contact(),
+        )
+
+    return _make_person
+
+
+@pytest.fixture
+def make_person_in_db(
+    request,
+    event_loop,
+    database: Database,
+    make_person,
+    make_contact_in_db,
+    make_country_in_db,
+    country_japan,
+):
+    """A factory fixture for a person in a database."""
+
+    async def _make_person_in_db(person: PersonEntity | None = None) -> PersonEntity:
+        person = person or make_person(
+            contact=await make_contact_in_db(),
+            nationality=await make_country_in_db(country_japan),
+        )
+        repo = PersonDbRepository(database)
+        async with UnitOfWork(database):
+            person = await repo.create(person)
+
+        def cleanup():
+            async def acleanup():
+                async with UnitOfWork(database):
+                    await repo.delete(person)
+
+            event_loop.run_until_complete(acleanup())
+
+        request.addfinalizer(cleanup)
+
+        return person
+
+    return _make_person_in_db
--- backend/src/tests/modules/club/conftest.py
+++ backend/src/tests/modules/club/conftest.py
@@ -3,3 +3,4 @@
 from tests.fixtures.club.countries import *  # noqa
 from tests.fixtures.club.members import *  # noqa
 from tests.fixtures.club.contacts import *  # noqa
+from tests.fixtures.club.persons import *  # noqa
