c89daad9ec50549613020dfad79990c4791e8bee
zumuta <franky.braem@zumuta.be>
2024-04-14 18:46:12 +0200

    feat: use button or a tag

--- frontend/packages/kwai-ui/src/form/Button.vue
+++ frontend/packages/kwai-ui/src/form/Button.vue
@@ -1,13 +1,22 @@
 <script setup lang="ts">
+import { computed } from 'vue';
+
 defineEmits(['click']);
+interface Props {
+  button?: boolean
+}
+const props = defineProps<Props>();
+
+const tag = computed(() => props.button ? 'button' : 'a');
 </script>
 
 <template>
-  <a
+  <component
+    :is="tag"
     class="hover:cursor-pointer text-sm font-bold px-12 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1"
     tabindex="0"
     @click="$emit('click')"
   >
     <slot />
-  </a>
+  </component>
 </template>
