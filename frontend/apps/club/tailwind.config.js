b8c180f7e8124fd080062ece0fe229b52929ea2a
zumuta <franky.braem@zumuta.be>
2024-04-18 20:49:32 +0200

    ci: add @indielayer/ui

--- frontend/apps/club/tailwind.config.js
+++ frontend/apps/club/tailwind.config.js
@@ -1,12 +1,18 @@
 /** @type {import('tailwindcss').Config} */
+import colors from 'tailwindcss/colors';
 module.exports = {
   content: [
     './index.html',
     './src/**/*.{html,js,ts,vue,jsx,tsx}',
     '../../packages/kwai-ui/src/**/*.{html,js,ts,vue,jsx,tsx}',
+    'node_modules/@indielayer/ui/{lib,src}/**/*',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: colors.green,
+      },
+    },
   },
   plugins: [],
 };
