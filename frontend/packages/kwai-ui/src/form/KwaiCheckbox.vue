b30408a38c28a46aa9b578becbf9bfc6af1f2159
zumuta <franky.braem@zumuta.be>
2024-05-17 19:12:22 +0200

    refactor: replace CheckBox with KwaiCheckbox

---
+++ frontend/packages/kwai-ui/src/form/KwaiCheckbox.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import Checkbox from 'primevue/checkbox';
+
+const model = defineModel<boolean>();
+
+defineOptions({
+  inheritAttrs: false,
+});
+</script>
+
+<template>
+  <div class="flex items-center">
+    <Checkbox
+      v-model="model"
+      v-bind="$attrs"
+      binary
+    />
+    <label
+      class="ml-2"
+      :for="$attrs.id as string"
+    >
+      <slot name="label" />
+    </label>
+  </div>
+</template>
