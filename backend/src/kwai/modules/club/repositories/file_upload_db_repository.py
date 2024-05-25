25d28b3d2f542042b585b07fc2b15a45f822b08b
zumuta <franky.braem@zumuta.be>
2024-05-25 15:10:54 +0200

    chore: add save_member

--- backend/src/kwai/modules/club/repositories/file_upload_db_repository.py
+++ backend/src/kwai/modules/club/repositories/file_upload_db_repository.py
@@ -3,7 +3,8 @@
 from kwai.core.db.database import Database
 from kwai.core.domain.entity import Entity
 from kwai.modules.club.domain.file_upload import FileUploadEntity, FileUploadIdentifier
-from kwai.modules.club.repositories._tables import FileUploadRow
+from kwai.modules.club.domain.member import MemberEntity
+from kwai.modules.club.repositories._tables import FileUploadRow, MemberUploadRow
 from kwai.modules.club.repositories.file_upload_repository import FileUploadRepository
 
 
@@ -22,5 +23,10 @@ class FileUploadDbRepository(FileUploadRepository):
         new_id = await self._database.insert(
             FileUploadRow.__table_name__, FileUploadRow.persist(file_upload)
         )
-        await self._database.commit()
         return Entity.replace(file_upload, id_=FileUploadIdentifier(new_id))
+
+    async def save_member(self, file_upload: FileUploadEntity, member: MemberEntity):
+        await self._database.insert(
+            MemberUploadRow.__table_name__,
+            MemberUploadRow.persist(file_upload, member),
+        )
--- backend/src/kwai/modules/club/repositories/file_upload_repository.py
+++ backend/src/kwai/modules/club/repositories/file_upload_repository.py
@@ -3,6 +3,7 @@
 from abc import ABC, abstractmethod
 
 from kwai.modules.club.domain.file_upload import FileUploadEntity
+from kwai.modules.club.domain.member import MemberEntity
 
 
 class FileUploadRepository(ABC):
@@ -17,5 +18,15 @@ class FileUploadRepository(ABC):
 
         Args:
             file_upload: A fileupload to save.
+        """
+        raise NotImplementedError
+
+    @abstractmethod
+    async def save_member(self, file_upload: FileUploadEntity, member: MemberEntity):
+        """Save a member imported from the file upload.
+
+        Args:
+            file_upload: The file upload.
+            member: The member from the file upload.
         """
         raise NotImplementedError
