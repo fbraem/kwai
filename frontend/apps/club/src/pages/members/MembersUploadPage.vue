a8e8a7bf87cbb33db772fba30d5d4ab5c3f31bb2
zumuta <franky.braem@zumuta.be>
2024-05-26 19:39:27 +0200

    feat: preview of members upload

--- frontend/apps/club/src/pages/members/MembersUploadPage.vue
+++ frontend/apps/club/src/pages/members/MembersUploadPage.vue
@@ -6,6 +6,7 @@ import {
   ContainerSectionTitle,
   KwaiFileUpload,
   KwaiCheckbox,
+  ErrorAlert,
 } from '@kwai/ui';
 import { type Ref, ref } from 'vue';
 import { useHttpApi, type JsonApiErrorType } from '@kwai/api';
@@ -14,7 +15,6 @@ import { MemberDocumentSchema, type Members, transform } from '@root/composables
 const { t } = useI18n({ useScope: 'global' });
 
 const useMemberUpload = (files: File[]) => {
-  const members: Ref<Members|null> = ref(null);
   (async() => {
     const formData = new FormData();
     formData.append('member_file', files[0]);
@@ -27,7 +27,6 @@ const useMemberUpload = (files: File[]) => {
       .then(json => {
         const result = MemberDocumentSchema.safeParse(json);
         if (result.success) {
-          console.log(result.data);
           errors.value = result.data.errors ?? [];
           count.value = result.data.meta?.count ?? 0;
           members.value = transform(result.data) as Members;
@@ -57,20 +56,94 @@ const upload = (files: File[]) => {
       {{ t('members_upload.title') }}
     </ContainerSectionTitle>
     <ContainerSectionContent>
-      {{ t('members_upload.description') }}
-      <KwaiFileUpload
-        name="test"
-        :uploader="upload"
-        :max-number-of-files="1"
-      />
+      <div>
+        {{ t('members_upload.description') }}
+      </div>
+      <div class="w-full flex items-center space-x-5">
+        <div class="w-2/3">
+          <KwaiFileUpload
+            name="test"
+            :uploader="upload"
+            :max-number-of-files="1"
+          />
+        </div>
+        <div class="w-1/3 flex flex-col items-center">
+          <p class="text-sm">
+            {{ t('members_upload.preview_description') }}
+          </p>
+          <KwaiCheckbox v-model="preview">
+            <template #label>
+              {{ t('members_upload.preview') }}
+            </template>
+          </KwaiCheckbox>
+        </div>
+      </div>
     </ContainerSectionContent>
-    <KwaiCheckbox v-model="preview">
-      <template #label>
-        Preview
-      </template>
-    </KwaiCheckbox>
-    {{ errors }}
-    {{ members }}
+    <ErrorAlert v-if="errors.length > 0">
+      {{ t('members_upload.error.message') }}
+    </ErrorAlert>
+    <template v-if="errors.length > 0">
+      <table class="divide-y divide-gray-400 w-full rounded-lg">
+        <thead>
+          <tr>
+            <th class="w-1/6 px-6 py-3 text-left text-sm uppercase">
+              {{ t('members_upload.error.row') }}
+            </th>
+            <th class="px-6 py-3 text-left text-sm uppercase">
+              {{ t('members_upload.error.description') }}
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr
+            v-for="error in errors"
+            :key="error.source!.pointer"
+          >
+            <th class="w-1/6 px-6 py-3 text-left">
+              {{ error.source!.pointer }}
+            </th>
+            <td class="px-6 py-3 text-left">
+              {{ error.detail }}
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </template>
+    <template v-if="members && members.items">
+      <div>
+        <p>
+          Er zijn {{ members.meta.count }} leden gevonden in dit bestand.
+        </p>
+      </div>
+    </template>
+    <table
+      v-if="members && members.items"
+      class="divide-y divide-gray-400 w-full rounded-lg"
+    >
+      <thead>
+        <tr>
+          <th class="w-1/6 px-6 py-3 text-left text-sm uppercase">
+            {{ t('members_upload.error.row') }}
+          </th>
+          <th class="px-6 py-3 text-left text-sm uppercase">
+            {{ t('members_upload.error.description') }}
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr
+          v-for="(member, index) in members.items"
+          :key="index"
+        >
+          <th class="w-1/6 px-6 py-1 text-left">
+            {{ index + 1 }}
+          </th>
+          <td class="px-6 py-1 text-left">
+            {{ member.person.lastName }} {{ member.person.firstName }}
+          </td>
+        </tr>
+      </tbody>
+    </table>
   </ContainerSection>
 </template>
 
