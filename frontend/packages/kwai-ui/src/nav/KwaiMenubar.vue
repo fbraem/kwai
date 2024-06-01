e606082d711214b6c945998b5875d938e830419f
zumuta <franky.braem@zumuta.be>
2024-06-01 16:13:18 +0200

    feat: add KwaiMenubar

---
+++ frontend/packages/kwai-ui/src/nav/KwaiMenubar.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+import Menubar from 'primevue/menubar';
+import { MenuItem } from '../types';
+import { computed } from 'vue';
+
+interface Props {
+  items: MenuItem[]
+}
+const props = defineProps<Props>();
+
+const menuItems = computed(() => {
+  return props.items.map(item => ({
+    label: item.title,
+    command: item.method,
+    route: item.route,
+    disabled: item.disabled ?? false,
+  }));
+});
+</script>
+
+<template>
+  <Menubar
+    :model="menuItems"
+    pt:root:class="bg-primary-500 text-primary-text w-full lg:px-6 lg:mx-auto lg:max-w-6xl"
+    :pt-options="{ mergeSections: false, mergeProps: true }"
+  />
+</template>
+
+<style>
+li[data-pc-section="menuitem"] {
+  @apply text-gray-700;
+}
+</style>
--- frontend/packages/kwai-ui/src/nav/index.ts
+++ frontend/packages/kwai-ui/src/nav/index.ts
@@ -7,3 +7,4 @@ export { default as ToolbarLogo } from './ToolbarLogo.vue';
 export { default as LinkTag } from './LinkTag.vue';
 export { default as OffsetPagination } from './OffsetPagination.vue';
 export { usePagination } from './usePagination';
+export { default as KwaiMenubar } from './KwaiMenubar.vue';
