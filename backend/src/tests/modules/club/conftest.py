7009b8702b1c8e9b0d47b02187615e610f73bf5d
zumuta <franky.braem@zumuta.be>
2024-06-02 16:19:44 +0200

    refactor: rename coach.py into coaches.py

--- backend/src/tests/modules/club/conftest.py
+++ backend/src/tests/modules/club/conftest.py
@@ -4,4 +4,4 @@ from tests.fixtures.club.countries import *  # noqa
 from tests.fixtures.club.contacts import *  # noqa
 from tests.fixtures.club.persons import *  # noqa
 from tests.fixtures.club.members import *  # noqa
-from tests.fixtures.club.coach import *  # noqa
+from tests.fixtures.club.coaches import *  # noqa
