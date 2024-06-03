03ae203ee96a28f608bf51042cdade5506ce5fd1
zumuta <franky.braem@zumuta.be>
2024-06-03 15:35:12 +0200

    test: use make_training_definition_in_db

--- backend/src/tests/modules/training/test_get_training_definitions.py
+++ backend/src/tests/modules/training/test_get_training_definitions.py
@@ -1,7 +1,5 @@
 """Module for testing the use case "Get Training Definitions"."""
 
-from typing import AsyncIterator
-
 import pytest
 from kwai.core.db.database import Database
 from kwai.modules.training.get_training_definitions import (
@@ -10,7 +8,6 @@ from kwai.modules.training.get_training_definitions import (
 )
 from kwai.modules.training.trainings.training_definition import (
     TrainingDefinitionEntity,
-    TrainingDefinitionIdentifier,
 )
 from kwai.modules.training.trainings.training_definition_db_repository import (
     TrainingDefinitionDbRepository,
@@ -18,14 +15,6 @@ from kwai.modules.training.trainings.training_definition_db_repository import (
 from kwai.modules.training.trainings.training_definition_repository import (
     TrainingDefinitionRepository,
 )
-
-
-async def _find(iterator: AsyncIterator, id_: TrainingDefinitionIdentifier):
-    """Search for an entity with the given id."""
-    async for entity in iterator:
-        if entity.id == id_:
-            return entity
-    return None
 
 
 @pytest.fixture(scope="module")
@@ -45,14 +34,15 @@ async def saved_training_definition(
 
 async def test_get_training_definitions(
     training_definition_repo: TrainingDefinitionRepository,
-    saved_training_definition: TrainingDefinitionEntity,
+    make_training_definition_in_db,
 ):
     """Test a successful execution of the use case."""
+    definition = await make_training_definition_in_db()
     command = GetTrainingDefinitionsCommand()
     count, iterator = await GetTrainingDefinitions(training_definition_repo).execute(
         command
     )
     assert count >= 1, "There should be at least a training definition"
 
-    entity = await _find(iterator, saved_training_definition.id)
-    assert entity is not None, "The definition should be part of the data"
+    definitions = {definition.id: definition async for definition in iterator}
+    assert definition.id in definitions, "The definition should be part of the data"
