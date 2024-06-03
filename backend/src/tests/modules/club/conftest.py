37d18370a7485447b11f062e65928a20d9952faf
zumuta <franky.braem@zumuta.be>
2024-06-03 11:36:19 +0200

    test: create team

--- backend/src/tests/modules/club/conftest.py
+++ backend/src/tests/modules/club/conftest.py
@@ -5,3 +5,4 @@ from tests.fixtures.club.contacts import *  # noqa
 from tests.fixtures.club.persons import *  # noqa
 from tests.fixtures.club.members import *  # noqa
 from tests.fixtures.club.coaches import *  # noqa
+from tests.fixtures.club.teams import *  # noqa
---
+++ backend/src/tests/modules/club/repositories/test_team_db_repository.py
@@ -0,0 +1,7 @@
+"""Module for testing the team db repository."""
+
+
+async def test_create_team(make_team_in_db):
+    """Test creating a team in the database."""
+    team = await make_team_in_db()
+    assert team is not None, "There should be a team in the database."
