4ab99b8e8a197f351a43acdcee1d9582e77858c2
zumuta <franky.braem@zumuta.be>
2024-06-10 21:53:12 +0200

    feat: add members

--- backend/src/kwai/modules/club/domain/team.py
+++ backend/src/kwai/modules/club/domain/team.py
@@ -3,6 +3,7 @@
 from kwai.core.domain.entity import Entity
 from kwai.core.domain.value_objects.identifier import IntIdentifier
 from kwai.core.domain.value_objects.traceable_time import TraceableTime
+from kwai.modules.club.domain.team_member import TeamMemberEntity
 
 TeamIdentifier = IntIdentifier
 
@@ -17,12 +18,14 @@ class TeamEntity(Entity[TeamIdentifier]):
         name: str,
         active: bool = True,
         remark: str = "",
+        members: list[TeamMemberEntity] = None,
         traceable_time: TraceableTime | None = None,
     ):
         super().__init__(id_ or TeamIdentifier())
         self._name = name
         self._active = active
         self._remark = remark
+        self._members = [] if members is None else members.copy()
         self._traceable_time = traceable_time or TraceableTime()
 
     @property
@@ -44,3 +47,11 @@ class TeamEntity(Entity[TeamIdentifier]):
     def traceable_time(self) -> TraceableTime:
         """Return the traceable_time."""
         return self._traceable_time
+
+    @property
+    def members(self) -> list[TeamMemberEntity]:
+        """Return the members.
+
+        Note: the returned list is a copy.
+        """
+        return self._members.copy()
