d0076f0d941165fb6ced20565f30dec838d9d972
zumuta <franky.braem@zumuta.be>
2024-06-12 21:11:18 +0200

    test: add entity fixtures

---
+++ backend/src/tests/modules/teams/conftest.py
@@ -0,0 +1,8 @@
+"""Module for common fixtures used for testing code of the club module."""
+
+from tests.fixtures.club.countries import *  # noqa
+from tests.fixtures.club.contacts import *  # noqa
+from tests.fixtures.club.persons import *  # noqa
+from tests.fixtures.club.members import *  # noqa
+from tests.fixtures.club.coaches import *  # noqa
+from tests.fixtures.club.teams import *  # noqa
