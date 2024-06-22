5a5d89b4bf26493ae625e2a736d855ee4d036b94
zumuta <franky.braem@zumuta.be>
2024-06-22 16:52:49 +0200

    tests: add test for TeamMemberDbQuery

---
+++ backend/src/tests/modules/teams/repositories/test_team_member_db_query.py
@@ -0,0 +1,21 @@
+"""Module for testing the TeamMemberDbQuery class."""
+
+import pytest
+from kwai.core.db.database import Database
+from kwai.modules.teams.domain.team import TeamIdentifier
+from kwai.modules.teams.repositories.team_member_db_query import TeamMemberDbQuery
+
+
+@pytest.fixture
+def query(database: Database) -> TeamMemberDbQuery:
+    """A fixture for a team member database query."""
+    return TeamMemberDbQuery(database)
+
+
+async def test_filter_by_teams(query: TeamMemberDbQuery):
+    """Test filter by teams."""
+    query.filter_by_teams(TeamIdentifier(1))
+    try:
+        await query.fetch_team_members()
+    except Exception as exc:
+        pytest.fail(f"An exception occurred: f{exc}")
