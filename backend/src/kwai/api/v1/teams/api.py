9954cced61e9ece7115162b0ce1f83b062690896
zumuta <franky.braem@zumuta.be>
2024-07-04 18:21:37 +0200

    feat: get team

--- backend/src/kwai/api/v1/teams/api.py
+++ backend/src/kwai/api/v1/teams/api.py
@@ -5,9 +5,10 @@ from typing import Annotated
 from fastapi import APIRouter, Depends
 
 from kwai.api.dependencies import create_database
-from kwai.api.v1.teams.presenters import JsonApiTeamsPresenter
+from kwai.api.v1.teams.presenters import JsonApiTeamPresenter, JsonApiTeamsPresenter
 from kwai.api.v1.teams.schemas import TeamDocument
 from kwai.core.db.database import Database
+from kwai.modules.teams.get_team import GetTeam, GetTeamCommand
 from kwai.modules.teams.get_teams import GetTeams, GetTeamsCommand
 from kwai.modules.teams.repositories.team_db_repository import TeamDbRepository
 
@@ -22,4 +23,16 @@ async def get_teams(
     presenter = JsonApiTeamsPresenter()
     command = GetTeamsCommand(offset=0, limit=0)
     await GetTeams(TeamDbRepository(database), presenter).execute(command)
+    return presenter.get_document()
+
+
+@router.get("/teams/{id}")
+async def get_team(
+    id: int,
+    database: Annotated[Database, Depends(create_database)],
+) -> TeamDocument:
+    """Get the team with the given id."""
+    presenter = JsonApiTeamPresenter()
+    command = GetTeamCommand(id=id)
+    await GetTeam(TeamDbRepository(database), presenter).execute(command)
     return presenter.get_document()
--- backend/src/kwai/api/v1/teams/presenters.py
+++ backend/src/kwai/api/v1/teams/presenters.py
@@ -1,9 +1,16 @@
 """Module for defining presenters of the teams api."""
 
 from kwai.api.v1.teams.schemas import TeamDocument
-from kwai.core.domain.presenter import AsyncPresenter, IterableResult
+from kwai.core.domain.presenter import AsyncPresenter, IterableResult, Presenter
 from kwai.core.json_api import JsonApiPresenter, Meta
 from kwai.modules.teams.domain.team import TeamEntity
+
+
+class JsonApiTeamPresenter(JsonApiPresenter[TeamDocument], Presenter[TeamEntity]):
+    """A presenter that transform a team entity into a JSON:API document."""
+
+    def present(self, team: TeamEntity) -> None:
+        self._document = TeamDocument.create(team)
 
 
 class JsonApiTeamsPresenter(
