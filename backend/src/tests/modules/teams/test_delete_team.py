112290c9c53c114a02641ebbc283c6bd9e579a0d
zumuta <franky.braem@zumuta.be>
2024-07-11 20:16:21 +0200

    feat: add delete team

---
+++ backend/src/tests/modules/teams/test_delete_team.py
@@ -0,0 +1,18 @@
+"""Module for testing the Delete Team use case."""
+
+import pytest
+from kwai.core.db.database import Database
+from kwai.modules.teams.delete_team import DeleteTeam, DeleteTeamCommand
+from kwai.modules.teams.repositories.team_db_repository import TeamDbRepository
+from kwai.modules.teams.repositories.team_repository import TeamNotFoundException
+
+
+async def test_delete_team(database: Database, make_team_in_db):
+    """Test deleting a team."""
+    team = await make_team_in_db()
+    command = DeleteTeamCommand(id=team.id.value)
+    team_repo = TeamDbRepository(database)
+    await DeleteTeam(team_repo).execute(command)
+
+    with pytest.raises(TeamNotFoundException):
+        await team_repo.get(team_repo.create_query().filter_by_id(team.id))
