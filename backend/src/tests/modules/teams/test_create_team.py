5cad3567f2cb403652ba617cd59497c882ee9a6e
zumuta <franky.braem@zumuta.be>
2024-07-12 14:37:42 +0200

    refactor: use team_presenter fixture

--- backend/src/tests/modules/teams/test_create_team.py
+++ backend/src/tests/modules/teams/test_create_team.py
@@ -2,37 +2,19 @@
 
 import pytest
 from kwai.core.db.database import Database
-from kwai.core.domain.presenter import Presenter
 from kwai.modules.teams.create_team import CreateTeam, CreateTeamCommand
-from kwai.modules.teams.domain.team import TeamEntity
 from kwai.modules.teams.repositories.team_db_repository import TeamDbRepository
 
 pytestmark = pytest.mark.db
 
 
-class TestPresenter(Presenter[TeamEntity]):
-    """A dummy presenter for checking the use case result."""
-
-    def __init__(self):
-        self._entity = None
-
-    @property
-    def entity(self) -> TeamEntity:
-        """Return the entity."""
-        return self._entity
-
-    def present(self, use_case_result: TeamEntity) -> None:
-        self._entity = use_case_result
-
-
-async def test_create_team(database: Database, make_team) -> None:
+async def test_create_team(database: Database, make_team, team_presenter) -> None:
     """Test the use case 'Create Team'."""
-    presenter = TestPresenter()
     team = make_team()
     command = CreateTeamCommand(
         name=team.name,
         active=team.is_active,
         remark=team.remark,
     )
-    await CreateTeam(TeamDbRepository(database), presenter).execute((command))
-    assert presenter.entity is not None, "The team should be created"
+    await CreateTeam(TeamDbRepository(database), team_presenter).execute((command))
+    assert team_presenter.entity is not None, "The team should be created"
