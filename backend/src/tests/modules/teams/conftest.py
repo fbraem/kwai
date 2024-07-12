dc82df9cb17d0a2684e61277f7cc2d9d650cbee2
zumuta <franky.braem@zumuta.be>
2024-07-12 14:34:31 +0200

    test: add TeamPresenter fixture

--- backend/src/tests/modules/teams/conftest.py
+++ backend/src/tests/modules/teams/conftest.py
@@ -1,9 +1,35 @@
 """Module for common fixtures used for testing code of the club module."""
 
-from tests.fixtures.club.countries import *  # noqa
-from tests.fixtures.club.contacts import *  # noqa
-from tests.fixtures.club.persons import *  # noqa
-from tests.fixtures.club.members import *  # noqa
+import pytest
+from kwai.core.domain.presenter import Presenter
+from kwai.modules.teams.domain.team import TeamEntity
+
 from tests.fixtures.club.coaches import *  # noqa
-from tests.fixtures.teams.teams import *  # noqa
+from tests.fixtures.club.contacts import *  # noqa
+from tests.fixtures.club.countries import *  # noqa
+from tests.fixtures.club.members import *  # noqa
+from tests.fixtures.club.persons import *  # noqa
 from tests.fixtures.teams.team_members import *  # noqa
+from tests.fixtures.teams.teams import *  # noqa
+
+
+class TestPresenter(Presenter[TeamEntity]):
+    """A dummy presenter for checking the use case result."""
+
+    def __init__(self):
+        super().__init__()
+        self._entity = None
+
+    @property
+    def entity(self):
+        """Return the entity."""
+        return self._entity
+
+    def present(self, use_case_result: TeamEntity) -> None:
+        self._entity = use_case_result
+
+
+@pytest.fixture
+def team_presenter() -> TestPresenter:
+    """A fixture for a team presenter."""
+    return TestPresenter()
