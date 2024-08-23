38f6efcb80e19dc5ac30fb12bebdbeb4702b20d8
zumuta <franky.braem@zumuta.be>
2024-08-23 15:37:56 +0200

    refactor: add manifest_path fixture

--- backend/src/tests/frontend/conftest.py
+++ backend/src/tests/frontend/conftest.py
@@ -7,7 +7,12 @@ from kwai.frontend.manifest import Manifest
 
 
 @pytest.fixture
-def manifest() -> Manifest:
+def manifest_path() -> Path:
     """Load a manifest file from the data folder."""
-    manifest_file_path = Path(__file__).parent / "data" / "manifest.json"
-    return Manifest.load_from_file(manifest_file_path)
+    return Path(__file__).parent / "data" / "manifest.json"
+
+
+@pytest.fixture
+def manifest(manifest_path) -> Manifest:
+    """Load a manifest file from the data folder."""
+    return Manifest.load_from_file(manifest_path)
