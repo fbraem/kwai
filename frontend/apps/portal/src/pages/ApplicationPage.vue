0a72cdf099c09c0478e76d9c671d9a16b540812b
zumuta <franky.braem@zumuta.be>
2024-08-23 19:36:49 +0200

    fix: make sure ApplicationPage is refreshed when route is changed

--- frontend/apps/portal/src/pages/ApplicationPage.vue
+++ frontend/apps/portal/src/pages/ApplicationPage.vue
@@ -2,25 +2,35 @@
 import { useRoute, useRouter } from 'vue-router';
 import IntroSection from '@root/components/IntroSection.vue';
 import { useApplications } from '@root/composables/useApplication';
-import { computed, ref, toRef } from 'vue';
+import { computed, ref, toRef, watch } from 'vue';
 import { usePages } from '@root/composables/usePage';
 import FullArticle from '@root/components/FullArticle.vue';
 
 const route = useRoute();
-const applicationName = route.meta.application as string;
-const heroImageUrl = route.meta.heroImageUrl as string;
+
+const applicationName = toRef(route.meta.application as string);
+const heroImageUrl = toRef(route.meta.heroImageUrl as string);
+
+watch(route, (nv) => {
+  if (nv.meta.application) {
+    applicationName.value = nv.meta.application as string;
+  }
+  if (nv.meta.heroImageUrl) {
+    heroImageUrl.value = nv.meta.heroImageUrl as string;
+  }
+});
 
 // Application
 const { data: applications } = useApplications();
 const application = computed(() => {
   if (applications.value) {
-    return applications.value.find(application => application.name === applicationName);
+    return applications.value.find(application => application.name === applicationName.value);
   }
   return null;
 });
 
 // Pages
-const { data: pages } = usePages(toRef(applicationName));
+const { data: pages } = usePages(applicationName);
 const sortedPages = computed(() => {
   return [...pages.value || []].sort((a, b) => b.priority - a.priority);
 });
@@ -41,7 +51,10 @@ const currentPage = computed(() => {
     if (articleSection.value) articleSection.value.scrollIntoView(true);
     return sortedPages.value.find(page => page.id === route.query.page);
   }
-  return sortedPages.value[0];
+  if (sortedPages.value.length > 0) {
+    return sortedPages.value[0];
+  }
+  return null;
 });
 const router = useRouter();
 
@@ -55,6 +68,7 @@ const gotoPage = (id: string) => {
   <IntroSection
     :hero-image-url="heroImageUrl"
   >
+    <span class="text-white">{{ applicationName }}</span>
     <div class="container lg:max-w-5xl relative mx-auto flex h-full items-center">
       <div
         v-if="application"
