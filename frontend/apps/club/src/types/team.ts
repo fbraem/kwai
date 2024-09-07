27331b36b3dd192cc56337e00b57c8544be5673a
zumuta <franky.braem@zumuta.be>
2024-09-07 21:37:54 +0200

    feat: add TeamMember interface

--- frontend/apps/club/src/types/team.ts
+++ frontend/apps/club/src/types/team.ts
@@ -1,6 +1,20 @@
+import type { License } from '@root/types/member';
+import type { Country } from '@root/types/country';
+import type { DateType } from '@kwai/date';
+
+export interface TeamMember {
+  id: string,
+  name: string,
+  license: License,
+  gender: number,
+  birthdate: DateType,
+  nationality: Country,
+}
+
 export interface Team {
   id?: string,
   name: string,
   active: boolean,
-  remark: string
+  remark: string,
+  members: TeamMember[],
 }
