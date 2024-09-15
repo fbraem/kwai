6692b3de9f481f833174de8abc62cbf1115e40a5
zumuta <franky.braem@zumuta.be>
2024-09-15 18:43:01 +0200

    fix: remove @indielayer

--- frontend/apps/club/tailwind.config.js
+++ frontend/apps/club/tailwind.config.js
@@ -5,7 +5,6 @@ module.exports = {
     './index.html',
     './src/**/*.{html,js,ts,vue,jsx,tsx}',
     '../../packages/kwai-ui/src/**/*.{html,js,ts,vue,jsx,tsx}',
-    'node_modules/@indielayer/ui/{lib,src}/**/*',
   ],
   theme: {
     extend: {
