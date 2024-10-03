821e3067e899f34cb61fa5179f201fedcbedbeec
zumuta <franky.braem@zumuta.be>
2024-10-03 20:28:52 +0200

    feat: add KwaiCard and KwaiPanel

---
+++ frontend/packages/kwai-ui/src/card/KwaiCard.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+import Card from 'primevue/card';
+import { useSlots } from 'vue';
+
+const slots = useSlots();
+</script>
+
+<template>
+  <Card>
+    <template
+      v-if="!!slots.title"
+      #title
+    >
+      <slot name="title" />
+    </template>
+    <template
+      v-if="!!slots.content"
+      #content
+    >
+      <slot name="content" />
+    </template>
+    <template
+      v-if="!!slots.footer"
+      #footer
+    >
+      <slot name="footer" />
+    </template>
+  </Card>
+</template>
+
+<style scoped>
+
+</style>
---
+++ frontend/packages/kwai-ui/src/card/KwaiPanel.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+import Panel from 'primevue/panel';
+
+const preset = {
+  root: () => ({
+    class: 'border rounded-lg',
+  }),
+  header: () => ({
+    class: ['border-0'],
+  }),
+  content: () => ({
+    class: ['border-0'],
+  }),
+};
+</script>
+
+<template>
+  <Panel
+    :pt="preset"
+    :pt-options="{ mergeSections: true, mergeProps: true }"
+  >
+    <template
+      v-if="$slots.header"
+      #header
+    >
+      <slot name="header" />
+    </template>
+    <template
+      v-if="$slots.footer"
+      #footer
+    >
+      <slot name="footer" />
+    </template>
+    <slot />
+  </Panel>
+</template>
+
+<style scoped>
+
+</style>
