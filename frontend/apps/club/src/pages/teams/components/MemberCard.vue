673a40dcd455004ed3a6d5eeb11fc9a2fcc11332
zumuta <franky.braem@zumuta.be>
2024-10-03 20:26:55 +0200

    fix: MemberCard moved to teams/components

---
+++ frontend/apps/club/src/pages/teams/components/MemberCard.vue
@@ -0,0 +1,56 @@
+<script setup lang="ts">
+
+import { KwaiBadge, KwaiButton, KwaiCard } from '@kwai/ui';
+import type { Member } from '@root/types/member';
+import { computed } from 'vue';
+import { now } from '@kwai/date';
+import AddMemberIcon from '@root/components/icons/AddMemberIcon.vue';
+
+interface Props {
+  member: Member
+}
+const props = defineProps<Props>();
+const member = computed(() => props.member);
+const validLicense = computed(() => {
+  return !member.value.license.endDate.isBefore(now());
+});
+const age = computed(() => {
+  return now().get('year') - member.value.person.birthdate.get('year');
+});
+const emit = defineEmits(['add']);
+</script>
+
+<template>
+  <KwaiCard>
+    <template #title>
+      {{ member.person.lastName }} {{ member.person.firstName }}
+    </template>
+    <template #content>
+      <div class="grid grid-cols-2">
+        <div>Geboortedatum:</div>
+        <div>{{ member.person.birthdate.format('L') }} ({{ age }})</div>
+        <div>Licentie</div>
+        <div>
+          {{ member.license.number }} ({{ member.license.endDate.format('L') }})
+          <KwaiBadge
+            class="align-top"
+            :severity="validLicense ? 'success' : 'danger'"
+          />
+        </div>
+        <div>Nationaliteit:</div>
+        <div>{{ member.person.nationality.iso3 }}</div>
+      </div>
+    </template>
+    <template #footer>
+      <div class="flex justify-end">
+        <KwaiButton :method="() => emit('add')">
+          <AddMemberIcon class="w-4 h-4 fill-primary-text" />
+        </KwaiButton>
+      </div>
+    </template>
+  </KwaiCard>
+</template>
+
+<style scoped>
+
+</style>
