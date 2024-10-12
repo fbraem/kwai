a0e69c0770ff6b110e6b2903db262d4e9956d6e8
zumuta <franky.braem@zumuta.be>
2024-10-12 20:17:51 +0200

    fix: add team_members

--- backend/src/tests/api/v1/teams/conftest.py
+++ backend/src/tests/api/v1/teams/conftest.py
@@ -1,6 +1,7 @@
 """Module for defining common fixtures for testing the teams endpoints."""
 
 from tests.fixtures.teams.teams import *  # noqa
+from tests.fixtures.teams.team_members import *  # noqa
 from tests.fixtures.club.countries import *  # noqa
 from tests.fixtures.club.contacts import *  # noqa
 from tests.fixtures.club.persons import *  # noqa
