398da616e3bbd84feb730ad9d8c7bbc68f23a076
zumuta <franky.braem@zumuta.be>
2024-10-12 19:54:39 +0200

    fix: add member to team

--- backend/src/tests/fixtures/teams/team_members.py
+++ backend/src/tests/fixtures/teams/team_members.py
@@ -1,9 +1,11 @@
 """Module for defining factory fixtures for team members."""
 
 import pytest
+from kwai.core.db.database import Database
 from kwai.core.domain.value_objects.traceable_time import TraceableTime
 from kwai.modules.teams.domain.team import TeamEntity
 from kwai.modules.teams.domain.team_member import MemberEntity, TeamMember
+from kwai.modules.teams.repositories.team_db_repository import TeamDbRepository
 
 
 @pytest.fixture
@@ -30,13 +32,18 @@ def make_team_member(make_member):
 
 
 @pytest.fixture
-def make_team_member_in_db(make_team_member, make_member_in_db):
+def make_team_member_in_db(
+    database: Database, make_team_member, make_member_in_db, make_team_in_db
+):
     """A factory fixture for a team member in a database."""
 
     async def _make_team_member_in_db(
         member: MemberEntity | None = None, team: TeamEntity | None = None
     ) -> TeamMember:
         member = member or make_team_member(await make_member_in_db())
+        team = team or make_team_in_db()
+        if team is not None:
+            await TeamDbRepository(database).add_team_member(team, member)
         return member
 
     return _make_team_member_in_db
