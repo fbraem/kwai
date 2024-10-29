b6aa4c41425f2716733fb5bba393ce5a8b035c6b
zumuta <franky.braem@zumuta.be>
2024-10-29 20:42:12 +0100

    refactor: use KwaiMenubar

--- frontend/apps/author/src/components/AuthorToolbar.vue
+++ frontend/apps/author/src/components/AuthorToolbar.vue
@@ -2,25 +2,10 @@
 // eslint-disable-next-line import/no-absolute-path
 import logoUrl from '/logo.png';
 import { website } from '@kwai/config';
-import type { MenuItem } from '@kwai/ui';
-import { KwaiButton, ToolbarLogo, ToolbarMenu } from '@kwai/ui';
-import { useRouter } from 'vue-router';
-import { computed } from 'vue';
+import { useMenu, KwaiMenubar, KwaiButton, ToolbarLogo } from '@kwai/ui';
 import { isLoggedIn, useHttpLogout } from '@kwai/api';
 
-const router = useRouter();
-const menuItems = computed(() : MenuItem[] => {
-  const result: MenuItem[] = [];
-  for (const route of router.getRoutes()) {
-    if (route.meta.title) {
-      result.push({
-        title: route.meta.title as string,
-        route,
-      });
-    }
-  }
-  return result;
-});
+const menuItems = useMenu();
 
 const loggedIn = isLoggedIn;
 const logout = () => {
@@ -62,9 +47,8 @@ const logout = () => {
       </div>
     </div>
   </header>
-  <ToolbarMenu
-    :menu-items="menuItems"
-    class="bg-yellow-300"
+  <KwaiMenubar
+    :items="menuItems"
     item-class="text-gray-600 hover:text-black"
   />
 </template>
