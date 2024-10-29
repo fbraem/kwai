f7a39d545ae0c47188875512a878f1786f0964e0
zumuta <franky.braem@zumuta.be>
2024-10-29 20:42:26 +0100

    fix: set yellow primary color

--- frontend/apps/author/tailwind.config.js
+++ frontend/apps/author/tailwind.config.js
@@ -9,7 +9,18 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        primary: colors.yellow,
+        primary: {
+          50: colors.yellow['50'],
+          100: colors.yellow['100'],
+          200: colors.yellow['200'],
+          300: colors.yellow['300'],
+          400: colors.yellow['400'],
+          500: colors.yellow['400'],
+          600: colors.yellow['300'],
+          700: colors.yellow['500'],
+          800: colors.yellow['600'],
+          900: colors.yellow['700'],
+        },
         'primary-text': colors.white,
       },
     },
