71fad983365a8175a0e9c8bc6d967e686544be4e
zumuta <franky.braem@zumuta.be>
2024-10-29 20:21:49 +0100

    fix: login/logout

--- frontend/apps/portal/src/components/toolbar/ToolbarUser.vue
+++ frontend/apps/portal/src/components/toolbar/ToolbarUser.vue
@@ -1,11 +1,23 @@
 <script setup lang="ts">
 import { website } from '@kwai/config';
 import { KwaiButton } from '@kwai/ui';
+import { isLoggedIn, useHttpLogout } from '@kwai/api';
+
+const loggedIn = isLoggedIn;
+const logout = () => {
+  useHttpLogout();
+  window.location.reload();
+};
 </script>
 
 <template>
   <div class="flex flex-col place-items-end">
-    <div>
+    <div v-if="loggedIn">
+      <KwaiButton :method="logout">
+        Logout
+      </KwaiButton>
+    </div>
+    <div v-else>
       <KwaiButton :href="`${website.url}/apps/auth/login`">
         Login
       </KwaiButton>
