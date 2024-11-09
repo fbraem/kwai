853b8bd9acaa315d46093eac10fea911878c5ff8
zumuta <franky.braem@zumuta.be>
2024-11-09 18:56:11 +0100

    fix: check for production build

--- frontend/packages/kwai-config/vite.config.ts
+++ frontend/packages/kwai-config/vite.config.ts
@@ -3,21 +3,24 @@ import dts from 'vite-plugin-dts';
 import { defineConfig } from 'vite';
 import { resolve } from 'path';
 
-export default defineConfig({
-  plugins: [
-    toml(),
-    dts({ copyDtsFiles: true }),
-  ],
-  build: {
-    lib: {
-      entry: resolve(__dirname, 'src/config.toml'),
-      name: '@kwai/config',
-      fileName: 'kwai-config',
-    },
-    rollupOptions: {
-      output: {
-        exports: 'named',
+export default defineConfig(({ mode }) => {
+  const configFile = mode === 'production' ? 'src/config.production.toml' : 'src/config.toml';
+  return {
+    plugins: [
+      toml(),
+      dts({ copyDtsFiles: true }),
+    ],
+    build: {
+      lib: {
+        entry: resolve(__dirname, configFile),
+        name: '@kwai/config',
+        fileName: 'kwai-config',
+      },
+      rollupOptions: {
+        output: {
+          exports: 'named',
+        },
       },
     },
-  },
+  };
 });
