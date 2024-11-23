ef13b6546958cd85bab63649ff5dfa06079e235b
zumuta <franky.braem@zumuta.be>
2024-11-23 21:40:24 +0100

    fix: use main yellow color

--- frontend/apps/author/tailwind.config.js
+++ frontend/apps/author/tailwind.config.js
@@ -2,25 +2,13 @@
 import colors from 'tailwindcss/colors';
 module.exports = {
   content: [
-    './index.html',
     './src/**/*.{html,js,ts,vue,jsx,tsx}',
     '../../packages/kwai-ui/src/**/*.{html,js,ts,vue,jsx,tsx}',
   ],
   theme: {
     extend: {
       colors: {
-        primary: {
-          50: colors.yellow['50'],
-          100: colors.yellow['100'],
-          200: colors.yellow['200'],
-          300: colors.yellow['300'],
-          400: colors.yellow['400'],
-          500: colors.yellow['400'],
-          600: colors.yellow['300'],
-          700: colors.yellow['500'],
-          800: colors.yellow['600'],
-          900: colors.yellow['700'],
-        },
+        primary: colors.yellow,
         'primary-text': colors.white,
       },
     },
