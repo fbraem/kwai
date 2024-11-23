07cc7fdaefd319693d34372471d8f915f12b6a35
zumuta <franky.braem@zumuta.be>
2024-11-23 15:08:19 +0100

    docs: add api docs

--- backend/src/kwai/api/v1/auth/endpoints/user.py
+++ backend/src/kwai/api/v1/auth/endpoints/user.py
@@ -8,7 +8,14 @@ from kwai.modules.identity.users.user import UserEntity
 router = APIRouter()
 
 
-@router.get("/user")
+@router.get(
+    "/user",
+    summary="Get the id of the current user",
+    responses={
+        200: {"description": "Ok."},
+        401: {"description": "Not authorized"},
+    },
+)
 def get(user: UserEntity = Depends(get_current_user)):
     """Get the current user."""
     return {"id": str(user.uuid)}
