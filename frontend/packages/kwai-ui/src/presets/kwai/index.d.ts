fb274df2a354f71d14b51ba31a44f6c7c0f80431
zumuta <franky.braem@zumuta.be>
2024-11-29 22:16:08 +0100

    chore: remove preset

--- frontend/packages/kwai-ui/src/presets/kwai/index.d.ts
+++
@@ -1,6 +0,0 @@
-import type { PrimeVuePTOptions } from 'primevue/config';
-import type { PassThrough } from 'primevue/ts-helpers';
-
-declare module 'index.js' {
-  export = PassThrough<PrimeVuePTOptions>;
-}
