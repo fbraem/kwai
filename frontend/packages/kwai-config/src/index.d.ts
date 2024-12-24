f52966eb8b18b6468e75c13d7fdd3f622d50acab
fbraem <franky.braem@gmail.com>
2024-12-24 14:22:52 +0100

    fix: add missing types

--- frontend/packages/kwai-config/src/index.d.ts
+++ frontend/packages/kwai-config/src/index.d.ts
@@ -1,9 +1,14 @@
 declare module '@kwai/config' {
     const website: {
-        title: string
-        copyright: string
+        title: string,
+        copyright: string,
         email: string,
         url: string,
+    };
+    const contact: {
+        street: string,
+        city: string,
+        email: string,
     };
     const portal: {
         social_media: [{
@@ -11,8 +16,14 @@ declare module '@kwai/config' {
             account: string,
             icon: string,
             url: string,
+            text: string,
         }],
-        promotion: string[]
+        links: [{
+            title: string,
+            url: string,
+        }],
+        promotion: string[],
+        promotion_footer: string[],
     };
     const api: {
         base_url: string
