d504b0146048025c834686af9df54782c01ac7f7
fbraem <franky.braem@gmail.com>
2024-12-26 15:46:29 +0100

    fix: The glob option "as" has been deprecated in favour of "query"

--- frontend/apps/portal/src/composables/useHeroImage.ts
+++ frontend/apps/portal/src/composables/useHeroImage.ts
@@ -2,7 +2,14 @@
  * Function to get the url for a hero image of an application.
  */
 export const heroImage = new URL('../../images/hero.jpg', import.meta.url).href;
-const images = import.meta.glob('../../images/hero*.jpg', { eager: true, as: 'url' });
+const images = import.meta.glob(
+  '../../images/hero*.jpg',
+  {
+    eager: true,
+    query: '?url',
+    import: 'default'
+  },
+);
 export function getHeroImageUrl(applicationName: string) {
   for (const image in images) {
     const pathParts = image.split('/');
