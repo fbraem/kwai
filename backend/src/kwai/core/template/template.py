6b9346ed152bc36a146b1f7128a06da4dd3d092a
fbraem <franky.braem@gmail.com>
2024-12-26 14:34:36 +0100

    fix: remove wrong type for kwargs

--- backend/src/kwai/core/template/template.py
+++ backend/src/kwai/core/template/template.py
@@ -1,12 +1,12 @@
 """Module that defines an interface for a template."""
+
 from abc import abstractmethod
-from typing import Any
 
 
 class Template:
     """Interface for a template."""
 
     @abstractmethod
-    def render(self, **kwargs: dict[str, Any]) -> str:
+    def render(self, **kwargs) -> str:
         """Render the template with the variables."""
         raise NotImplementedError
