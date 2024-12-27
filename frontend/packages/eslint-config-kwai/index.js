a5f7db3262cd75462450a7d6abbf36879596dbce
fbraem <franky.braem@gmail.com>
2024-12-27 14:28:19 +0100

    fix: remove standard

--- frontend/packages/eslint-config-kwai/index.js
+++ frontend/packages/eslint-config-kwai/index.js
@@ -5,7 +5,6 @@ module.exports = {
   },
   plugins: ['@typescript-eslint', 'prettier'],
   extends: [
-    'standard',
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:vue/vue3-recommended',
