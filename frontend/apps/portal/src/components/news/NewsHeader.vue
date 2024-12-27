a082db4a100f421e7688635a9b88d780782e04a9
fbraem <franky.braem@gmail.com>
2024-12-27 14:42:45 +0100

    fix: add some right margin to tag

--- frontend/apps/portal/src/components/news/NewsHeader.vue
+++ frontend/apps/portal/src/components/news/NewsHeader.vue
@@ -16,19 +16,21 @@ defineProps<Props>();
     :to="`/news/${newsItem.id}`"
   >
     <NewsDate :date="newsItem.publishDate " />
-    <div class="flex-grow flex py-2 pl-2 border-b-2 border-red-600 items-center">
-      <h3 class="flex-grow text-2xl text-gray-500 font-bold">
-        {{ newsItem.texts[0].title }}
+    <div class="flex-grow py-2 pl-2 border-b-2 border-red-600 items-center">
+      <h3 class="flex content-between text-2xl text-gray-500 font-bold">
+        <span class="inline-block flex-grow">
+          {{ newsItem.texts[0].title }}
+        </span>
+        <router-link
+          :to="`/news/application/${newsItem.application.name}`"
+        >
+          <div class="mr-2">
+            <KwaiTag severity="secondary">
+              {{ newsItem.application.title }}
+            </KwaiTag>
+          </div>
+        </router-link>
       </h3>
-      <router-link
-        :to="`/news/application/${newsItem.application.name}`"
-      >
-        <div>
-          <KwaiTag severity="secondary">
-            {{ newsItem.application.title }}
-          </KwaiTag>
-        </div>
-      </router-link>
     </div>
   </router-link>
 </template>
