be4f4a86b66caf499a5b535e0fdd1bfe4484f889
fbraem <franky.braem@gmail.com>
2024-12-29 20:00:12 +0100

    refactor: use KwaiCard

--- frontend/apps/coach/src/pages/training_definitions/components/TrainingDefinitionCard.vue
+++ frontend/apps/coach/src/pages/training_definitions/components/TrainingDefinitionCard.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { Card, CardTitle } from '@kwai/ui';
+import { KwaiCard } from '@kwai/ui';
 import type { TrainingDefinition } from '@root/composables/useTrainingDefinition';
 import { computed } from 'vue';
 import { weekday } from '@kwai/date';
@@ -17,27 +17,21 @@ const period = computed(() => {
 </script>
 
 <template>
-  <Card class="w-full">
-    <template #header>
-      <CardTitle class="p-3">
-        <div class="font-bold text-lg text-gray-900">
-          {{ trainingDefinition.name }}
-        </div>
-        <div class="text-sm text-gray-600">
-          {{ weekdayName }} {{ period }}
-          <span v-if="trainingDefinition.team">
-            &bull; {{ trainingDefinition.team.name }}
-          </span>
-        </div>
-      </CardTitle>
+  <KwaiCard>
+    <template #title>
+      {{ trainingDefinition.name }}
     </template>
-    <p class="p-3">
-      {{ trainingDefinition.description }}
-    </p>
+    <template #subtitle>
+      {{ weekdayName }} {{ period }}
+      <span v-if="trainingDefinition.team">
+        &bull; {{ trainingDefinition.team.name }}
+      </span>
+    </template>
+    {{ trainingDefinition.description }}
     <template #footer>
       <slot name="footer" />
     </template>
-  </Card>
+  </KwaiCard>
 </template>
 
 <style scoped>
