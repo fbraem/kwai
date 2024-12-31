b17be2e87bb885ee5e4f03b5de1004d1983aaea8
fbraem <franky.braem@gmail.com>
2024-12-31 11:42:06 +0100

    refactor: eslint

--- frontend/packages/kwai-ui/src/dialogs/InformationDialog.vue
+++ frontend/packages/kwai-ui/src/dialogs/InformationDialog.vue
@@ -1,11 +1,23 @@
+<script setup lang="ts">
+withDefaults(
+  defineProps<{
+    bgDialog?: string
+    bgInformation?: string
+  }>(),
+  {
+    bgDialog: 'bg-white',
+    bgInformation: 'bg-red-700',
+  });
+</script>
+
 <template>
   <div
-      class="flex flex-col sm:flex-row-reverse mb-6 shadow-lg rounded-lg max-w-6xl"
-      :class="bgDialog"
+    class="flex flex-col sm:flex-row-reverse mb-6 shadow-lg rounded-lg max-w-6xl"
+    :class="bgDialog"
   >
     <div
-        class="w-full sm:w-1/2 md:w-1/3 sm:p-12 p-4 sm:rounded-r-lg"
-        :class="bgInformation"
+      class="w-full sm:w-1/2 md:w-1/3 sm:p-12 p-4 sm:rounded-r-lg"
+      :class="bgInformation"
     >
       <slot name="information" />
     </div>
@@ -14,13 +26,3 @@
     </div>
   </div>
 </template>
-
-<script setup lang="ts">
-const props = withDefaults(defineProps<{
-  bgDialog?: string,
-  bgInformation?: string,
-}>(), {
-  bgDialog: 'bg-white',
-  bgInformation: 'bg-red-700',
-})
-</script>
