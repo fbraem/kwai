6042394e05de59b944b717f23f49e486aad6b663
fbraem <franky.braem@gmail.com>
2025-01-01 14:25:49 +0100

    fix: eslint

--- frontend/packages/kwai-ui/src/badges/KwaiTag.vue
+++ frontend/packages/kwai-ui/src/badges/KwaiTag.vue
@@ -2,11 +2,15 @@
 import Tag from 'primevue/tag';
 
 interface Props {
-  value?: string | null,
-  rounded?: boolean,
-  severity?: 'info' | 'warning' | 'success' | 'danger' | 'secondary',
+  value?: string | null
+  rounded?: boolean
+  severity?: 'info' | 'warning' | 'success' | 'danger' | 'secondary'
 }
-withDefaults(defineProps<Props>(), { value: null, rounded: false, severity: undefined });
+withDefaults(defineProps<Props>(), {
+  value: null,
+  rounded: false,
+  severity: undefined,
+});
 </script>
 
 <template>
