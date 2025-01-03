57e1c3a2117b830e8c9befb988eaaf7f10d5f6fa
fbraem <franky.braem@gmail.com>
2025-01-03 20:10:23 +0100

    refactor: remove ContainerSectionBanner.vue

--- frontend/packages/kwai-ui/src/section/index.ts
+++ frontend/packages/kwai-ui/src/section/index.ts
@@ -1,4 +1,3 @@
 export { default as ContainerSection } from './ContainerSection.vue';
-export { default as ContainerSectionBanner } from './ContainerSectionBanner.vue';
 export { default as ContainerSectionContent } from './ContainerSectionContent.vue';
 export { default as ContainerSectionTitle } from './ContainerSectionTitle.vue';
