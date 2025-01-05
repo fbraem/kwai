79835f62446809cb3aaf1940de4c5ae47c8c91f6
fbraem <franky.braem@gmail.com>
2025-01-05 11:39:02 +0100

    feat: add is_duplicate and DuplicateMemberUploadException

--- backend/src/kwai/modules/club/repositories/file_upload_repository.py
+++ backend/src/kwai/modules/club/repositories/file_upload_repository.py
@@ -6,6 +6,10 @@ from kwai.modules.club.domain.file_upload import FileUploadEntity
 from kwai.modules.club.domain.member import MemberEntity
 
 
+class DuplicateMemberUploadedException(Exception):
+    """Raised when the member in current upload is loaded more than once."""
+
+
 class FileUploadRepository(ABC):
     """Interface for an import repository.
 
@@ -22,11 +26,19 @@ class FileUploadRepository(ABC):
         raise NotImplementedError
 
     @abstractmethod
+    def is_duplicate(self, member: MemberEntity) -> bool:
+        """Check if the member was already uploaded with this file upload."""
+
+    @abstractmethod
     async def save_member(self, file_upload: FileUploadEntity, member: MemberEntity):
         """Save a member imported from the file upload.
 
         Args:
             file_upload: The file upload.
             member: The member from the file upload.
+
+        Raises:
+            DuplicateMemberUploadedException: if the member in the file upload is
+                already uploaded.
         """
         raise NotImplementedError
