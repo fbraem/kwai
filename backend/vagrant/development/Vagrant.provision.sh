a0953a1f03189a15dec310c3062cf1076b3eab07
fbraem <franky.braem@gmail.com>
2025-01-16 20:36:55 +0100

    ci: install node

--- backend/vagrant/development/Vagrant.provision.sh
+++ backend/vagrant/development/Vagrant.provision.sh
@@ -51,3 +51,12 @@ EOF
 )
 echo "${REDIS_CONFIG}" > /etc/redis/redis.conf
 sudo /etc/init.d/redis-server restart
+
+# Install node
+cd /home/vagrant
+wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
+nvm install node
+node --version
