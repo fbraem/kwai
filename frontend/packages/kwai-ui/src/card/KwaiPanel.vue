1345c7afd15932cc338103f97543da29b35e1769
fbraem <franky.braem@gmail.com>
2025-01-27 19:30:34 +0100

    feat: add toggleable and icons slot

--- frontend/packages/kwai-ui/src/card/KwaiPanel.vue
+++ frontend/packages/kwai-ui/src/card/KwaiPanel.vue
@@ -1,9 +1,26 @@
 <script setup lang="ts">
 import Panel from 'primevue/panel';
+
+interface Props {
+  toggleable?: boolean
+  headerClass?: string
+}
+
+withDefaults(defineProps<Props>(), {
+  toggleable: false,
+  headerClass: '',
+});
 </script>
 
 <template>
-  <Panel>
+  <Panel
+    :toggleable="toggleable"
+    :pt="{
+      header: () => headerClass,
+      headerActions: () => 'flex align-items-center',
+    }"
+    class="w-full"
+  >
     <template
       v-if="$slots.header"
       #header
@@ -16,10 +33,14 @@ import Panel from 'primevue/panel';
     >
       <slot name="footer" />
     </template>
+    <template
+      v-if="$slots.icons"
+      #icons
+    >
+      <slot name="icons" />
+    </template>
     <slot />
   </Panel>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
