086b4cf1cd437b4af498708191e79644f70600c7
fbraem <franky.braem@gmail.com>
2025-02-12 20:36:42 +0100

    test: use fixture factory

--- backend/src/tests/modules/identity/tokens/test_refresh_token.py
+++ backend/src/tests/modules/identity/tokens/test_refresh_token.py
@@ -1,14 +1,8 @@
 """Module for testing the refresh token entity."""
 
-from kwai.core.domain.value_objects.timestamp import Timestamp
-from kwai.modules.identity.tokens.refresh_token import RefreshTokenEntity
 
-
-def test_create(make_access_token) -> None:
+def test_create(make_refresh_token) -> None:
     """Test creation of a refresh token entity."""
-    token = RefreshTokenEntity(
-        access_token=make_access_token(),
-        expiration=Timestamp.create_now().add_delta(days=1),
-    )
+    token = make_refresh_token()
     assert token.revoked is False, "A new token should not be revoked"
     assert token.expired is False, "A new token should not be expired"
