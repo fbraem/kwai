4cb01730b547ec4e70bb51bc8a082c6046b79660
fbraem <franky.braem@gmail.com>
2025-02-19 21:54:45 +0100

    feat: add always_commit

--- backend/src/kwai/core/db/uow.py
+++ backend/src/kwai/core/db/uow.py
@@ -6,8 +6,14 @@ from kwai.core.db.database import Database
 class UnitOfWork:
     """A unit of work implementation."""
 
-    def __init__(self, database: Database):
+    def __init__(self, database: Database, *, always_commit: bool = False):
+        """Initialize the unit of work.
+
+        When always_commit is True, the unit of work will always be committed, even
+        when an exception occurred.
+        """
         self._database = database
+        self._always_commit = always_commit
 
     async def __aenter__(self):
         """Enter the unit of work."""
@@ -17,8 +23,13 @@ class UnitOfWork:
     async def __aexit__(self, exc_type, exc_val, exc_tb):
         """Exit the unit of work.
 
-        When an exception occurred, the transaction will be rollbacked.
+        When an exception occurred and always_commit is False, the transaction will
+        be rollbacked.
         """
+        if self._always_commit:
+            await self._database.commit()
+            return
+
         if exc_type:
             await self._database.rollback()
         else:
