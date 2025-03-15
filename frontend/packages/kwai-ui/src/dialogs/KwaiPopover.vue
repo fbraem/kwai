5757b3ecaed0772c705c3b824fa681df86a955cf
fbraem <franky.braem@gmail.com>
2025-03-15 19:25:14 +0100

    Remove wrapping div

--- frontend/packages/kwai-ui/src/dialogs/KwaiPopover.vue
+++ frontend/packages/kwai-ui/src/dialogs/KwaiPopover.vue
@@ -21,28 +21,26 @@ defineOptions({ inheritAttrs: false });
 </script>
 
 <template>
-  <div>
-    <Popover
-      ref="op"
-      :pt="{ content: () => 'p-0' }"
-      v-bind="$attrs"
-    >
-      <div class="px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
-        <div class="sm:flex sm:items-start">
-          <div
-            class="mx-auto flex size-12 shrink-0 items-center justify-center rounded-full sm:mx-0 sm:size-10"
-          >
-            <slot name="icon" />
-          </div>
-          <div class="text-center sm:text-left mt-3 sm:mt-0 sm:ml-4">
-            <slot name="title" />
-            <div class="mt-2">
-              <slot />
-            </div>
+  <Popover
+    ref="op"
+    :pt="{ content: () => 'p-0' }"
+    v-bind="$attrs"
+  >
+    <div class="px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
+      <div class="sm:flex sm:items-start">
+        <div
+          class="mx-auto flex size-12 shrink-0 items-center justify-center rounded-full sm:mx-0 sm:size-10"
+        >
+          <slot name="icon" />
+        </div>
+        <div class="text-center sm:text-left mt-3 sm:mt-0 sm:ml-4">
+          <slot name="title" />
+          <div class="mt-2">
+            <slot />
           </div>
         </div>
       </div>
-      <slot name="footer" />
-    </Popover>
-  </div>
+    </div>
+    <slot name="footer" />
+  </Popover>
 </template>
