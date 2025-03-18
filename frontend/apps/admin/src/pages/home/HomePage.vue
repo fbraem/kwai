2bc646fd7dc7a4de5b08b5f3c1c357663f909f2c
fbraem <franky.braem@gmail.com>
2025-03-18 22:13:24 +0100

    feat: add user invitations

--- frontend/apps/admin/src/pages/home/HomePage.vue
+++ frontend/apps/admin/src/pages/home/HomePage.vue
@@ -7,7 +7,9 @@ import {
   KwaiCard,
   ListIcon,
 } from '@kwai/ui';
+
 import { useI18n } from 'vue-i18n';
+import InviteUserIcon from '@root/components/icons/InviteUserIcon.vue';
 
 const { t } = useI18n({ useScope: 'global' });
 </script>
@@ -24,13 +26,26 @@ const { t } = useI18n({ useScope: 'global' });
         </template>
         {{ t('home.users.description') }}
         <template #footer>
-          <KwaiButton
-            :to="{ name: 'admin.users' }"
-            small
-          >
-            <ListIcon class="w-4 mr-2 fill-current" />
-            {{ t('home.users.list') }}
-          </KwaiButton>
+          <div class="flex flex-row justify-between">
+            <div>
+              <KwaiButton
+                :to="{ name: 'admin.users' }"
+                small
+              >
+                <ListIcon class="w-4 mr-2 fill-current" />
+                {{ t('home.users.list') }}
+              </KwaiButton>
+            </div>
+            <div>
+              <KwaiButton
+                :to="{ name: 'admin.user_invitations' }"
+                small
+              >
+                <InviteUserIcon class="w-4 mr-2 fill-current" />
+                {{ t('home.user_invitations') }}
+              </KwaiButton>
+            </div>
+          </div>
         </template>
       </KwaiCard>
     </ContainerSectionContent>
