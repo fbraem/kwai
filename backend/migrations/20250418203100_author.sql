fb0b2137e934b31c842879e433e599f2768a6988
fbraem <franky.braem@gmail.com>
2025-04-30 20:23:22 +0200

    feat: add editor column to authors table

--- backend/migrations/20250418203100_author.sql
+++ backend/migrations/20250418203100_author.sql
@@ -3,9 +3,10 @@
 create table authors
 (
     user_id          int unsigned primary key,
-    name             varchar (255),
+    name             varchar(255),
     remark           text,
     active           tinyint(1) default 1 not null,
+    editor           tinyint(1) default 0 not null,
     created_at       datetime default CURRENT_TIMESTAMP not null,
     updated_at       datetime null
 ) charset = utf8mb3;
