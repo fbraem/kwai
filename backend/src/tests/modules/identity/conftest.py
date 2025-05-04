364407cd4c7b6d2023b97e7db3fea26e11e65daa
fbraem <franky.braem@gmail.com>
2025-05-04 19:22:57 +0200

    fix: remove user_invitation fixture

--- backend/src/tests/modules/identity/conftest.py
+++ backend/src/tests/modules/identity/conftest.py
@@ -1,19 +1,9 @@
 """Module that defines fixtures for user recovery testing."""
 
-from typing import AsyncGenerator
-
 import pytest
 
-from kwai.core.db.database import Database
-from kwai.core.domain.value_objects.email_address import EmailAddress
-from kwai.core.domain.value_objects.name import Name
 from kwai.core.template.mail_template import MailTemplate
 from kwai.core.template.template_engine import TemplateEngine
-from kwai.modules.identity.user_invitations.user_invitation import UserInvitationEntity
-from kwai.modules.identity.user_invitations.user_invitation_db_repository import (
-    UserInvitationDbRepository,
-)
-from kwai.modules.identity.users.user import UserEntity
 from tests.fixtures.identity.tokens import *  # noqa
 from tests.fixtures.identity.user_invitations import *  # noqa
 from tests.fixtures.identity.user_recoveries import *  # noqa
@@ -36,22 +26,3 @@ def user_invitation_mail_template(template_engine: TemplateEngine) -> MailTempla
         template_engine.create("mail/identity/invitation_html"),
         template_engine.create("mail/identity/invitation_txt"),
     )
-
-
-@pytest.fixture
-async def user_invitation(
-    database: Database, user: UserEntity
-) -> AsyncGenerator[UserInvitationEntity, None]:
-    """Fixture for a user invitation."""
-    repo = UserInvitationDbRepository(database)
-    invitation = await repo.create(
-        UserInvitationEntity(
-            email=EmailAddress("ichiro.abe@kwai.com"),
-            name=Name(first_name="Ichiro", last_name="Abe"),
-            remark="Created with pytest",
-            user=user,
-        )
-    )
-    yield invitation
-
-    await repo.delete(invitation)
